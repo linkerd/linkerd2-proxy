1fdcbfaba6a748b6a7a71921417a4b492c01ff32
Brian Smith <brian@briansmith.org>
2018-07-17 09:22:17 -1000

    Replace "conduit" with "linkerd" in TLS test data. (#17)
    This is purely aesthetic; the TLS logic doesn't care about the product name.
    
    The test data was regenerated by re-running gen-certs.sh after modifying it.
    
    Signed-off-by: Brian Smith <brian@briansmith.org>

--- src/transport/tls/testdata/gen-certs.sh
+++ src/transport/tls/testdata/gen-certs.sh
@@ -23,7 +23,7 @@ ee() {
   controller_namespace=$4
 
   ee_name=${ee_deployment}-${ee_namespace}
-  hostname=${ee_deployment}.deployment.${ee_namespace}.conduit-managed.${controller_namespace}.svc.cluster.local
+  hostname=${ee_deployment}.deployment.${ee_namespace}.linkerd-managed.${controller_namespace}.svc.cluster.local
 
   echo '{}' \
     | cfssl gencert -ca ${ca_name}.pem -ca-key ${ca_name}-key.pem -hostname=${hostname} - \
@@ -46,7 +46,7 @@ ee() {
   echo "apiVersion: v1" > ${secret}
   echo "kind: Secret" >> ${secret}
   echo "metadata:" >> ${secret}
-  echo "  name: ${ee_deployment}-deployment-tls-conduit-io" >> ${secret}
+  echo "  name: ${ee_deployment}-deployment-tls-linkerd-io" >> ${secret}
   echo "data:" >> ${secret}
   echo "  certificate.crt: ${crt}" >> "${ee_name}-${ca_name}-secret.yml"
   echo "  private-key.p8: ${p8}" >> "${ee_name}-${ca_name}-secret.yml"
@@ -56,8 +56,8 @@ ca "Cluster-local CA 1" ca1
 ca "Cluster-local CA 1" ca2 # Same name, different key pair.
 
 # The controller itself.
-ee ca1 controller conduit conduit
+ee ca1 controller linkerd linkerd
 
-ee ca1 foo ns1 conduit
-ee ca2 foo ns1 conduit # Same, but different CA
-ee ca1 bar ns1 conduit # Different service.
+ee ca1 foo ns1 linkerd
+ee ca2 foo ns1 linkerd # Same, but different CA
+ee ca1 bar ns1 linkerd # Different service.
