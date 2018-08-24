eb569aa3c5055055fbd7d0e2d70cedff0e9baa2b
Oliver Gould <ver@buoyant.io>
2018-08-24 03:20:54 +0000

    Move telemetry::metrics into dedicated crate
    The `metrics!` macro is currently local to the telemetry module.
    Furthermore, the `telemetry::metrics` module no longer has
    proxy-specific logic.
    
    This change moves the `telemetry::metrics` module into a new crate,
    `linkerd2_metrics`.
    
    This will enable unifying `telemetry::http` and `telemetry::transport`
    into `http` and `transport`, respectively.

---
+++ metrics/src/lib.rs
@@ -0,0 +1,39 @@
+//! Utilties for exposing metrics to Prometheus.
+
+extern crate deflate;
+extern crate indexmap;
+extern crate futures;
+extern crate http;
+extern crate hyper;
+#[macro_use]
+extern crate log;
+
+mod counter;
+mod gauge;
+mod histogram;
+pub mod latency;
+mod prom;
+mod scopes;
+mod serve;
+
+pub use self::counter::Counter;
+pub use self::gauge::Gauge;
+pub use self::histogram::Histogram;
+pub use self::prom::{FmtMetrics, FmtLabels, FmtMetric, Metric};
+pub use self::scopes::Scopes;
+pub use self::serve::Serve;
+
+#[macro_export]
+macro_rules! metrics {
+    { $( $name:ident : $kind:ty { $help:expr } ),+ } => {
+        $(
+            #[allow(non_upper_case_globals)]
+            const $name: ::linkerd2_metrics::Metric<'static, $kind> =
+                ::linkerd2_metrics::Metric {
+                    name: stringify!($name),
+                    help: $help,
+                    _p: ::std::marker::PhantomData,
+                };
+        )+
+    }
+}
