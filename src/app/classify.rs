f2a3c08e70561d21a14c36578e88a35467fddc83
Oliver Gould <ver@buoyant.io>
2018-10-29 23:50:18 +0000

    Use the grpc-status response header for classification
    When a gRPC service fails a request eagerly, before it begins sending a
    response, a `grpc-status` header is simply added to the initial response
    header (rather than added to trailers).
    
    This change ensures that classification honors these status codes.
    
    Fixes linkerd/linkerd2#1819.

--- src/app/classify.rs
+++ src/app/classify.rs
@@ -96,9 +96,125 @@ fn grpc_class(headers: &http::HeaderMap) -> Option<Class> {
         .get("grpc-status")
         .and_then(|v| v.to_str().ok())
         .and_then(|s| s.parse::<u32>().ok())
-        .map(|grpc_status| if grpc_status == 0 {
-            Class::Grpc(SuccessOrFailure::Success, grpc_status)
-        } else {
-            Class::Grpc(SuccessOrFailure::Failure, grpc_status)
+        .map(|grpc_status| {
+            if grpc_status == 0 {
+                Class::Grpc(SuccessOrFailure::Success, grpc_status)
+            } else {
+                Class::Grpc(SuccessOrFailure::Failure, grpc_status)
+            }
         })
+}
+
+#[cfg(test)]
+mod tests {
+    use http::{HeaderMap, Response, StatusCode};
+
+    use super::*;
+    use proxy::http::classify::{ClassifyEos as _CE, ClassifyResponse as _CR};
+
+    #[test]
+    fn http_response_status_ok() {
+        let rsp = Response::builder().status(StatusCode::OK).body(()).unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, None);
+
+        let class = ceos.eos(None);
+        assert_eq!(
+            class,
+            Class::Http(SuccessOrFailure::Success, StatusCode::OK)
+        );
+    }
+
+    #[test]
+    fn http_response_status_bad_request() {
+        let rsp = Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(())
+            .unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, None);
+
+        let class = ceos.eos(None);
+        assert_eq!(
+            class,
+            Class::Http(SuccessOrFailure::Success, StatusCode::BAD_REQUEST)
+        );
+    }
+
+    #[test]
+    fn http_response_status_server_error() {
+        let rsp = Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(())
+            .unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, None);
+
+        let class = ceos.eos(None);
+        assert_eq!(
+            class,
+            Class::Http(SuccessOrFailure::Failure, StatusCode::INTERNAL_SERVER_ERROR)
+        );
+    }
+
+    #[test]
+    fn grpc_response_header_ok() {
+        let rsp = Response::builder()
+            .header("grpc-status", "0")
+            .status(StatusCode::OK)
+            .body(())
+            .unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, Some(Class::Grpc(SuccessOrFailure::Success, 0)));
+
+        let class = ceos.eos(None);
+        assert_eq!(class, Class::Grpc(SuccessOrFailure::Success, 0));
+    }
+
+    #[test]
+    fn grpc_response_header_error() {
+        let rsp = Response::builder()
+            .header("grpc-status", "2")
+            .status(StatusCode::OK)
+            .body(())
+            .unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, Some(Class::Grpc(SuccessOrFailure::Failure, 2)));
+
+        let class = ceos.eos(None);
+        assert_eq!(class, Class::Grpc(SuccessOrFailure::Failure, 2));
+    }
+
+    #[test]
+    fn grpc_response_trailer_ok() {
+        let rsp = Response::builder().status(StatusCode::OK).body(()).unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, None);
+
+        let mut trailers = HeaderMap::new();
+        trailers.insert("grpc-status", "0".parse().unwrap());
+
+        let class = ceos.eos(Some(&trailers));
+        assert_eq!(class, Class::Grpc(SuccessOrFailure::Success, 0));
+    }
+
+    #[test]
+    fn grpc_response_trailer_error() {
+        let rsp = Response::builder().status(StatusCode::OK).body(()).unwrap();
+        let crsp = ClassifyResponse {};
+        let (ceos, class) = crsp.start(&rsp);
+        assert_eq!(class, None);
+
+        let mut trailers = HeaderMap::new();
+        trailers.insert("grpc-status", "3".parse().unwrap());
+
+        let class = ceos.eos(Some(&trailers));
+        assert_eq!(class, Class::Grpc(SuccessOrFailure::Failure, 3));
+    }
 }
