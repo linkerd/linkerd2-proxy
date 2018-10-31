eebaddcf75b1eceb2900e4895917a8ef650388a1
Oliver Gould <ver@buoyant.io>
2018-10-31 14:57:22 +0000

    revert unneeded changes

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -3,9 +3,8 @@ pub mod destination;
 mod fully_qualified_authority;
 mod observe;
 pub mod pb;
-pub mod remote_stream;
+mod remote_stream;
 mod serve_http;
 
-pub use self::fully_qualified_authority::FullyQualifiedAuthority;
 pub use self::observe::Observe;
 pub use self::serve_http::serve_http;
