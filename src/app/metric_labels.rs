863802c9f5c8a089c41dc466a220ed4c6e99a9d2
Kevin Leimkuhler <kevin@kleimkuhler.com>
2019-02-06 10:19:40 -0800

    Remove endpoint address from metric labels
    Signed-off-by: Kevin Leimkuhler <kevin@kleimkuhler.com>
    Co-authored-by: Eliza Weisman <eliza@buoyant.io>

--- src/app/metric_labels.rs
+++ src/app/metric_labels.rs
@@ -1,6 +1,5 @@
 use std::{
     fmt::{self, Write},
-    net,
 };
 
 use metrics::FmtLabels;
@@ -18,7 +17,6 @@ pub struct ControlLabels {
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct EndpointLabels {
-    addr: net::SocketAddr,
     direction: Direction,
     tls_status: tls::Status,
     dst_name: Option<NameAddr>,
@@ -89,7 +87,6 @@ impl FmtLabels for RouteLabels {
 impl From<inbound::Endpoint> for EndpointLabels {
     fn from(ep: inbound::Endpoint) -> Self {
         Self {
-            addr: ep.addr,
             dst_name: ep.dst_name,
             direction: Direction::In,
             tls_status: ep.source_tls_status,
@@ -114,7 +111,6 @@ where
 impl From<outbound::Endpoint> for EndpointLabels {
     fn from(ep: outbound::Endpoint) -> Self {
         Self {
-            addr: ep.connect.addr,
             dst_name: ep.dst_name,
             direction: Direction::Out,
             tls_status: ep.connect.tls_status(),
