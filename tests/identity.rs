b826e72281c82ca564b1c2898a74a5c89d3373f1
Eliza Weisman <eliza@buoyant.io>
2019-03-29 15:34:09 -0700

    Add test for identity refresh
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- tests/identity.rs
+++ tests/identity.rs
@@ -4,7 +4,10 @@
 mod support;
 use self::support::*;
 
-use std::time::{Duration, SystemTime};
+use std::{
+    time::{Duration, SystemTime},
+    sync::{Arc, atomic::{AtomicBool, Ordering}},
+};
 
 #[test]
 fn ready() {
@@ -36,4 +39,47 @@ fn ready() {
 
     // Now, the proxy should be ready.
     assert_eventually!(ready().status() == http::StatusCode::OK);
+}
+
+#[test]
+fn refresh() {
+    let _ = env_logger_init();
+    let id = "foo.ns1.serviceaccount.identity.linkerd.cluster.local";
+    let identity::Identity {
+        mut env,
+        certify_rsp,
+    } = identity::Identity::new("foo-ns1", id.to_string());
+
+    let (expiry_tx, expiry_rx) = oneshot::channel();
+    let refreshed = Arc::new(AtomicBool::new(false));
+
+    let rsp1 = certify_rsp.clone();
+    let rsp2 = certify_rsp.clone();
+    let refreshed2 = refreshed.clone();
+    let id_svc = controller::identity()
+        .certify(move |_| {
+            let mut rsp = rsp1;
+            let expiry = SystemTime::now() + Duration::from_millis(20);
+            rsp.valid_until = Some(expiry.into());
+            expiry_tx.send(expiry).unwrap();
+            rsp
+        })
+        .certify(move |_| {
+            let mut rsp = rsp2;
+            rsp.valid_until = Some((SystemTime::now() + Duration::from_millis(6000)).into());
+            refreshed2.store(true, Ordering::SeqCst);
+            rsp
+        })
+        .run();
+
+    // Disable the minimum bound on when to refresh identity for this test.
+    env.put(app::config::ENV_IDENTITY_MIN_REFRESH, "0ms".into());
+    let _proxy = proxy::new().identity(id_svc).run_with_test_env(env);
+
+    let expiry = expiry_rx.wait().expect("wait for expiry");
+    let how_long = expiry.duration_since(SystemTime::now()).unwrap();
+
+    std::thread::sleep(how_long);
+
+    assert_eventually!(refreshed.load(Ordering::SeqCst) == true);
 }
