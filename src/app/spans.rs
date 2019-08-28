30a2bdb8ad16f1509acd4b197fd590bc82199dc3
Alex Leong <alex@buoyant.io>
2019-08-28 12:09:31 -0700

    Better logging of invalid ids
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- src/app/spans.rs
+++ src/app/spans.rs
@@ -4,6 +4,8 @@ use crate::proxy::http::trace;
 use opencensus_proto::gen::trace::{v1 as oc};
 use futures::{Async, Poll, Stream, try_ready};
 use tracing::warn;
+use linkerd2_proxy_core::Error;
+use std::{error, fmt};
 
 const SPAN_KIND_SERVER: i32 = 1;
 const SPAN_KIND_CLIENT: i32 = 2;
@@ -11,6 +13,25 @@ const SPAN_KIND_CLIENT: i32 = 2;
 pub struct SpanConverter<S> {
     direction: Direction,
     spans: S
+}
+
+#[derive(Debug)]
+pub struct IdLengthError {
+    id: String,
+    expected_size: usize,
+    actual_size: usize
+}
+
+impl error::Error for IdLengthError {
+    fn description(&self) -> &str {
+        "trace or span id is wrong number of bytes"
+    }
+}
+
+impl fmt::Display for IdLengthError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Id '{}' should have {} bytes but it has {}", self.id, self.expected_size, self.actual_size)
+    }
 }
 
 impl<S> SpanConverter<S> {
@@ -27,6 +48,30 @@ impl<S> SpanConverter<S> {
             spans,
         }
     }
+
+    fn mk_span(&self, span: trace::Span) -> Result<oc::Span, Error> {
+        Ok(oc::Span {
+            trace_id: into_bytes(span.trace_id, 16)?,
+            span_id: into_bytes(span.span_id, 8)?,
+            tracestate: None,
+            parent_span_id: into_bytes(span.parent_id, 8)?,
+            name: Some(truncatable(span.span_name)),
+            kind: match self.direction {
+                Direction::In => SPAN_KIND_SERVER,
+                Direction::Out => SPAN_KIND_CLIENT,
+            },
+            start_time: Some(span.start.into()),
+            end_time: Some(span.end.into()),
+            attributes: None,
+            stack_trace: None,
+            time_events: None,
+            links: None,
+            status: None, // TODO: record this
+            resource: None,
+            same_process_as_parent_span: Some(false),
+            child_span_count: None,
+        })
+    }
 }
 
 impl<S> Stream for SpanConverter<S>
@@ -37,38 +82,29 @@ where
     type Error = S::Error;
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        let s = try_ready!(self.spans.poll()).map(|span|
-            oc::Span {
-                trace_id: into_bytes(span.trace_id),
-                span_id: into_bytes(span.span_id),
-                tracestate: None,
-                parent_span_id: into_bytes(span.parent_id),
-                name: Some(truncatable(span.span_name)),
-                kind: match self.direction {
-                    Direction::In => SPAN_KIND_SERVER,
-                    Direction::Out => SPAN_KIND_CLIENT,
+        loop {
+            match try_ready!(self.spans.poll()) {
+                Some(span) => match self.mk_span(span) {
+                    Ok(s) => return Ok(Async::Ready(Some(s))),
+                    Err(e) => warn!("Invalid span: {}", e),
                 },
-                start_time: Some(span.start.into()),
-                end_time: Some(span.end.into()),
-                attributes: None,
-                stack_trace: None,
-                time_events: None,
-                links: None,
-                status: None, // TODO: record this
-                resource: None,
-                same_process_as_parent_span: Some(false),
-                child_span_count: None,
-            }
-        );
-        Ok(Async::Ready(s))
+                None => return Ok(Async::Ready(None)),
+            };
+        }
     }
 }
 
-fn into_bytes(id: String) -> Vec<u8> {
-    hex::decode(id).unwrap_or_else(|e| {
-        warn!("invalid trace id: {}", e);
-        Vec::new()
-    })
+fn into_bytes(id: String, size: usize) -> Result<Vec<u8>, Error> {
+    let bytes = hex::decode(&id)?;
+    if bytes.len() == size {
+        Ok(bytes)
+    } else {
+        Err(IdLengthError{
+            id,
+            expected_size: size,
+            actual_size: bytes.len(),
+        }.into())
+    }
 }
 
 fn truncatable(value: String) -> oc::TruncatableString {
