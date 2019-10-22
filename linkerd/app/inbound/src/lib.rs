9eea6f655a79d52bfaf705d1795d214d68e8eba1
Oliver Gould <ver@buoyant.io>
2019-10-22 19:06:11 +0000

    include target.addr on outbound source context

--- linkerd/app/inbound/src/lib.rs
+++ linkerd/app/inbound/src/lib.rs
@@ -237,6 +237,7 @@ pub fn spawn<O, L, P>(
                 "source",
                 peer.ip = %src.addrs.peer().ip(),
                 peer.id = ?src.peer_identity
+                target.addr = %src.target_addr()
             )
         }))
         .push(trace_context_layer)
