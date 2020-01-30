0212472244dacf4882378434c3f70077f8cc0c10
Eliza Weisman <eliza@buoyant.io>
2020-01-30 14:17:43 -0800

    ghjfdgkjls
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- profiling/run-proxy.sh
+++ profiling/run-proxy.sh
@@ -5,16 +5,13 @@ set -o pipefail
 declare -a PRECMD
 PRECMD=()
 
-if [ "$PROXY_HEAPTRACK" -eq "1" ]; then
-    PRECMD+=(env "LD_PRELOAD=/libmemory_profiler.so")
-fi
+# if [ "$PROXY_HEAPTRACK" -eq "1" ]; then
+#     PRECMD+=(env "LD_PRELOAD=/libmemory_profiler.so")
+# fi
 if [ "$PROXY_PERF" -eq "1" ]; then
-    PRECMD+=(perf record -F 2000 -g )
-fi
-
-"${PRECMD[@]}" /usr/lib/linkerd2_proxy
-
-if [ "$PROXY_PERF" -eq "1" ]; then
-  perf script | inferno-collapse-perf > "/out/out_$NAME.$ID.folded"  # separate step to be able to rerun flamegraph with another width if needed
-  inferno-flamegraph --width 4000 "out/out_$NAME.$ID.folded" > "/out/flamegraph_$NAME.$ID.svg"
+    ("${PRECMD[@]}" perf record -F 2000 -g /usr/lib/linkerd2-proxy) > perf.data
+    (perf script | inferno-collapse-perf) > "/out/out_$NAME.$ID.folded"  # separate step to be able to rerun flamegraph with another width if needed
+    inferno-flamegraph --width 4000 "out/out_$NAME.$ID.folded" > "/out/flamegraph_$NAME.$ID.svg"
+else
+    "${PRECMD[@]}" /usr/lib/linkerd2-proxy
 fi
