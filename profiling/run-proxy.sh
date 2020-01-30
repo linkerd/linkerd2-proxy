8105fec888586262155f35599058af06a3ba2cfb
Eliza Weisman <eliza@buoyant.io>
2020-01-30 14:49:49 -0800

    xdfklnjdfgskln;klsgdf

--- profiling/run-proxy.sh
+++ profiling/run-proxy.sh
@@ -10,7 +10,7 @@ PRECMD=()
 #     PRECMD+=(env "LD_PRELOAD=/libmemory_profiler.so")
 # fi
 if [ "$PROXY_PERF" -eq "1" ]; then
-    ("${PRECMD[@]}" perf record -F 2000 -g /usr/lib/linkerd2-proxy) > perf.data
+    ("${PRECMD[@]}" perf record -F 2000 -g /usr/lib/linkerd/linkerd2-proxy) > perf.data
     (perf script | inferno-collapse-perf) > "/out/out_$NAME.$ID.folded"  # separate step to be able to rerun flamegraph with another width if needed
     inferno-flamegraph --width 4000 "out/out_$NAME.$ID.folded" > "/out/flamegraph_$NAME.$ID.svg"
 else
