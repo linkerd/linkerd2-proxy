11a43b1ed0fabdbb0f663c8c4e4913e088f8b2c6
Oliver Gould <ver@buoyant.io>
2020-01-31 16:10:19 +0000

    router: Avoid large up-front allocations
    We initially opted to pre-allocate the delay queue to avoid allocation
    at runtime. However, practically, router capacities are _much_ higher
    than their typical usage; so we incur an extra ~1MB+ of allocation that
    will never really be used (for outbound routers).
    
    This change initializes the router's DelayQueue with only a single slot
    by default. The capacity will be increased at runtime as needed.

--- linkerd/router/src/cache.rs
+++ linkerd/router/src/cache.rs
@@ -58,7 +58,7 @@ where
         Self {
             capacity,
             expires,
-            expirations: DelayQueue::with_capacity(capacity),
+            expirations: DelayQueue::with_capacity(1),
             values: IndexMap::default(),
             purge_task: None,
         }
