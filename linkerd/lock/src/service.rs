2b7b875b1b569f99120295abff39554ecc0861ca
Oliver Gould <ver@buoyant.io>
2020-02-17 17:54:55 +0000

    Reimplement the Lock middleware with tokio::sync
    As described in tokio-rs/tokio#2237, the `tokio::sync::Semaphore` can
    hold unbounded memory, especially when the semaphor is being contested
    and consumers drop interest. Unfortunately, this use case is common in
    the proxy, especially when a destination service is unavailable and the
    proxy is timing out requests.
    
    This change reimplements the Lock middleware without using
    `tokio::sync::Semaphore`. This implementation is in some ways more
    naive and inefficient, but it appears to be better suited for the
    proxy's needs. Specifically, waiters are stored in a LIFO stack, which
    optimizes for minimizing latency. Under certain high-load scenarios,
    this Lock could be forced to grow its waiters set without cleaning up
    expired watchers. If this becomes a more serious concern, we could
    change the implementation to use a FIFO queue of waiters.

---
+++ linkerd/lock/src/service.rs
@@ -0,0 +1,177 @@
+use crate::error::{Error, Poisoned, ServiceError};
+use crate::shared::{Shared, Wait};
+use futures::{future, Async, Future, Poll};
+use std::sync::{Arc, Mutex};
+use tracing::trace;
+
+/// Guards access to an inner service with a `tokio::sync::lock::Lock`.
+///
+/// As the service is polled to readiness, the lock is acquired and the inner
+/// service is polled. If the sevice is cloned, the service's lock state is not
+/// retained by the clone.
+pub struct Lock<S> {
+    state: State<S>,
+    shared: Arc<Mutex<Shared<S>>>,
+}
+
+/// The state of a single `Lock` consumer.
+enum State<S> {
+    /// This lock has not registered interest in the inner service.
+    Released,
+
+    /// This lock is interested in the inner service.
+    Waiting(Wait),
+
+    /// This lock instance has exclusive ownership of the inner service.
+    Acquired(S),
+
+    /// The inner service has failed.
+    Failed(Arc<Error>),
+}
+
+// === impl Lock ===
+
+impl<S> Lock<S> {
+    pub fn new(service: S) -> Self {
+        Self {
+            state: State::Released,
+            shared: Arc::new(Mutex::new(Shared::new(service))),
+        }
+    }
+}
+
+impl<S> Clone for Lock<S> {
+    fn clone(&self) -> Self {
+        Self {
+            // Clones have an independent local lock state.
+            state: State::Released,
+            shared: self.shared.clone(),
+        }
+    }
+}
+
+impl<T, S> tower::Service<T> for Lock<S>
+where
+    S: tower::Service<T>,
+    S::Error: Into<Error>,
+{
+    type Response = S::Response;
+    type Error = Error;
+    type Future = future::MapErr<S::Future, fn(S::Error) -> Self::Error>;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        loop {
+            trace!(state = ?self.state, "Polling");
+            self.state = match self.state {
+                // This instance has exlcusive access to the inner service.
+                State::Acquired(ref mut svc) => match svc.poll_ready() {
+                    Ok(ok) => {
+                        trace!(ready = ok.is_ready());
+                        return Ok(ok);
+                    }
+                    Err(inner) => {
+                        // If the inner service fails to become ready, share
+                        // that error with all other consumers and update this
+                        // lock's state to prevent trying to acquire the shared
+                        // state again.
+                        let error = Arc::new(inner.into());
+                        trace!(%error);
+                        if let Ok(mut shared) = self.shared.lock() {
+                            shared.fail(error.clone());
+                        }
+                        State::Failed(error)
+                    }
+                },
+
+                // This instance has not registered interest in the lock.
+                State::Released => match self.shared.lock() {
+                    Err(_) => return Err(Poisoned::new().into()),
+                    // First, try to acquire the lock withotu creating a waiter.
+                    // If the lock isn't available, create a waiter and try
+                    // again, registering interest>
+                    Ok(mut shared) => match shared.try_acquire() {
+                        Ok(None) => State::Waiting(Wait::default()),
+                        Ok(Some(svc)) => State::Acquired(svc),
+                        Err(error) => State::Failed(error),
+                    },
+                },
+
+                // This instance is interested in the lock.
+                State::Waiting(ref waiter) => match self.shared.lock() {
+                    Err(_) => return Err(Poisoned::new().into()),
+                    Ok(mut shared) => match shared.poll_acquire(waiter) {
+                        Ok(Async::NotReady) => return Ok(Async::NotReady),
+                        Ok(Async::Ready(svc)) => State::Acquired(svc),
+                        Err(error) => State::Failed(error),
+                    },
+                },
+
+                // The inner service failed, so share that failure with all consumers.
+                State::Failed(ref error) => return Err(ServiceError::new(error.clone()).into()),
+            };
+        }
+    }
+
+    fn call(&mut self, req: T) -> Self::Future {
+        // The service must have been acquired by poll_ready. Reset this lock's
+        // state so that it must reacquire the service via poll_ready.
+        let mut svc = match std::mem::replace(&mut self.state, State::Released) {
+            State::Acquired(svc) => svc,
+            _ => panic!("Called before ready"),
+        };
+
+        let fut = svc.call(req);
+
+        // Return the service to the shared state, notifying waiters as needed.
+        //
+        // The service is dropped if the inner mutex has been poisoned, and
+        // subsequent calsl to poll_ready will return a Poisioned error.
+        if let Ok(mut shared) = self.shared.lock() {
+            trace!("Releasing acquired lock after use");
+            shared.release_and_notify(svc);
+        }
+
+        // The inner service's error type is *not* wrapped with a ServiceError.
+        fut.map_err(Into::into)
+    }
+}
+
+impl<S> Drop for Lock<S> {
+    fn drop(&mut self) {
+        let state = std::mem::replace(&mut self.state, State::Released);
+        trace!(?state, "Dropping");
+        match state {
+            // If this lock was holding the service, return it back back to the
+            // shared state so another lock may acquire it. Waiters are notified.
+            State::Acquired(service) => {
+                if let Ok(mut shared) = self.shared.lock() {
+                    shared.release_and_notify(service);
+                }
+            }
+
+            // If this lock is waiting but the waiter isn't registered, it must
+            // have been notified. Notify the next waiter to prevent deadlock.
+            State::Waiting(wait) => {
+                if let Ok(mut shared) = self.shared.lock() {
+                    if wait.is_not_waiting() {
+                        shared.notify_next_waiter();
+                    }
+                }
+            }
+
+            // No state to cleanup.
+            State::Released | State::Failed(_) => {}
+        }
+    }
+}
+
+impl<S> std::fmt::Debug for State<S> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            State::Released => write!(f, "Released"),
+            State::Acquired(_) => write!(f, "Acquired(..)"),
+            State::Waiting(ref w) => write!(f, "Waiting(pending={})", w.is_waiting()),
+            State::Failed(ref e) => write!(f, "Failed({:?})", e),
+        }
+    }
+}
---
+++ linkerd/lock/src/shared.rs
@@ -0,0 +1,166 @@
+use self::waiter::Notify;
+pub(crate) use self::waiter::Wait;
+use crate::error::Error;
+use futures::{Async, Poll};
+use std::sync::Arc;
+use tracing::trace;
+
+/// The shared state between one or more Lock instances.
+///
+/// When multiple lock instances are contending for the inner value, waiters are
+/// stored in a LIFO stack. This is done to bias for latency instead of fairness
+pub(crate) struct Shared<T> {
+    state: State<T>,
+    waiters: Vec<Notify>,
+}
+
+enum State<T> {
+    /// A Lock is holding the value.
+    Claimed,
+
+    /// The inner value is available.
+    Unclaimed(T),
+
+    /// The lock has failed.
+    Failed(Arc<Error>),
+}
+
+// === impl Shared ===
+
+impl<T> Shared<T> {
+    pub fn new(value: T) -> Self {
+        Self {
+            waiters: Vec::new(),
+            state: State::Unclaimed(value),
+        }
+    }
+
+    /// Try to claim a value without registering a waiter.
+    pub fn try_acquire(&mut self) -> Result<Option<T>, Arc<Error>> {
+        match std::mem::replace(&mut self.state, State::Claimed) {
+            // This lock has acquired the value.
+            State::Unclaimed(v) => {
+                trace!("acquired");
+                Ok(Some(v))
+            }
+            // The value is already claimed by a lock.
+            State::Claimed => Ok(None),
+            // The locks failed, so reset the state immediately so that all
+            // instances may be notified.
+            State::Failed(error) => {
+                self.state = State::Failed(error.clone());
+                Err(error)
+            }
+        }
+    }
+
+    /// Try to acquire a value or register the given waiter to be notified when
+    /// the lock is available.
+    pub fn poll_acquire(&mut self, wait: &Wait) -> Poll<T, Arc<Error>> {
+        match self.try_acquire() {
+            Ok(Some(svc)) => Ok(Async::Ready(svc)),
+            Ok(None) => {
+                // Register the current task to be notified.
+                wait.register();
+                // Register the waiter's notify handle if one isn't already registered.
+                if let Some(notify) = wait.get_notify() {
+                    trace!("Registering waiter");
+                    self.waiters.push(notify);
+                }
+                debug_assert!(wait.is_waiting());
+                Ok(Async::NotReady)
+            }
+            Err(error) => Err(error),
+        }
+    }
+
+    pub fn release_and_notify(&mut self, value: T) {
+        trace!(waiters = self.waiters.len(), "Releasing");
+        debug_assert!(match self.state {
+            State::Claimed => true,
+            _ => false,
+        });
+        self.state = State::Unclaimed(value);
+        self.notify_next_waiter();
+    }
+
+    pub fn notify_next_waiter(&mut self) {
+        while let Some(waiter) = self.waiters.pop() {
+            if waiter.notify() {
+                trace!("Notified waiter");
+                return;
+            }
+        }
+    }
+
+    pub fn fail(&mut self, error: Arc<Error>) {
+        trace!(waiters = self.waiters.len(), %error, "Failing");
+        debug_assert!(match self.state {
+            State::Claimed => true,
+            _ => false,
+        });
+        self.state = State::Failed(error);
+
+        while let Some(waiter) = self.waiters.pop() {
+            waiter.notify();
+        }
+    }
+}
+
+mod waiter {
+    use futures::task::AtomicTask;
+    use std::sync::{Arc, Weak};
+
+    /// A handle held by Lock instances when waiting to be notified.
+    #[derive(Default)]
+    pub(crate) struct Wait(Arc<AtomicTask>);
+
+    /// A handle held by shared lock state to notify a waiting Lock.
+    ///
+    /// There may be at most one `Notify` instance per `Wait` instance at any one
+    /// time.
+    pub(super) struct Notify(Weak<AtomicTask>);
+
+    impl Wait {
+        /// If a `Notify` handle does not currently exist for this waiter, create
+        /// a new one.
+        pub(super) fn get_notify(&self) -> Option<Notify> {
+            if self.is_not_waiting() {
+                let n = Notify(Arc::downgrade(&self.0));
+                debug_assert!(self.is_waiting());
+                Some(n)
+            } else {
+                None
+            }
+        }
+
+        /// Register this waiter with the current task.
+        pub(super) fn register(&self) {
+            self.0.register();
+        }
+
+        /// Returns true iff there is currently a `Notify` handle for this waiter.
+        pub fn is_waiting(&self) -> bool {
+            Arc::weak_count(&self.0) == 1
+        }
+
+        /// Returns true iff there is not currently a `Notify` handle for this waiter.
+        pub fn is_not_waiting(&self) -> bool {
+            Arc::weak_count(&self.0) == 0
+        }
+    }
+
+    impl Notify {
+        /// Attempt to notify the waiter.
+        ///
+        /// Returns true if a waiter was notified and false otherwise.
+        pub fn notify(self) -> bool {
+            if let Some(task) = self.0.upgrade() {
+                task.notify();
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
