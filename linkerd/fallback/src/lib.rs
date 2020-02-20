e092c203370f13ebec15f1a5adf75cccac3dd1db
Oliver Gould <ver@buoyant.io>
2020-02-20 23:26:16 +0000

    Fixup comment

--- linkerd/fallback/src/lib.rs
+++ linkerd/fallback/src/lib.rs
@@ -3,11 +3,10 @@ use linkerd2_error::Error;
 use tower::util::{Either, Oneshot, ServiceExt};
 use tracing::debug;
 
-/// A fallback layer composing two service builders.
+/// A fallback layer composing two services.
 ///
-/// If the future returned by the primary builder's `MakeService` fails with
-/// an error matching a given predicate, the fallback future will attempt
-/// to call the secondary `MakeService`.
+/// If the future returned by the primary service fails with an error matching a
+/// given predicate, the fallback service is called. The result is returned in an `Either`.
 #[derive(Clone, Debug)]
 pub struct FallbackLayer<F, P = fn(&Error) -> bool> {
     fallback: F,
