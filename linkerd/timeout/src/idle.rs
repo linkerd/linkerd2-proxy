5eba87838f73780485149ed947aac2ac49dad14b
Oliver Gould <ver@buoyant.io>
2020-03-04 21:15:12 +0000

    Use a Delay to be notified when the service shuold idle out

--- linkerd/timeout/src/idle.rs
+++ linkerd/timeout/src/idle.rs
@@ -2,16 +2,22 @@ use crate::error::HumanDuration;
 use futures::{future, Async, Future, Poll};
 use linkerd2_error::Error;
 use std::time::{Duration, Instant};
-use tracing::debug;
+use tokio_timer::Delay;
 
 #[derive(Copy, Clone, Debug)]
 pub struct IdleLayer(Duration);
 
 #[derive(Debug)]
 pub struct Idle<S> {
-    inner: S,
+    inner: Inner<S>,
     max_idle: Duration,
-    ready_since: Option<Instant>,
+}
+
+#[derive(Debug)]
+enum Inner<S> {
+    NotReady(S),
+    Ready(Delay, S),
+    Failed,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -30,9 +36,8 @@ impl<S> tower::layer::Layer<S> for IdleLayer {
 
     fn layer(&self, inner: S) -> Self::Service {
         Self::Service {
-            inner,
             max_idle: self.0,
-            ready_since: None,
+            inner: Inner::NotReady(inner),
         }
     }
 }
@@ -49,28 +54,51 @@ where
     type Future = future::MapErr<S::Future, fn(S::Error) -> Self::Error>;
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
-        if let Some(ready_since) = self.ready_since {
-            let idle = Instant::now() - ready_since;
-            debug!(idle = %HumanDuration(&idle), max = %HumanDuration(&self.max_idle));
-            if idle > self.max_idle {
-                return Err(IdleError(idle).into());
-            }
-        }
+        loop {
+            match std::mem::replace(&mut self.inner, Inner::Failed) {
+                // If the inner service isn't ready, poll it until it is.
+                Inner::NotReady(mut svc) => match svc.poll_ready().map_err(Into::into)? {
+                    Async::NotReady => {
+                        self.inner = Inner::NotReady(svc);
+                        return Ok(Async::NotReady);
+                    }
+                    Async::Ready(()) => {
+                        let timeout = Delay::new(Instant::now() + self.max_idle);
+                        self.inner = Inner::Ready(timeout, svc);
+                        // Continue, ensuring the timeout is polled.
+                    }
+                },
 
-        match self.inner.poll_ready().map_err(Into::into) {
-            Ok(Async::Ready(())) => {
-                if self.ready_since.is_none() {
-                    self.ready_since = Some(Instant::now());
+                // Once the inner service is ready, poll a timeout to be notified if it should be failed.
+                Inner::Ready(mut timeout, svc) => {
+                    if timeout.poll().expect("timer must not fail").is_not_ready() {
+                        // The service stays ready until the timeout expires.
+                        self.inner = Inner::Ready(timeout, svc);
+                        return Ok(Async::Ready(()));
+                    }
+                    debug_assert!(match self.inner {
+                        Inner::Failed => true,
+                        _ => false,
+                    });
                 }
-                Ok(Async::Ready(()))
+
+                // The inner service has been dropped this service will only return an IdleError now.
+                Inner::Failed => return Err(IdleError(self.max_idle).into()),
             }
-            ready => ready,
         }
     }
 
     fn call(&mut self, req: T) -> Self::Future {
-        self.ready_since = None;
-        self.inner.call(req).map_err(Into::into)
+        match std::mem::replace(&mut self.inner, Inner::Failed) {
+            Inner::Ready(_, mut svc) => {
+                let fut = svc.call(req);
+                self.inner = Inner::NotReady(svc);
+                fut.map_err(Into::into)
+            }
+            Inner::NotReady(_) | Inner::Failed => {
+                panic!("poll_ready must be called");
+            }
+        }
     }
 }
 
