842dd496e4039104a7e1ea26ee24df9e3313d3df
Zahari Dichev <zaharidichev@gmail.com>
2020-04-15 13:17:23 +0300

    Address more comments
    Signed-off-by: Zahari Dichev <zaharidichev@gmail.com>

--- linkerd/proxy/tap/src/accept.rs
+++ linkerd/proxy/tap/src/accept.rs
@@ -67,23 +67,19 @@ impl Service<Connection> for AcceptPermittedClients {
     }
 
     fn call(&mut self, (meta, io): Connection) -> Self::Future {
-        match meta.peer_identity {
+        future::ok(match meta.peer_identity {
             Conditional::Some(ref peer) => {
                 if self.permitted_client_ids.contains(peer) {
-                    future::ok(self.serve_authenticated(io))
+                    self.serve_authenticated(io)
                 } else {
-                    future::ok(
-                        self.serve_unauthenticated(io, format!("Unauthorized peer: {}", peer)),
-                    )
+                    self.serve_unauthenticated(io, format!("Unauthorized peer: {}", peer))
                 }
             }
             Conditional::None(ReasonForNoIdentity::NoPeerName(ReasonForNoPeerName::Loopback)) => {
-                future::ok(self.serve_authenticated(io))
+                self.serve_authenticated(io)
             }
-            Conditional::None(reason) => {
-                future::ok(self.serve_unauthenticated(io, reason.to_string()))
-            }
-        }
+            Conditional::None(reason) => self.serve_unauthenticated(io, reason.to_string()),
+        })
     }
 }
 
