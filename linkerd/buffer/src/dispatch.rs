fb17d46a006c4971036e9db221e1d0991ae65626
Eliza Weisman <eliza@buoyant.io>
2020-04-22 15:06:06 -0700

    port buffer to std::future
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/buffer/src/dispatch.rs
+++ linkerd/buffer/src/dispatch.rs
@@ -1,16 +1,19 @@
 use crate::error::ServiceError;
 use crate::InFlight;
-use futures::{Async, Future, Poll, Stream};
-use linkerd2_error::{Error, Never};
-use std::sync::Arc;
+use linkerd2_error::Error;
+use pin_project::pin_project;
+use std::task::{Context, Poll};
+use std::{future::Future, pin::Pin, sync::Arc};
 use tokio::sync::{mpsc, watch};
 use tracing::trace;
 
 /// A future that drives the inner service.
+#[pin_project]
 pub struct Dispatch<S, Req, F> {
     inner: Option<S>,
+    #[pin]
     rx: mpsc::Receiver<InFlight<Req, F>>,
-    ready: watch::Sender<Poll<(), ServiceError>>,
+    ready: watch::Sender<Poll<Result<(), ServiceError>>>,
 }
 
 impl<S, Req> Dispatch<S, Req, S::Future>
@@ -23,7 +26,7 @@ where
     pub(crate) fn new(
         inner: S,
         rx: mpsc::Receiver<InFlight<Req, S::Future>>,
-        ready: watch::Sender<Poll<(), ServiceError>>,
+        ready: watch::Sender<Poll<Result<(), ServiceError>>>,
     ) -> Self {
         Self {
             inner: Some(inner),
@@ -36,7 +39,7 @@ where
 macro_rules! return_ready {
     () => {{
         trace!("Complete");
-        return Ok(Async::Ready(()));
+        return Poll::Ready(());
     }};
 }
 
@@ -55,76 +58,81 @@ where
     S::Response: Send + 'static,
     S::Future: Send + 'static,
 {
-    type Item = ();
-    type Error = Never;
+    type Output = ();
 
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+        let mut this = self.project();
         // Complete the task when all services have dropped.
-        return_ready_if!(self.ready.poll_close().expect("must not fail").is_ready());
+        return_ready_if!({
+            let closed = this.ready.closed();
+            tokio::pin!(closed);
+            closed.poll(cx).is_ready()
+        });
 
         // Drive requests from the queue to the inner service.
         loop {
-            let ready = match self.inner.as_mut() {
-                Some(inner) => inner.poll_ready(),
+            let ready = match this.inner.as_mut() {
+                Some(inner) => inner.poll_ready(cx),
                 None => {
-                    // This is safe because ready.poll_close has returned NotReady.
-                    return Ok(Async::NotReady);
+                    // This is safe because ready.poll_close has returned Pending.
+                    return Poll::Pending;
                 }
             };
 
             match ready {
                 // If it's not ready, wait for it..
-                Ok(Async::NotReady) => {
-                    return_ready_if!(self.ready.broadcast(Ok(Async::NotReady)).is_err());
+                Poll::Pending => {
+                    return_ready_if!(this.ready.broadcast(Poll::Pending).is_err());
 
                     trace!("Waiting for inner service");
-                    return Ok(Async::NotReady);
+                    return Poll::Pending;
                 }
 
                 // If the service fails, propagate the failure to all pending
                 // requests and then complete.
-                Err(error) => {
+                Poll::Ready(Err(error)) => {
                     let shared = ServiceError(Arc::new(error.into()));
                     trace!(%shared, "Inner service failed");
 
                     // First, notify services of the readiness change to prevent new requests from
                     // being buffered.
-                    let is_active = self.ready.broadcast(Err(shared.clone())).is_ok();
+                    let is_active = this
+                        .ready
+                        .broadcast(Poll::Ready(Err(shared.clone())))
+                        .is_ok();
 
                     // Propagate the error to all in-flight requests.
-                    while let Ok(Async::Ready(Some(InFlight { tx, .. }))) = self.rx.poll() {
+                    while let Poll::Ready(Some(InFlight { tx, .. })) = this.rx.poll_recv(cx) {
                         let _ = tx.send(Err(shared.clone().into()));
                     }
 
                     // Drop the inner Service to free its resources. It won't be used again.
-                    self.inner = None;
+                    let _ = this.inner.take();
 
                     // Ensure the task remains active until all services have observed the error.
                     return_ready_if!(!is_active);
 
-                    // This is safe because ready.poll_close has returned NotReady. The task will
+                    // This is safe because ready.poll_close has returned Pending. The task will
                     // complete when all observes have dropped their interest in `ready`.
-                    return Ok(Async::NotReady);
+                    return Poll::Pending;
                 }
 
                 // If inner service can receive requests, start polling the channel.
-                Ok(Async::Ready(())) => {
-                    return_ready_if!(self.ready.broadcast(Ok(Async::Ready(()))).is_err());
+                Poll::Ready(Ok(())) => {
+                    return_ready_if!(this.ready.broadcast(Poll::Ready(Ok(()))).is_err());
                     trace!("Ready for requests");
                 }
             }
 
             // The inner service is ready, so poll for new requests.
-            match self.rx.poll() {
-                Ok(Async::NotReady) => return Ok(Async::NotReady),
-
+            match futures::ready!(this.rx.poll_recv(cx)) {
                 // All senders have been dropped, complete.
-                Err(_) | Ok(Async::Ready(None)) => return_ready!(),
+                None => return_ready!(),
 
                 // If a request was ready return it to the caller.
-                Ok(Async::Ready(Some(InFlight { request, tx }))) => {
+                Some(InFlight { request, tx }) => {
                     trace!("Dispatching a request");
-                    let fut = self
+                    let fut = this
                         .inner
                         .as_mut()
                         .expect("Service must not be dropped")
