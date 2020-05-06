068737f5dd15b01225813cd02c6e3f189749108e
Eliza Weisman <eliza@buoyant.io>
2020-05-06 13:27:50 -0700

    port boxed io stuff to tokio 0.2
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/io/src/lib.rs
+++ linkerd/io/src/lib.rs
@@ -1,16 +1,16 @@
 mod boxed;
-mod peek;
-mod prefixed;
-
-pub use self::{boxed::BoxedIo, peek::Peek, prefixed::PrefixedIo};
+// mod peek;
+// mod prefixed;
+pub use self::boxed::BoxedIo;
+// pub use self::{boxed::BoxedIo, peek::Peek, prefixed::PrefixedIo};
 pub use std::io::{Error, Read, Result, Write};
 pub use tokio::io::{AsyncRead, AsyncWrite};
 
-pub type Poll<T> = std::task::Poll<Result<T, Error>>;
+pub type Poll<T> = std::task::Poll<Result<T>>;
 
 mod internal {
     use super::{AsyncRead, AsyncWrite, Poll, Result};
-    use bytes::Buf;
+    use bytes::{Buf, BufMut};
     use std::net::Shutdown;
     use std::pin::Pin;
     use std::task::Context;
@@ -23,7 +23,15 @@ mod internal {
     pub trait Io: AsyncRead + AsyncWrite + Send {
         fn shutdown_write(&mut self) -> Result<()>;
 
-        /// This method is to allow using `Async::write_buf` even through a
+        /// This method is to allow using `Async::polL_read_buf` even through a
+        /// trait object.
+        fn poll_read_buf_erased(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &mut dyn BufMut,
+        ) -> Poll<usize>;
+
+        /// This method is to allow using `Async::poll_write_buf` even through a
         /// trait object.
         fn poll_write_buf_erased(
             self: Pin<&mut Self>,
@@ -40,13 +48,21 @@ mod internal {
         fn poll_write_buf_erased(
             self: Pin<&mut Self>,
             cx: &mut Context<'_>,
-            buf: &mut dyn Buf,
+            mut buf: &mut dyn Buf,
         ) -> Poll<usize> {
-            self.write_buf(&mut buf)
+            self.poll_write_buf(cx, &mut buf)
+        }
+
+        fn poll_read_buf_erased(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            mut buf: &mut dyn BufMut,
+        ) -> Poll<usize> {
+            self.poll_read_buf(cx, &mut buf)
         }
     }
 
-    impl<S: Io> Io for tokio_rustls::server::TlsStream<S> {
+    impl<S: Io + Unpin> Io for tokio_rustls::server::TlsStream<S> {
         fn shutdown_write(&mut self) -> Result<()> {
             self.get_mut().0.shutdown_write()
         }
@@ -54,13 +70,21 @@ mod internal {
         fn poll_write_buf_erased(
             self: Pin<&mut Self>,
             cx: &mut Context<'_>,
-            buf: &mut dyn Buf,
+            mut buf: &mut dyn Buf,
         ) -> Poll<usize> {
-            self.write_buf(&mut buf)
+            self.poll_write_buf(cx, &mut buf)
+        }
+
+        fn poll_read_buf_erased(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            mut buf: &mut dyn BufMut,
+        ) -> Poll<usize> {
+            self.poll_read_buf(cx, &mut buf)
         }
     }
 
-    impl<S: Io> Io for tokio_rustls::client::TlsStream<S> {
+    impl<S: Io + Unpin> Io for tokio_rustls::client::TlsStream<S> {
         fn shutdown_write(&mut self) -> Result<()> {
             self.get_mut().0.shutdown_write()
         }
@@ -68,9 +92,17 @@ mod internal {
         fn poll_write_buf_erased(
             self: Pin<&mut Self>,
             cx: &mut Context<'_>,
-            buf: &mut dyn Buf,
+            mut buf: &mut dyn Buf,
         ) -> Poll<usize> {
-            self.write_buf(&mut buf)
+            self.poll_write_buf(cx, &mut buf)
+        }
+
+        fn poll_read_buf_erased(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            mut buf: &mut dyn BufMut,
+        ) -> Poll<usize> {
+            self.poll_read_buf(cx, &mut buf)
         }
     }
 }
