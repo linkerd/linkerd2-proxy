4673e699d901b5e2d81d543b984d7db5db0a9e68
Zahari Dichev <zaharidichev@gmail.com>
2020-05-08 19:34:49 +0300

    Add middleware that rejects connections with no identity (#507)
    Add middleware that rejects connections with no identity on specific ports
    
    Signed-off-by: Zahari Dichev <zaharidichev@gmail.com>

---
+++ linkerd/admit/src/lib.rs
@@ -0,0 +1,62 @@
+#![deny(warnings, rust_2018_idioms)]
+
+//! A middleware that fails requests by policy.
+
+use futures::{future, Future, Poll};
+use linkerd2_error::Error;
+
+pub struct AdmitLayer<A>(A);
+
+pub trait Admit<T> {
+    type Error: Into<Error>;
+
+    fn admit(&mut self, target: &T) -> Result<(), Self::Error>;
+}
+
+#[derive(Debug, Clone)]
+pub struct AdmitService<A, S> {
+    admit: A,
+    inner: S,
+}
+
+impl<A> AdmitLayer<A> {
+    pub fn new(admit: A) -> Self {
+        Self(admit)
+    }
+}
+
+impl<A: Clone, S> tower::layer::Layer<S> for AdmitLayer<A> {
+    type Service = AdmitService<A, S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        Self::Service {
+            admit: self.0.clone(),
+            inner,
+        }
+    }
+}
+
+impl<A, T, S> tower::Service<T> for AdmitService<A, S>
+where
+    A: Admit<T>,
+    S: tower::Service<T>,
+    S::Error: Into<Error>,
+{
+    type Response = S::Response;
+    type Error = Error;
+    type Future = future::Either<
+        future::MapErr<S::Future, fn(S::Error) -> Error>,
+        future::FutureResult<Self::Response, Self::Error>,
+    >;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        self.inner.poll_ready().map_err(Into::into)
+    }
+
+    fn call(&mut self, t: T) -> Self::Future {
+        match self.admit.admit(&t) {
+            Ok(()) => future::Either::A(self.inner.call(t).map_err(Into::into)),
+            Err(e) => future::Either::B(future::err(e.into())),
+        }
+    }
+}
--- linkerd/app/inbound/src/lib.rs
+++ linkerd/app/inbound/src/lib.rs
@@ -8,7 +8,9 @@
 pub use self::endpoint::{
     HttpEndpoint, Profile, ProfileTarget, RequestTarget, Target, TcpEndpoint,
 };
+use self::require_identity_for_ports::RequireIdentityForPorts;
 use futures::future;
+use linkerd2_admit as admit;
 use linkerd2_app_core::{
     classify,
     config::{ProxyConfig, ServerConfig},
@@ -36,6 +38,7 @@ use tokio::sync::mpsc;
 use tracing::{info, info_span};
 
 mod endpoint;
+mod require_identity_for_ports;
 #[allow(dead_code)] // TODO #2597
 mod set_client_id_on_req;
 #[allow(dead_code)] // TODO #2597
@@ -44,6 +47,7 @@ mod set_remote_ip_on_req;
 #[derive(Clone, Debug)]
 pub struct Config {
     pub proxy: ProxyConfig,
+    pub require_identity_for_inbound_ports: RequireIdentityForPorts,
 }
 
 pub struct Inbound {
@@ -78,6 +82,7 @@ impl Config {
                     max_in_flight_requests,
                     detect_protocol_timeout,
                 },
+            require_identity_for_inbound_ports,
         } = self;
 
         let listen = bind.bind().map_err(Error::from)?;
@@ -271,6 +276,7 @@ impl Config {
                 .push(DetectProtocolLayer::new(ProtocolDetect::new(
                     disable_protocol_detection_for_ports.clone(),
                 )))
+                .push(admit::AdmitLayer::new(require_identity_for_inbound_ports))
                 // Terminates inbound mTLS from other outbound proxies.
                 .push(tls::AcceptTls::layer(
                     local_identity,
