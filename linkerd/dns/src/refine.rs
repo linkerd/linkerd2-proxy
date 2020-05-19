2cdea1e64c8d4f103ec9137d0470c0464d436b43
Eliza Weisman <eliza@buoyant.io>
2020-05-19 11:10:34 -0700

    port `linkerd2-dns` to std::future
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/dns/src/refine.rs
+++ linkerd/dns/src/refine.rs
@@ -1,24 +1,28 @@
-use futures::{future, try_ready, Future, Poll};
+use futures::{future, ready};
+
 use linkerd2_dns_name::Name;
 use linkerd2_stack::NewService;
 use std::convert::TryFrom;
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 use std::time::Instant;
-use trust_dns_resolver::{error::ResolveError, lookup_ip::LookupIp, AsyncResolver};
+use trust_dns_resolver::{error::ResolveError, lookup_ip::LookupIp, TokioAsyncResolver};
 
 /// A `MakeService` that produces a `Refine` for a given name.
 #[derive(Clone)]
-pub struct MakeRefine(pub(super) AsyncResolver);
+pub struct MakeRefine(pub(super) TokioAsyncResolver);
 
 /// A `Service` that produces the most recent result if one is known.
 pub struct Refine {
-    resolver: AsyncResolver,
+    resolver: TokioAsyncResolver,
     name: Name,
     state: State,
 }
 
 enum State {
     Init,
-    Pending(Box<dyn Future<Item = LookupIp, Error = ResolveError> + Send + 'static>),
+    Pending(Pin<Box<dyn Future<Output = Result<LookupIp, ResolveError>> + Send + 'static>>),
     Refined { name: Name, valid_until: Instant },
 }
 
@@ -40,17 +44,20 @@ impl NewService<Name> for MakeRefine {
 impl tower::Service<()> for Refine {
     type Response = Name;
     type Error = RefineError;
-    type Future = future::FutureResult<Self::Response, Self::Error>;
+    type Future = future::Ready<Result<Self::Response, Self::Error>>;
 
-    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         loop {
             self.state = match self.state {
                 State::Init => {
-                    let fut = self.resolver.lookup_ip(self.name.as_ref());
-                    State::Pending(Box::new(fut))
+                    let resolver = self.resolver.clone();
+                    let name = self.name.clone();
+                    State::Pending(Box::pin(
+                        async move { resolver.lookup_ip(name.as_ref()).await },
+                    ))
                 }
                 State::Pending(ref mut fut) => {
-                    let lookup = try_ready!(fut.poll().map_err(RefineError));
+                    let lookup = ready!(fut.as_mut().poll(cx).map_err(RefineError))?;
                     let valid_until = lookup.valid_until();
                     let n = lookup.query().name();
                     let name = Name::try_from(n.to_ascii().as_bytes())
@@ -59,7 +66,7 @@ impl tower::Service<()> for Refine {
                 }
                 State::Refined { valid_until, .. } => {
                     if Instant::now() < valid_until {
-                        return Ok(().into());
+                        return Poll::Ready(Ok(()));
                     }
                     State::Init
                 }
