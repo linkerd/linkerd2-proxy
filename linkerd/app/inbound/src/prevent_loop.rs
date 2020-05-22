cb161a13d504b963f10ed386d08352b17b4fe501
Oliver Gould <ver@buoyant.io>
2020-05-22 16:17:05 -0700

    Add loop detection to inbound & TCP forwarding (#527)
    e77fe181 introduced loop detection to the outbound HTTP proxy. This
    change extends this behavior to the inbound HTTP proxy and the TCP
    proxy for both inbound and outbound. This helps ensure malicious
    requests can't consume proxy resources.

--- linkerd/app/inbound/src/prevent_loop.rs
+++ linkerd/app/inbound/src/prevent_loop.rs
@@ -1,4 +1,4 @@
-use super::endpoint::{HttpEndpoint, Target, TcpEndpoint};
+use super::endpoint::{Target, TcpEndpoint};
 use linkerd2_app_core::admit;
 
 /// A connection policy that drops
@@ -18,12 +18,12 @@ impl PreventLoop {
     }
 }
 
-impl admit::Admit<Target<HttpEndpoint>> for PreventLoop {
+impl admit::Admit<Target> for PreventLoop {
     type Error = LoopPrevented;
 
-    fn admit(&mut self, ep: &Target<HttpEndpoint>) -> Result<(), Self::Error> {
-        tracing::debug!(addr = %ep.inner.addr, self.port);
-        if ep.inner.addr.ip().is_loopback() && ep.inner.addr.port() == self.port {
+    fn admit(&mut self, ep: &Target) -> Result<(), Self::Error> {
+        tracing::debug!(addr = %ep.addr, self.port);
+        if ep.addr.port() == self.port {
             return Err(LoopPrevented { port: self.port });
         }
 
@@ -35,8 +35,8 @@ impl admit::Admit<TcpEndpoint> for PreventLoop {
     type Error = LoopPrevented;
 
     fn admit(&mut self, ep: &TcpEndpoint) -> Result<(), Self::Error> {
-        tracing::debug!(addr = %ep.addr, self.port);
-        if ep.addr.ip().is_loopback() && ep.addr.port() == self.port {
+        tracing::debug!(port = %ep.port, self.port);
+        if ep.port == self.port {
             return Err(LoopPrevented { port: self.port });
         }
 
@@ -48,7 +48,7 @@ impl std::fmt::Display for LoopPrevented {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "outbound requests must not target localhost:{}",
+            "inbound requests must not target localhost:{}",
             self.port
         )
     }
