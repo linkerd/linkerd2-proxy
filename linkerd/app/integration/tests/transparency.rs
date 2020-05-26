5b35268ba76179486ef2cf8b46742b058fc2e118
Eliza Weisman <eliza@buoyant.io>
2020-05-26 11:11:16 -0700

    disable test that depends on transport metrics
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/app/integration/tests/transparency.rs
+++ linkerd/app/integration/tests/transparency.rs
@@ -1089,6 +1089,8 @@ fn http1_requests_without_host_have_unique_connections() {
 }
 
 #[tokio::test]
+// Re-enable when transport metrics are put back.
+#[cfg_attr(not(feature = "nyi"), ignore)]
 async fn retry_reconnect_errors() {
     let _ = trace_init();
 
