fb82cd23462acc89d50c955e90a302e393e8cdbf
Eliza Weisman <eliza@buoyant.io>
2020-06-11 16:08:27 -0700

    can't believe this is a thing we have to do
    but, whatever
    
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd2-proxy/src/main.rs
+++ linkerd2-proxy/src/main.rs
@@ -2,7 +2,7 @@
 
 #![deny(warnings, rust_2018_idioms)]
 #![recursion_limit = "256"]
-#![type_length_limit = "16287993"]
+#![type_length_limit = "16289823"]
 
 use futures::compat::Future01CompatExt;
 use linkerd2_app::{trace, Config};
