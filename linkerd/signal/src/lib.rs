66aa812a713a1dbc241582527fc294d74650e818
Oliver Gould <ver@buoyant.io>
2020-06-15 10:29:15 -0700

    Port signal to async/await (#564)

--- linkerd/signal/src/lib.rs
+++ linkerd/signal/src/lib.rs
@@ -2,100 +2,56 @@
 
 #![deny(warnings, rust_2018_idioms)]
 
-use futures::Future;
-
-type ShutdownSignal = Box<dyn Future<Item = (), Error = ()> + Send>;
-
 /// Returns a `Future` that completes when the proxy should start to shutdown.
-pub fn shutdown() -> ShutdownSignal {
-    imp::shutdown()
+pub async fn shutdown() {
+    imp::shutdown().await
 }
 
 #[cfg(unix)]
 mod imp {
-    use super::ShutdownSignal;
-    use futures::{future, Future, Stream};
-    use std::fmt;
-    use tokio_signal::unix::{Signal, SIGINT, SIGTERM};
+    use tokio::signal::unix::{signal, SignalKind};
     use tracing::info;
 
-    pub(super) fn shutdown() -> ShutdownSignal {
-        // SIGTERM - Kubernetes sends this to start a graceful shutdown.
-        // SIGINT  - To allow Ctrl-c to emulate SIGTERM while developing.
-        //
-        // If you add to this list, you should also update DisplaySignal below
-        // to output nicer signal names.
-        let on_any_signal = future::lazy(|| {
-            // Do everything lazily to prevent `Signal::new()` from calling
-            // `Handle::default()` and starting the background reactor when
-            // we haven't initialized the runtime yet.
-            Ok([SIGINT, SIGTERM].iter())
-        })
-        .and_then(|signals| {
-            let signals = signals.map(|&sig| {
-                // Create a Future that completes the first
-                // time the process receives 'sig'.
-                Signal::new(sig)
-                    .flatten_stream()
-                    .into_future()
-                    .map(move |_| {
-                        info!(
-                            // use target to remove 'imp' from output
-                            target: "linkerd2_proxy::signal",
-                            "received {}, starting shutdown",
-                            DisplaySignal(sig),
-                        );
-                    })
-            });
-            // With a list of Futures that could notify us,
-            // we just want to know when the first one triggers,
-            // so select over them all.
-            future::select_all(signals)
-                .map(|_| ())
-                .map_err(|_| unreachable!("Signal never returns an error"))
-        });
-
-        Box::new(on_any_signal)
+    pub(super) async fn shutdown() {
+        tokio::select! {
+            // SIGINT  - To allow Ctrl-c to emulate SIGTERM while developing.
+            () = sig(SignalKind::interrupt(), "SIGINT") => {}
+            // SIGTERM - Kubernetes sends this to start a graceful shutdown.
+            () = sig(SignalKind::terminate(), "SIGTERM") => {}
+        };
     }
 
-    struct DisplaySignal(i32);
-
-    impl fmt::Display for DisplaySignal {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            let s = match self.0 {
-                SIGINT => "SIGINT",
-                SIGTERM => "SIGTERM",
-                other => return write!(f, "signal {}", other),
-            };
-            f.write_str(s)
-        }
+    async fn sig(kind: SignalKind, name: &'static str) {
+        // Create a Future that completes the first
+        // time the process receives 'sig'.
+        signal(kind)
+            .expect("Failed to register signal handler")
+            .recv()
+            .await;
+        info!(
+            // use target to remove 'imp' from output
+            target: "linkerd2_proxy::signal",
+            "received {}, starting shutdown",
+            name,
+        );
     }
 }
 
 #[cfg(not(unix))]
 mod imp {
-    use super::ShutdownSignal;
-    use futures::{Future, Stream};
-    use tokio_signal;
+    use futures::prelude::*;
     use tracing::info;
 
-    pub(super) fn shutdown() -> ShutdownSignal {
+    pub(super) async fn shutdown() {
         // On Windows, we don't have all the signals, but Windows also
         // isn't our expected deployment target. This implementation allows
         // developers on Windows to simulate proxy graceful shutdown
         // by pressing Ctrl-C.
-        let on_ctrl_c = tokio_signal::ctrl_c()
-            .flatten_stream()
-            .into_future()
-            .map(|_| {
-                info!(
-                    // use target to remove 'imp' from output
-                    target: "linkerd2_proxy::signal",
-                    "received Ctrl-C, starting shutdown",
-                );
-            })
-            .map_err(|_| unreachable!("ctrl_c never returns errors"));
-
-        Box::new(on_ctrl_c)
+        tokio::signal::ctrl_c().recv().await;
+        info!(
+            // use target to remove 'imp' from output
+            target: "linkerd2_proxy::signal",
+            "received Ctrl-C, starting shutdown",
+        );
     }
 }
--- linkerd2-proxy/src/main.rs
+++ linkerd2-proxy/src/main.rs
@@ -4,7 +4,6 @@
 #![recursion_limit = "256"]
 #![type_length_limit = "16289823"]
 
-use futures::compat::Future01CompatExt;
 use linkerd2_app::{trace, Config};
 use linkerd2_signal as signal;
 pub use tracing::{debug, error, info, warn};
@@ -73,7 +72,7 @@ fn main() {
             }
 
             let drain = app.spawn();
-            signal::shutdown().compat().await.expect("shutdown");
+            signal::shutdown().await;
             drain.drain().await;
         });
 }
