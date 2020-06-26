821264a6e384b476d71b6853ec76e7ef87e9814b
Eliza Weisman <eliza@buoyant.io>
2020-06-26 10:38:43 -0700

    Revert "you know what, screw it, part 2: the part i forgot"
    This reverts commit d4bad3d2ebe0dbf5edaa0354a8a387692efa9cbf.

--- linkerd/app/integration/src/proxy.rs
+++ linkerd/app/integration/src/proxy.rs
@@ -141,16 +141,16 @@ impl Proxy {
         self
     }
 
-    pub fn run(self) -> Listening {
-        self.run_with_test_env(TestEnv::new())
+    pub async fn run(self) -> Listening {
+        self.run_with_test_env(TestEnv::new()).await
     }
 
-    pub fn run_with_test_env(self, env: TestEnv) -> Listening {
-        run(self, env, true)
+    pub async fn run_with_test_env(self, env: TestEnv) -> Listening {
+        run(self, env, true).await
     }
 
-    pub fn run_with_test_env_and_keep_ports(self, env: TestEnv) -> Listening {
-        run(self, env, false)
+    pub async fn run_with_test_env_and_keep_ports(self, env: TestEnv) -> Listening {
+        run(self, env, false).await
     }
 }
 
@@ -195,14 +195,14 @@ impl Listening {
     }
 }
 
-fn run(proxy: Proxy, mut env: TestEnv, random_ports: bool) -> Listening {
+async fn run(proxy: Proxy, mut env: TestEnv, random_ports: bool) -> Listening {
     use app::env::Strings;
 
     let controller = if let Some(controller) = proxy.controller {
         controller
     } else {
         // bummer that the whole function needs to be async just for this...
-        controller::new().run()
+        controller::new().run().await
     };
     let inbound = proxy.inbound;
     let outbound = proxy.outbound;
