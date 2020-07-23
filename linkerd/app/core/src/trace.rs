32051a4728d95e7de5e219a96be77937a8bdeb1e
Eliza Weisman <eliza@buoyant.io>
2020-07-23 11:49:12 -0700

    whoopsy
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/app/core/src/trace.rs
+++ linkerd/app/core/src/trace.rs
@@ -64,11 +64,7 @@ pub fn with_filter_and_format(
     let (dispatch, level, tasks) = match format.as_ref().to_uppercase().as_ref() {
         "JSON" => {
             let (tasks, tasks_layer) = TasksLayer::<format::JsonFields>::new();
-            let builder = builder
-                .builder
-                .json()
-                .with_span_list(true)
-                .with_filter_reloading();
+            let builder = builder.json().with_span_list(true).with_filter_reloading();
             let handle = LevelHandle::Json(builder.reload_handle());
             let dispatch = Dispatch::new(builder.finish().with(tasks_layer));
             (dispatch, handle, tasks)
