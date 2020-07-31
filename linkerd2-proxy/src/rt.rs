935b65d1203acce9515014fbb6785825d300bcfd
Eliza Weisman <eliza@buoyant.io>
2020-07-30 22:44:38 -0700

    Add a `multicore` feature flag (#611)
    This branch enables feature-flagged support for using Tokio's
    multithreaded runtime when the proxy is given more than two CPU cores to
    run on. The proxy's understanding of the system's number of CPUs
    reflects cgroups limits, so docker CPU limits are taken into
    consideration.
    
    If only a single core is available to run a worker on, we will construct
    Tokio's basic_scheduler, rather than a threaded_scheduler with a
    single worker thread. This is because there is some overhead introduced
    by the threaded scheduler, which results in worse performance when only
    1 core is available.
    
    Finally, a bug in the handle time metrics layer results in a deadlock
    when used with the multithreaded runtime. Therefore, I've disabled this
    metric for now --- my understanding is that nothing consumes it at the
    moment. If it's needed, we can fix the bug and put it back.

---
+++ linkerd2-proxy/src/rt.rs
@@ -0,0 +1,31 @@
+use tokio::runtime::{self, Runtime};
+
+#[cfg(feature = "multicore")]
+pub(crate) fn build() -> Runtime {
+    let builder = runtime::Builder::new()
+        .enable_all()
+        .thread_name("linkerd2-proxy-worker");
+    let num_cpus = num_cpus::get();
+    if num_cpus > 2 {
+        builder
+            .threaded_scheduler()
+            .core_threads(num_cpus - 1) // Save 1 core for the admin runtime.
+            .build()
+            .expect("failed to build multithreaded runtime!")
+    } else {
+        builder
+            .basic_scheduler()
+            .build()
+            .expect("failed to build single-threaded runtime!")
+    }
+}
+
+#[cfg(not(feature = "multicore"))]
+pub(crate) fn build() -> Runtime {
+    runtime::Builder::new()
+        .enable_all()
+        .thread_name("linkerd2-proxy-worker")
+        .basic_scheduler()
+        .build()
+        .expect("failed to build single-threaded runtime!")
+}
