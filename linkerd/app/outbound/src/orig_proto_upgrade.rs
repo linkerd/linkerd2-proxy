b97909fdb9d79ce18848a8fe699aa516bfc5cbf5
Oliver Gould <ver@buoyant.io>
2020-09-08 10:32:37 -0700

    proxy-http: Remove unneeded boilerplate (#651)
    There's a lot of needless boilerplate around the HTTP client, mostly due
    to manual future implementations. These have been converted to boxed
    futures.
    
    Furthermore, the HasSettings trait has been eliminated, in favor of
    AsRef.

--- linkerd/app/outbound/src/orig_proto_upgrade.rs
+++ linkerd/app/outbound/src/orig_proto_upgrade.rs
@@ -1,7 +1,4 @@
-use crate::proxy::http::{
-    orig_proto,
-    settings::{HasSettings, Settings},
-};
+use crate::proxy::http::{orig_proto, settings::Settings};
 use crate::svc::stack;
 use crate::{HttpEndpoint, Target};
 use futures::{ready, TryFuture};
@@ -56,7 +53,7 @@ where
             return Either::B(self.inner.new_service(endpoint));
         }
 
-        let was_absolute = endpoint.http_settings().was_absolute_form();
+        let was_absolute = endpoint.inner.settings.was_absolute_form();
         trace!(
             header = %orig_proto::L5D_ORIG_PROTO,
             was_absolute,
@@ -84,7 +81,7 @@ where
     fn call(&mut self, mut endpoint: Target<HttpEndpoint>) -> Self::Future {
         let can_upgrade = endpoint.inner.can_use_orig_proto();
 
-        let was_absolute = endpoint.http_settings().was_absolute_form();
+        let was_absolute = endpoint.inner.settings.was_absolute_form();
 
         if can_upgrade {
             trace!(
