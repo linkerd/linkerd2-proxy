eefbd26b497a62cab734a47fb8d00c233e326e44
Eliza Weisman <eliza@buoyant.io>
2020-09-16 10:17:07 -0700

    Update linkerd/app/test/src/lib.rs
    Co-authored-by: Oliver Gould <ver@buoyant.io>

--- linkerd/app/test/src/lib.rs
+++ linkerd/app/test/src/lib.rs
@@ -1,8 +1,6 @@
 //! Shared infrastructure for integration tests
 
 #![deny(warnings, rust_2018_idioms)]
-#![recursion_limit = "256"]
-#![type_length_limit = "16289823"]
 
 pub use futures::{future, FutureExt, TryFuture, TryFutureExt};
 
