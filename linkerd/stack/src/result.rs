5d173db52a714576c43f53ea983ff22a9f664738
Oliver Gould <ver@buoyant.io>
2020-09-23 23:23:26 +0000

    Just use Result<S, Option<E>>

--- linkerd/stack/src/result.rs
+++ linkerd/stack/src/result.rs
@@ -3,21 +3,15 @@ use linkerd2_error::Error;
 use std::task::{Context, Poll};
 
 #[derive(Copy, Clone, Debug)]
-pub struct ResultService<S, E>(Inner<S, E>);
-
-#[derive(Copy, Clone, Debug)]
-enum Inner<S, E> {
-    Ok(S),
-    Err(Option<E>),
-}
+pub struct ResultService<S, E>(Result<S, Option<E>>);
 
 impl<S, E> ResultService<S, E> {
     pub fn ok(svc: S) -> Self {
-        ResultService(Inner::Ok(svc))
+        ResultService(Ok(svc))
     }
 
     pub fn err(err: E) -> Self {
-        ResultService(Inner::Err(Some(err)))
+        ResultService(Err(Some(err)))
     }
 }
 
@@ -42,16 +36,16 @@ where
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
         let res = match self.0 {
-            Inner::Ok(ref mut svc) => ready!(svc.poll_ready(cx)).map_err(Into::into),
-            Inner::Err(ref mut err) => Err(err.take().expect("polled after failure").into()),
+            Ok(ref mut svc) => ready!(svc.poll_ready(cx)).map_err(Into::into),
+            Err(ref mut err) => Err(err.take().expect("polled after failure").into()),
         };
         Poll::Ready(res)
     }
 
     fn call(&mut self, t: T) -> Self::Future {
         match self.0 {
-            Inner::Ok(ref mut svc) => svc.call(t).err_into::<Error>(),
-            Inner::Err(_) => panic!("poll_ready not called"),
+            Ok(ref mut svc) => svc.call(t).err_into::<Error>(),
+            Err(_) => panic!("poll_ready not called"),
         }
     }
 }
