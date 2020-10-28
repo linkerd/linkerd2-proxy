6bfbdf351ce59e77e200d7c5f4ca8fa8da10536b
Oliver Gould <ver@buoyant.io>
2020-10-28 21:58:04 +0000

    buffer: Remove readiness propagation

--- linkerd/buffer/src/service.rs
+++ linkerd/buffer/src/service.rs
@@ -1,49 +1,20 @@
-use crate::error::{Closed, ServiceError};
+use crate::error::Closed;
 use crate::InFlight;
 use linkerd2_error::Error;
 use std::task::{Context, Poll};
 use std::{future::Future, pin::Pin};
-use tokio::sync::{mpsc, oneshot, watch};
+use tokio::sync::{mpsc, oneshot};
 
 pub struct Buffer<Req, Rsp> {
-    /// Updates with the readiness state of the inner service. This allows the buffer to reliably
-    /// exert backpressure and propagate errors, especially before the inner service has been
-    /// initialized.
-    ready: watch::Receiver<Poll<Result<(), ServiceError>>>,
-
     /// The queue on which in-flight requests are sent to the inner service.
-    ///
-    /// Because the inner service's status is propagated via `ready` watch, this is here to
-    /// allow multiple services race to send a request.
     tx: mpsc::Sender<InFlight<Req, Rsp>>,
 }
 
 // === impl Buffer ===
 
 impl<Req, Rsp> Buffer<Req, Rsp> {
-    pub(crate) fn new(
-        tx: mpsc::Sender<InFlight<Req, Rsp>>,
-        ready: watch::Receiver<Poll<Result<(), ServiceError>>>,
-    ) -> Self {
-        Self { tx, ready }
-    }
-
-    /// Get the inner service's state, ensuring the Service is registered to receive updates.
-    fn poll_inner(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        // Drive the watch to NotReady to ensure we are registered to be notified.
-        loop {
-            match self.ready.poll_recv_ref(cx) {
-                Poll::Pending => break,
-                Poll::Ready(Some(ready)) => match &*ready {
-                    Poll::Ready(Err(err)) => return Poll::Ready(Err(err.clone().into())),
-                    _ => {} // continue;
-                },
-                Poll::Ready(None) => return Poll::Ready(Err(Closed(()).into())),
-            }
-        }
-
-        // Get the most recent state.
-        self.ready.borrow().clone().map_err(Into::into)
+    pub(crate) fn new(tx: mpsc::Sender<InFlight<Req, Rsp>>) -> Self {
+        Self { tx }
     }
 }
 
@@ -56,10 +27,7 @@ where
     type Future = Pin<Box<dyn Future<Output = Result<Rsp, Error>> + Send + 'static>>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        match self.poll_inner(cx) {
-            Poll::Ready(Ok(())) => self.tx.poll_ready(cx).map_err(|_| Closed(()).into()),
-            ready => ready,
-        }
+        self.tx.poll_ready(cx).map_err(|_| Closed(()).into())
     }
 
     fn call(&mut self, request: Req) -> Self::Future {
@@ -74,6 +42,6 @@ where
 
 impl<Req, Rsp> Clone for Buffer<Req, Rsp> {
     fn clone(&self) -> Self {
-        Self::new(self.tx.clone(), self.ready.clone())
+        Self::new(self.tx.clone())
     }
 }
