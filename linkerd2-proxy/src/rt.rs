c8e447370ed81ef07c1a585a6bb543db919de3c7
Eliza Weisman <eliza@buoyant.io>
2020-10-30 15:58:46 -0700

    use new rt builder
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd2-proxy/src/rt.rs
+++ linkerd2-proxy/src/rt.rs
@@ -7,17 +7,15 @@ pub(crate) fn build() -> Runtime {
     // is used when the threaded scheduler would provide no benefit.
     match num_cpus::get() {
         // `0` is unexpected, but it's a wild world out there.
-        0 | 1 => Builder::new()
+        0 | 1 => Builder::new_current_thread()
             .enable_all()
             .thread_name("proxy")
-            .basic_scheduler()
             .build()
             .expect("failed to build basic runtime!"),
-        num_cpus => Builder::new()
+        num_cpus => Builder::new_multi_thread()
             .enable_all()
             .thread_name("proxy")
-            .threaded_scheduler()
-            .core_threads(num_cpus)
+            .worker_threads(num_cpus)
             .max_threads(num_cpus)
             .build()
             .expect("failed to build threaded runtime!"),
