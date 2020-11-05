c7c3a7e4e69d6bf6230c02a6590fbd8077b1d0a4
Eliza Weisman <eliza@buoyant.io>
2020-11-05 13:43:39 -0800

    put back set_keepalive
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/proxy/transport/src/lib.rs
+++ linkerd/proxy/transport/src/lib.rs
@@ -27,10 +27,23 @@ fn set_nodelay_or_warn(socket: &TcpStream) {
 }
 
 fn set_keepalive_or_warn(tcp: &TcpStream, ka: Option<Duration>) {
-    // TODO(eliza): we now have to do "socket2 nonsense" here because tokio
+    // XXX(eliza): we now have to do "socket2 nonsense" here because tokio
     // doesn't have `set_keepalive` any more...
-    let _ = (tcp, ka);
-    // if let Err(e) = tcp.set_keepalive(ka) {
-    //     tracing::warn!("failed to set keepalive: {}", e);
-    // }
+    #[cfg(unix)]
+    let sock = unsafe {
+        use std::os::unix::io::{AsRawFd, FromRawFd};
+        socket2::Socket::from_raw_fd(tcp.as_raw_fd())
+    };
+    #[cfg(windows)]
+    let sock = unsafe {
+        use std::os::unix::io::{AsRawSocket, FromRawSocket};
+        socket2::Socket::from_raw_socket(tcp.as_raw_socket())
+    };
+
+    if let Err(e) = sock.set_keepalive(ka) {
+        tracing::warn!("failed to set keepalive: {}", e);
+    }
+
+    // Don't let the socket2 socket close the fd on drop!
+    std::mem::forget(sock);
 }
