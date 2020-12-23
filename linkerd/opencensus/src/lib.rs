9bfceb90b49ae16825c640c24c3831874d807246
Oliver Gould <ver@buoyant.io>
2020-12-23 07:28:20 +0000

    Flush traces after idle timeout
    Adds a 10s idle timeout so that traces are flushed when traffic stops.
    
    Increases the span buffer to 1000 spans and minimizes the initial
    allocation so that the buffer grows with use.
    
    Improves opencensus factoring by splitting more methods.

--- linkerd/opencensus/src/lib.rs
+++ linkerd/opencensus/src/lib.rs
@@ -1,4 +1,6 @@
 #![deny(warnings, rust_2018_idioms)]
+
+use futures::FutureExt;
 use http_body::Body as HttpBody;
 use linkerd2_error::Error;
 use metrics::Registry;
@@ -12,6 +14,7 @@ use std::task::{Context, Poll};
 use tokio::{
     stream::{Stream, StreamExt},
     sync::mpsc,
+    time,
 };
 use tonic::{self as grpc, body::BoxBody, client::GrpcService};
 use tracing::{debug, trace};
@@ -46,7 +49,8 @@ where
     T::ResponseBody: 'static,
     S: Stream<Item = Span> + Unpin,
 {
-    const MAX_BATCH_SIZE: usize = 100;
+    const MAX_BATCH_SIZE: usize = 1000;
+    const MAX_BATCH_IDLE: time::Duration = time::Duration::from_secs(10);
 
     fn new(client: T, node: Node, spans: S, metrics: Registry) -> Self {
         Self {
@@ -58,37 +62,18 @@ where
     }
 
     async fn run(mut self) {
-        let mut spans = Vec::with_capacity(Self::MAX_BATCH_SIZE);
+        // Holds the batch of pending spans. Cleared as the spans are flushed.
+        // Contains no more than MAX_BATCH_SIZE spans.
+        let mut spans = Vec::new();
+
         let mut svc = TraceServiceClient::new(self.client.clone());
         loop {
-            let (request_tx, request_rx) = mpsc::channel(1);
-            let req = grpc::Request::new(request_rx);
             trace!("Establishing new TraceService::export request");
-            match svc.export(req).await {
+            let (tx, rx) = mpsc::channel(1);
+            match svc.export(grpc::Request::new(rx)).await {
                 Ok(_) => {
-                    self.metrics.start_stream();
-
-                    // The node is included on only the first message of every
-                    // stream.
-                    let mut node = Some(self.node.clone());
-
-                    // Send batches as long as the gRPC channel is open.
-                    while let Ok(tx) = request_tx.reserve().await {
-                        // Collect spans and then send them.
-                        let collect = self.collect_batch(&mut spans).await;
-                        if !spans.is_empty() {
-                            let msg = ExportTraceServiceRequest {
-                                spans: spans.drain(..).collect(),
-                                node: node.take(),
-                                ..Default::default()
-                            };
-                            trace!(?msg, "Sending");
-                            tx.send(msg);
-                        }
-                        if collect.is_err() {
-                            debug!("Span channel lost");
-                            return;
-                        }
+                    if self.stream(tx, &mut spans).await.is_err() {
+                        return;
                     }
                 }
                 Err(error) => {
@@ -98,13 +83,79 @@ where
         }
     }
 
+    async fn stream(
+        &mut self,
+        tx: mpsc::Sender<ExportTraceServiceRequest>,
+        spans: &mut Vec<Span>,
+    ) -> Result<(), ()> {
+        self.metrics.start_stream();
+
+        // The node is included on only the first message of every
+        // stream.
+        let mut node = Some(self.node.clone());
+
+        loop {
+            // Collect spans into a batch.
+            let collect = self.collect_batch(spans).await;
+
+            // If we collected spans, flush them.
+            if !spans.is_empty() {
+                // Once a batch has been accumulated, ensure that the
+                // request stream is ready to accept the batch.
+                match tx.reserve().await {
+                    Ok(tx) => {
+                        let msg = ExportTraceServiceRequest {
+                            spans: spans.drain(..).collect(),
+                            node: node.take(),
+                            ..Default::default()
+                        };
+                        trace!(
+                            node = msg.node.is_some(),
+                            spans = msg.spans.len(),
+                            "Sending batch"
+                        );
+                        tx.send(msg);
+                    }
+                    Err(error) => {
+                        // If the channel isn't open, start a new stream
+                        // and retry sending the batch.
+                        debug!(%error, "Request stream lost; restarting");
+                        return Ok(());
+                    }
+                }
+            }
+
+            // The span source was closed, so end the task.
+            if collect.is_err() {
+                debug!("Span channel lost");
+                return Err(());
+            }
+        }
+    }
+
     async fn collect_batch(&mut self, spans: &mut Vec<Span>) -> Result<(), ()> {
         loop {
-            let s = self.spans.next().await.ok_or(())?;
-            spans.push(s);
             if spans.len() == Self::MAX_BATCH_SIZE {
                 trace!(capacity = Self::MAX_BATCH_SIZE, "Batch capacity reached");
                 return Ok(());
+            }
+
+            futures::select_biased! {
+                res = self.spans.next().fuse() => match res {
+                    Some(span) => {
+                        trace!(?span, "Adding to batch");
+                        spans.push(span);
+                    }
+                    None => return Err(()),
+                },
+                // Don't hold spans indefinitely. Return if we hit an idle
+                // timeout and spans have been collected.
+                _ = time::sleep(Self::MAX_BATCH_IDLE).fuse() => {
+                    if !spans.is_empty() {
+                        trace!(spans = spans.len(), "Flushing spans due to inactivitiy");
+                        return Ok(());
+                    }
+                }
             }
         }
     }
