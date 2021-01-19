1e98a5b15b8d7a0832e8f699d001bf2f7bfa6be1
Oliver Gould <ver@buoyant.io>
2021-01-19 14:31:45 -0800

    Annotate socket-level errors with a scope (#852)
    We frequently encounter socket errors like:
    
        Connection closed error=Transport endpoint is not connected (os error 107)
    
    While the handler is generally scoped with tracing context to tell us
    information about the server-side connection, we lack information about
    which socket actually failed. For every "logical" TCP stream, we
    generally have two concrete connections: One on the server-side of the
    proxy and another on the client-side.
    
    This change introduces a new `io::ScopedIo` wrapper type that annotates
    all I/O error messages with a `client` or `server` prefix. Then, the
    proxy's clients and servers are wrapped with this type so that all
    socket-level error messages are disambiguated.

---
+++ linkerd/io/src/scoped.rs
@@ -0,0 +1,132 @@
+use crate as io;
+use pin_project::pin_project;
+use std::{pin::Pin, task::Context};
+
+/// An I/O stream where errors are annotated a scope.
+#[pin_project]
+#[derive(Debug)]
+pub struct ScopedIo<I> {
+    scope: Scope,
+
+    #[pin]
+    io: I,
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Scope {
+    Client,
+    Server,
+}
+
+// === impl Scope ===
+
+impl Scope {
+    #[inline]
+    fn err(&self) -> impl Fn(io::Error) -> io::Error {
+        let scope = *self;
+        move |err| io::Error::new(err.kind(), format!("{}: {}", scope, err))
+    }
+}
+
+impl std::fmt::Display for Scope {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Client => write!(f, "client"),
+            Self::Server => write!(f, "server"),
+        }
+    }
+}
+
+// === impl ScopedIo ===
+
+impl<I> ScopedIo<I> {
+    pub fn client(io: I) -> Self {
+        Self {
+            scope: Scope::Client,
+            io,
+        }
+    }
+
+    pub fn server(io: I) -> Self {
+        Self {
+            scope: Scope::Server,
+            io,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl<I: io::Peek + Send + Sync> io::Peek for ScopedIo<I> {
+    async fn peek(&self, buf: &mut [u8]) -> io::Result<usize> {
+        self.io.peek(buf).await.map_err(self.scope.err())
+    }
+}
+
+impl<I: io::PeerAddr> io::PeerAddr for ScopedIo<I> {
+    #[inline]
+    fn peer_addr(&self) -> io::Result<std::net::SocketAddr> {
+        self.io.peer_addr().map_err(self.scope.err())
+    }
+}
+
+impl<I: io::AsyncRead> io::AsyncRead for ScopedIo<I> {
+    #[inline]
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut io::ReadBuf<'_>,
+    ) -> io::Poll<()> {
+        let this = self.project();
+        this.io.poll_read(cx, buf).map_err(this.scope.err())
+    }
+}
+
+impl<I: io::Write> io::Write for ScopedIo<I> {
+    #[inline]
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.io.write(buf).map_err(self.scope.err())
+    }
+
+    #[inline]
+    fn flush(&mut self) -> io::Result<()> {
+        self.io.flush().map_err(self.scope.err())
+    }
+}
+
+impl<I: io::AsyncWrite> io::AsyncWrite for ScopedIo<I> {
+    #[inline]
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> io::Poll<()> {
+        let this = self.project();
+        this.io.poll_shutdown(cx).map_err(this.scope.err())
+    }
+
+    #[inline]
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> io::Poll<()> {
+        let this = self.project();
+        this.io.poll_flush(cx).map_err(this.scope.err())
+    }
+
+    #[inline]
+    fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> io::Poll<usize> {
+        let this = self.project();
+        let scope = this.scope;
+        this.io.poll_write(cx, buf).map_err(scope.err())
+    }
+
+    #[inline]
+    fn poll_write_vectored(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        bufs: &[io::IoSlice<'_>],
+    ) -> io::Poll<usize> {
+        let this = self.project();
+        this.io
+            .poll_write_vectored(cx, bufs)
+            .map_err(this.scope.err())
+    }
+
+    #[inline]
+    fn is_write_vectored(&self) -> bool {
+        self.io.is_write_vectored()
+    }
+}
