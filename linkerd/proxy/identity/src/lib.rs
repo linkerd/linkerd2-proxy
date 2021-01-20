07a6f44ae047c4ef7635701919ac3c1c4f65ea6a
Oliver Gould <ver@buoyant.io>
2021-01-20 19:01:36 +0000

    Remove commented imports

--- linkerd/proxy/identity/src/lib.rs
+++ linkerd/proxy/identity/src/lib.rs
@@ -4,4 +4,3 @@ pub mod certify;
 pub mod metrics;
 
 pub use self::certify::{AwaitCrt, CrtKeySender, Local};
-// XXX pub use linkerd_identity::{Crt, CrtKey, Csr, InvalidName, Key, Name, TokenSource, TrustAnchors};
