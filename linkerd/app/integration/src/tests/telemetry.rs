b1deff3062c6623909b853e929dd4d29b1e9ed5f
Eliza Weisman <eliza@buoyant.io>
2021-01-23 09:47:38 -0800

    punt on path labels for now
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/app/integration/src/tests/telemetry.rs
+++ linkerd/app/integration/src/tests/telemetry.rs
@@ -194,24 +194,15 @@ impl TcpFixture {
 async fn metrics_endpoint_admin_request_count() {
     let fixture = Fixture::inbound().await;
     let metrics = fixture.metrics;
-    let mut metric = metrics::metric("request_total")
+    let metric = metrics::metric("request_total")
         .label("direction", "inbound")
         .label("tls", "disabled")
         .label("target_addr", metrics.target_addr())
-        .label("path", "/ready")
         .value(1usize);
 
-    assert!(metric.is_not_in(metrics.get("/metrics").await));
-
-    info!("GET /ready");
-    metrics.get("/ready").await;
-
+    // We can't assert that the metric is not present, since `GET /metrics`
+    // will bump the request count, lol
     metric.assert_in(&metrics).await;
-
-    info!("GET /ready");
-    metrics.get("/ready").await;
-
-    metric.set_value(2usize).assert_in(&metrics).await;
 }
 
 #[tokio::test]
