5f3dbefc4e395f6f2e63985f9fb8a22438afb5e4
Oliver Gould <ver@buoyant.io>
2021-03-01 17:17:54 +0000

    lint

--- linkerd/app/core/src/serve.rs
+++ linkerd/app/core/src/serve.rs
@@ -5,7 +5,7 @@ use linkerd_error::Error;
 use linkerd_proxy_transport::listen::Addrs;
 use tower::util::ServiceExt;
 use tracing::instrument::Instrument;
-use tracing::{debug, info, info_span, warn};
+use tracing::{debug, info_span, warn};
 
 /// Spawns a task that binds an `L`-typed listener with an `A`-typed
 /// connection-accepting service.
