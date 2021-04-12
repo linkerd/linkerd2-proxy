3708b751fc315fb8bc5c7d8bf8b4142b8bffbe90
Eliza Weisman <eliza@buoyant.io>
2021-04-12 11:57:16 -0700

    Update linkerd/app/outbound/src/logical.rs
    Co-authored-by: Oliver Gould <ver@buoyant.io>

--- linkerd/app/outbound/src/logical.rs
+++ linkerd/app/outbound/src/logical.rs
@@ -1,27 +1,23 @@
 use crate::{tcp, Outbound};
-use linkerd_app_core::{profiles, svc, Error};
+use linkerd_app_core::{profiles, svc};
 
 impl<L> Outbound<L> {
     /// Pushes a layer that unwraps the [`Logical`] address of a given target
     /// from its profile resolution, or else falls back to the provided
     /// per-endpoint service if there was no profile resolution for that target.
-    pub fn push_unwrap_logical<T, I, E, ESvc, LSvc>(
+    pub fn push_unwrap_logical<T, E>(
         self,
         endpoint: E,
     ) -> Outbound<
         impl svc::NewService<
                 (Option<profiles::Receiver>, T),
-                Service = impl svc::Service<I, Response = (), Error = Error, Future = impl Send>,
+                Service = svc::stack::ResultService<svc::Either<L::Service, E::Service>>,
             > + Clone,
     >
     where
         tcp::Logical: From<(profiles::Receiver, T)>,
-        L: svc::NewService<tcp::Logical, Service = LSvc> + Clone,
-        LSvc: svc::Service<I, Response = (), Error = Error>,
-        LSvc::Future: Send,
-        E: svc::NewService<T, Service = ESvc> + Clone,
-        ESvc: svc::Service<I, Response = (), Error = Error>,
-        ESvc::Future: Send,
+        L: svc::NewService<tcp::Logical> + Clone,
+        E: svc::NewService<T> + Clone,
     {
         let Self {
             config,
@@ -30,8 +26,7 @@ impl<L> Outbound<L> {
         } = self;
         let stack = logical
             .push_map_target(tcp::Logical::from)
-            .push(svc::UnwrapOr::layer(endpoint))
-            .check_new_service::<(Option<profiles::Receiver>, T), _>();
+            .push(svc::UnwrapOr::layer(endpoint));
         Outbound {
             config,
             runtime,
