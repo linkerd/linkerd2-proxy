acdb4a537b515f8397f30f77b69a038874660df6
davkor <david@adalogics.com>
2021-04-16 15:35:10 +0100

    app: inbound: fuzzer: generalise fuzzer to send more packets.
    Signed-off-by: davkor <david@adalogics.com>

--- linkerd/app/inbound/fuzz/fuzz_targets/fuzz_target_1.rs
+++ linkerd/app/inbound/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,27 +1,11 @@
 #![no_main]
-use libfuzzer_sys::arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
 
-#[derive(Debug, Arbitrary)]
-struct TransportHeaderSpec {
-    uri: Vec<u8>,
-    header_name: Vec<u8>,
-    header_value: Vec<u8>,
-    http_method: bool,
-}
-
-fuzz_target!(|inp: TransportHeaderSpec| {
-    if let Ok(uri) = std::str::from_utf8(&inp.uri[..]) {
-        if let Ok(header_name) = std::str::from_utf8(&inp.header_name[..]) {
-            if let Ok(header_value) = std::str::from_utf8(&inp.header_value[..]) {
-                let rt = tokio::runtime::Runtime::new().unwrap();
-                rt.block_on(linkerd_app_inbound::http::fuzz_logic::fuzz_entry_raw(
-                    uri,
-                    header_name,
-                    header_value,
-                    inp.http_method,
-                ));
-            }
-        }
+fuzz_target!(|packets: Vec<linkerd_app_inbound::http::fuzz_logic::TransportHeaderSpec>| {
+    if packets.len() == 0 {
+        return;
     }
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(linkerd_app_inbound::http::fuzz_logic::fuzz_entry_raw(packets));
 });
