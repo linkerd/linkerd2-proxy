c117a59a2048ffcdc9c12f5b63b3421868edfa5e
Oliver Gould <ver@buoyant.io>
2021-05-09 05:52:45 +0000

    unify discovery cache tests

--- linkerd/app/outbound/src/discover.rs
+++ linkerd/app/outbound/src/discover.rs
@@ -92,113 +92,129 @@ mod tests {
     use crate::test_util::*;
     use hyper::{client::conn::Builder as ClientBuilder, Body, Request};
     use linkerd_app_core::svc::{NewService, Service, ServiceExt};
-    use std::net::SocketAddr;
+    use std::{
+        net::SocketAddr,
+        sync::{
+            atomic::{AtomicUsize, Ordering},
+            Arc,
+        },
+    };
     use tokio::time;
 
+    // The discover stack caches resolutions for each unique destination address.
+    //
+    // This test obtains a service, drops it obtains the service again, and then drops it again,
+    // testing that only one service is built and that it is dropped after an idle timeout.
     #[tokio::test(flavor = "current_thread")]
-    async fn idle_dropped() {
+    async fn caches_profiles_until_idle() {
         let _trace = support::trace_init();
         time::pause();
 
         let addr = SocketAddr::new([10, 0, 0, 41].into(), 5550);
         let idle_timeout = time::Duration::from_secs(1);
-        let mut cfg = default_config();
-        cfg.proxy.cache_max_idle_age = idle_timeout;
+        let sleep_time = idle_timeout + time::Duration::from_millis(1);
 
+        let profiles = support::profile::resolver().profile(addr, profiles::Profile::default());
+
+        // Mock an inner stack with a service that never returns.
+        let new_count = Arc::new(AtomicUsize::new(0));
         let (handle, stack) = {
+            let new_count = new_count.clone();
             support::track::new_service(move |_| {
+                new_count.fetch_add(1, Ordering::SeqCst);
                 svc::mk(move |_: SensorIo<io::DuplexStream>| future::pending::<Result<(), Error>>())
             })
         };
 
+        // Create a profile stack that uses the tracked inner stack.
+        let cfg = {
+            let mut cfg = default_config();
+            cfg.proxy.cache_max_idle_age = idle_timeout;
+            cfg
+        };
         let (rt, _shutdown) = runtime();
         let mut stack = Outbound::new(cfg, rt)
             .with_stack(stack)
-            .push_discover(support::profile::resolver().profile(addr, profiles::Profile::default()))
+            .push_discover(profiles)
             .into_inner();
-        assert_eq!(handle.tracked_services(), 0);
 
-        let mut svc = stack.new_service(tcp::Accept::from(OrigDstAddr(addr)));
-        let (server_io, _client_io) = io::duplex(1);
-        let fut = svc.ready().await.unwrap().call(server_io);
-        time::advance(idle_timeout).await;
+        // The service hasn't yet been created.
         assert_eq!(
-            handle.tracked_services(),
-            1,
-            "there should be exactly one service"
+            new_count.load(Ordering::SeqCst),
+            0,
+            "no services have been created yet"
         );
-
-        drop(fut);
-        drop(svc);
-        assert_eq!(
-            handle.tracked_services(),
-            1,
-            "the service should be retained"
-        );
-
-        time::sleep(idle_timeout + time::Duration::from_millis(1)).await;
         assert_eq!(
             handle.tracked_services(),
             0,
-            "the service should have been dropped"
+            "no services have been created yet"
         );
-    }
 
-    #[tokio::test(flavor = "current_thread")]
-    async fn active_retained_dropped() {
-        let _trace = support::trace_init();
-        time::pause();
-
-        let addr = SocketAddr::new([10, 0, 0, 41].into(), 5550);
-        let idle_timeout = time::Duration::from_secs(1);
-        let mut cfg = default_config();
-        cfg.proxy.cache_max_idle_age = idle_timeout;
-
-        let (handle, stack) = {
-            support::track::new_service(move |_| {
-                svc::mk(move |_: SensorIo<io::DuplexStream>| future::pending::<Result<(), Error>>())
-            })
-        };
-
-        let (rt, _shutdown) = runtime();
-        let mut stack = Outbound::new(cfg, rt)
-            .with_stack(stack)
-            .push_discover(support::profile::resolver().profile(addr, profiles::Profile::default()))
-            .into_inner();
-        assert_eq!(handle.tracked_services(), 0);
-
-        let (server_io, _client_io) = io::duplex(1);
-        let mut svc0 = stack.new_service(tcp::Accept::from(OrigDstAddr(addr)));
-        let fut0 = svc0.ready().await.unwrap().call(server_io);
-        time::advance(idle_timeout).await;
+        // Instantiate a service from the stack so that it instantiates the tracked inner service.
+        let svc0 = stack.new_service(tcp::Accept::from(OrigDstAddr(addr)));
+        // XXX The discover stack's buffer does not drive profile resolution (or the inner service)
+        // until the service is called?! So we have to
+        let task = tokio::spawn(async move {
+            let (server_io, _client_io) = io::duplex(1);
+            svc0.oneshot(server_io).await
+        });
+        // We have to let some time pass for the buffer to drive the profile to readiness.
+        time::advance(time::Duration::from_millis(100)).await;
+        assert_eq!(
+            new_count.load(Ordering::SeqCst),
+            1,
+            "exactly one service has been created"
+        );
         assert_eq!(
             handle.tracked_services(),
             1,
             "there should be exactly one service"
         );
 
-        drop((svc0, fut0));
-        assert_eq!(
-            handle.tracked_services(),
-            1,
-            "the service should be retained"
-        );
-
+        // Rebuild the stack as we cancel the pending task (so the original reference is dropped).
+        // Let some time pass and ensure the service hasn't been dropped from the stack.
         let svc1 = stack.new_service(tcp::Accept::from(OrigDstAddr(addr)));
-
-        time::sleep(idle_timeout + time::Duration::from_millis(1)).await;
+        task.abort();
+        time::sleep(sleep_time).await;
+        assert_eq!(
+            new_count.load(Ordering::SeqCst),
+            1,
+            "one service has been created"
+        );
         assert_eq!(
             handle.tracked_services(),
             1,
             "the service should be retained"
         );
 
+        // Finally, drop the service reference and ensure the service is dropped from the cache.
         drop(svc1);
-        time::sleep(idle_timeout + time::Duration::from_millis(1)).await;
+        assert_eq!(
+            handle.tracked_services(),
+            1,
+            "the service should have been dropped"
+        );
+        time::sleep(sleep_time).await;
         assert_eq!(
             handle.tracked_services(),
             0,
             "the service should have been dropped"
         );
+
+        // Obtaining the service again creates a new inner stack.
+        let svc2 = stack.new_service(tcp::Accept::from(OrigDstAddr(addr)));
+        let task = tokio::spawn(async move {
+            let (server_io, _client_io) = io::duplex(1);
+            svc2.oneshot(server_io).await
+        });
+        // We have to let some time pass for the buffer to drive the profile to readiness.
+        time::advance(time::Duration::from_millis(100)).await;
+        assert_eq!(
+            new_count.load(Ordering::SeqCst),
+            2,
+            "two services have been created"
+        );
+        assert_eq!(handle.tracked_services(), 1, "the service should be active");
+        task.abort();
     }
 }
