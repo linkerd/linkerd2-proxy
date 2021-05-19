cf9620faf608734d1a5339d053e8281eb4223ea3
aatarasoff <aatarasoff@gmail.com>
2021-05-19 16:08:05 +0300

    Make span name without query string
    Signed-off-by: aatarasoff <aatarasoff@gmail.com>

--- linkerd/trace-context/src/service.rs
+++ linkerd/trace-context/src/service.rs
@@ -98,9 +98,8 @@ where
                     let mut sink = self.sink.clone();
                     let span_name = req
                         .uri()
-                        .path_and_query()
-                        .map(|pq| pq.as_str().to_owned())
-                        .unwrap_or_default();
+                        .path()
+                        .to_owned();
                     return Either::Right(Box::pin(self.inner.call(req).map_ok(move |rsp| {
                         // Emit the completed span with the response metadata.
                         let span = Span {
