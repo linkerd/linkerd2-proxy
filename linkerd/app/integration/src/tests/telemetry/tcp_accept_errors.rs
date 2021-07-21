0dedec952fe601c49c1d5c022fe15d69343b974a
Eliza Weisman <eliza@buoyant.io>
2021-07-21 13:42:55 -0700

    test: add metrics test for invalid IP labels
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/app/integration/src/tests/telemetry/tcp_accept_errors.rs
+++ linkerd/app/integration/src/tests/telemetry/tcp_accept_errors.rs
@@ -5,59 +5,102 @@ const TIMEOUT: Duration = Duration::from_millis(640); // 640ms ought to be enoug
 
 const METRIC: &str = "inbound_tcp_accept_errors_total";
 
-/// A helper that builds a proxy with the above detect timeout and a TCP server that always drops
-/// the accepted socket.
-async fn default_proxy() -> (proxy::Listening, client::Client) {
-    // We provide a mocked TCP server that always immediately drops accepted socket. This should
-    // trigger errors.
-    let srv = tcp::server()
-        .accept_fut(move |sock| async { drop(sock) })
-        .run()
-        .await;
-    let identity = identity::Identity::new(
-        "foo-ns1",
-        "foo.ns1.serviceaccount.identity.linkerd.cluster.local".to_string(),
-    );
-    run_proxy(proxy::new().inbound(srv), identity).await
+struct Test {
+    proxy: Option<proxy::Proxy>,
+    identity: Option<identity::Identity>,
+    set_env: Box<dyn FnMut(&mut TestEnv)>,
 }
 
-/// A helper that configures and runs the provided proxy builder with the above
-/// detect timeout and the provided inbound server and identity.
-async fn run_proxy(
-    proxy: proxy::Proxy,
-    identity::Identity {
-        mut env,
-        mut certify_rsp,
-        ..
-    }: identity::Identity,
-) -> (proxy::Listening, client::Client) {
-    // The identity service is needed for the proxy to start.
-    let id_svc = {
-        certify_rsp.valid_until = Some((SystemTime::now() + Duration::from_secs(666)).into());
-        controller::identity()
-            .certify(move |_| certify_rsp)
-            .run()
-            .await
-    };
+impl Default for Test {
+    fn default() -> Self {
+        Self {
+            proxy: None,
+            identity: None,
+            set_env: Box::new(|_| {}),
+        }
+    }
+}
 
-    env.put(
-        app::env::ENV_INBOUND_DETECT_TIMEOUT,
-        format!("{:?}", TIMEOUT),
-    );
+impl Test {
+    fn new(proxy: proxy::Proxy) -> Self {
+        Self::default().with_proxy(proxy)
+    }
 
-    let proxy = proxy.identity(id_svc).run_with_test_env(env).await;
+    fn with_identity(self, identity: identity::Identity) -> Self {
+        Self {
+            identity: Some(identity),
+            ..self
+        }
+    }
 
-    // Wait for the proxy's identity to be certified.
-    let admin_client = client::http1(proxy.metrics, "localhost");
-    assert_eventually!(
-        admin_client
-            .request(admin_client.request_builder("/ready").method("GET"))
-            .await
-            .unwrap()
-            .status()
-            == http::StatusCode::OK
-    );
-    (proxy, admin_client)
+    fn with_proxy(self, proxy: proxy::Proxy) -> Self {
+        Self {
+            proxy: Some(proxy),
+            ..self
+        }
+    }
+
+    fn with_extra_env(self, set_env: impl FnMut(&mut TestEnv) + 'static) -> Self {
+        Self {
+            set_env: Box::new(set_env),
+            ..self
+        }
+    }
+
+    async fn run(mut self) -> (proxy::Listening, client::Client) {
+        let proxy = if let Some(proxy) = self.proxy {
+            proxy
+        } else {
+            // We provide a mocked TCP server that always immediately drops accepted socket. This should
+            // trigger errors.
+            let srv = tcp::server()
+                .accept_fut(move |sock| async { drop(sock) })
+                .run()
+                .await;
+            proxy::new().inbound(srv)
+        };
+
+        let identity::Identity {
+            mut env,
+            mut certify_rsp,
+            ..
+        }: identity::Identity = self.identity.unwrap_or_else(|| {
+            identity::Identity::new(
+                "foo-ns1",
+                "foo.ns1.serviceaccount.identity.linkerd.cluster.local".to_string(),
+            )
+        });
+
+        // The identity service is needed for the proxy to start.
+        let id_svc = {
+            certify_rsp.valid_until = Some((SystemTime::now() + Duration::from_secs(666)).into());
+            controller::identity()
+                .certify(move |_| certify_rsp)
+                .run()
+                .await
+        };
+
+        env.put(
+            app::env::ENV_INBOUND_DETECT_TIMEOUT,
+            format!("{:?}", TIMEOUT),
+        );
+
+        (self.set_env)(&mut env);
+
+        let proxy = proxy.identity(id_svc).run_with_test_env(env).await;
+
+        // Wait for the proxy's identity to be certified.
+        let admin_client = client::http1(proxy.metrics, "localhost");
+        assert_eventually!(
+            admin_client
+                .request(admin_client.request_builder("/ready").method("GET"))
+                .await
+                .unwrap()
+                .status()
+                == http::StatusCode::OK
+        );
+        (proxy, admin_client)
+    }
 }
 
 fn metric(proxy: &proxy::Listening) -> metrics::MetricMatch {
@@ -69,7 +112,7 @@ fn metric(proxy: &proxy::Listening) -> metrics::MetricMatch {
 async fn inbound_timeout() {
     let _trace = trace_init();
 
-    let (proxy, metrics) = default_proxy().await;
+    let (proxy, metrics) = Test::default().run().await;
     let client = client::tcp(proxy.inbound);
 
     let _tcp_client = client.connect().await;
@@ -89,7 +132,7 @@ async fn inbound_timeout() {
 async fn inbound_io_err() {
     let _trace = trace_init();
 
-    let (proxy, metrics) = default_proxy().await;
+    let (proxy, metrics) = Test::default().run().await;
     let client = client::tcp(proxy.inbound);
 
     let tcp_client = client.connect().await;
@@ -114,7 +157,10 @@ async fn inbound_success() {
     let id_name = "foo.ns1.serviceaccount.identity.linkerd.cluster.local";
     let identity = identity::Identity::new("foo-ns1", id_name.to_string());
     let client_config = client::TlsConfig::new(identity.client_config.clone(), id_name);
-    let (proxy, metrics) = run_proxy(proxy::new().inbound(srv), identity).await;
+    let (proxy, metrics) = Test::new(proxy::new().inbound(srv))
+        .with_identity(identity)
+        .run()
+        .await;
 
     let tls_client = client::http2_tls(
         proxy.inbound,
@@ -151,7 +197,7 @@ async fn inbound_success() {
 async fn inbound_multi() {
     let _trace = trace_init();
 
-    let (proxy, metrics) = default_proxy().await;
+    let (proxy, metrics) = Test::default().run().await;
     let client = client::tcp(proxy.inbound);
 
     let metric = metric(&proxy);
@@ -188,21 +234,16 @@ async fn inbound_multi() {
 #[tokio::test]
 async fn inbound_direct_multi() {
     let _trace = trace_init();
-
     let srv = tcp::server()
         .accept_fut(move |sock| async { drop(sock) })
         .run()
         .await;
-    let identity = identity::Identity::new(
-        "foo-ns1",
-        "foo.ns1.serviceaccount.identity.linkerd.cluster.local".to_string(),
-    );
 
     // Configure the mock SO_ORIGINAL_DST addr to behave as though the
     // connection's original destination was the proxy's inbound listener.
     let proxy = proxy::new().inbound(srv).inbound_direct();
 
-    let (proxy, metrics) = run_proxy(proxy, identity).await;
+    let (proxy, metrics) = Test::new(proxy).run().await;
     let client = client::tcp(proxy.inbound);
 
     let metric = metrics::metric(METRIC).label("target_addr", proxy.inbound);
@@ -235,6 +276,42 @@ async fn inbound_direct_multi() {
     drop(tcp_client);
 }
 
+#[tokio::test]
+async fn inbound_invalid_ip() {
+    let _trace = trace_init();
+    let srv = server::http2().route("/", "hello world").run().await;
+    let fake_ip = SocketAddr::from(([127, 0, 0, 1], 9999));
+    let (proxy, metrics) = Test::new(proxy::new().inbound(srv).inbound_ip(fake_ip))
+        .with_extra_env(|env| {
+            env.put(
+                app::env::ENV_INBOUND_IPS,
+                "10.5.1.42:9999,10.5.8.167:555".to_string(),
+            );
+        })
+        .run()
+        .await;
+
+    let client = client::tcp(proxy.inbound);
+    let metric = metric(&proxy)
+        .label("error", "invalid_ip")
+        .label("target_addr", fake_ip);
+
+    let tcp_client = client.connect().await;
+    tcp_client.write(TcpFixture::HELLO_MSG).await;
+    drop(tcp_client);
+
+    metric.clone().value(1u64).assert_in(&metrics).await;
+
+    // this connectiopn *would* time out, but it doesn't hit the timeout,
+    // because the invalid target addr will already have been rejected.
+    let tcp_client = client.connect().await;
+    tokio::time::sleep(TIMEOUT + Duration::from_millis(15)) // just in case
+        .await;
+    drop(tcp_client);
+
+    metric.clone().value(2u64).assert_in(&metrics).await;
+}
+
 /// Tests that the detect metric is not incremented when TLS is successfully
 /// detected by the direct stack.
 #[tokio::test]
@@ -248,7 +325,11 @@ async fn inbound_direct_success() {
     let proxy1 = proxy::new().inbound(srv).inbound_direct();
     let proxy1_id_name = "foo.ns1.serviceaccount.identity.linkerd.cluster.local";
     let proxy1_id = identity::Identity::new("foo-ns1", proxy1_id_name.to_string());
-    let (proxy1, metrics) = run_proxy(proxy1, proxy1_id).await;
+    let (proxy1, metrics) = Test::default()
+        .with_proxy(proxy1)
+        .with_identity(proxy1_id)
+        .run()
+        .await;
 
     // Route the connection through a second proxy, because inbound direct
     // connections require mutual authentication.
@@ -269,7 +350,11 @@ async fn inbound_direct_success() {
     let proxy2 = proxy::new().outbound_ip(proxy1.inbound).controller(ctrl);
     let proxy2_id_name = "bar.ns1.serviceaccount.identity.linkerd.cluster.local";
     let proxy2_id = identity::Identity::new("bar-ns1", proxy2_id_name.to_string());
-    let (proxy2, _) = run_proxy(proxy2, proxy2_id).await;
+    let (proxy2, _) = Test::default()
+        .with_proxy(proxy2)
+        .with_identity(proxy2_id)
+        .run()
+        .await;
 
     let tls_client = client::http1(proxy2.outbound, auth);
     let no_tls_client = client::tcp(proxy1.inbound);
