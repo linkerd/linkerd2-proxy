c9efca651ac1db98d82e8e8a01c3e8eee0dba6c3
Oliver Gould <ver@buoyant.io>
2021-07-21 19:09:10 +0000

    Touchup gateway test helper per eliza's review

--- linkerd/app/gateway/src/tests.rs
+++ linkerd/app/gateway/src/tests.rs
@@ -12,7 +12,8 @@ use tower_test::mock;
 async fn gateway() {
     assert_eq!(
         Test::default()
-            .run_default_profile()
+            .with_default_profile()
+            .run()
             .await
             .unwrap()
             .status(),
@@ -23,13 +24,18 @@ async fn gateway() {
 #[tokio::test]
 async fn gateway_endpoint() {
     let addr = std::net::SocketAddr::new([192, 0, 2, 10].into(), 777);
-    let profile = Some(support::profile::only(profiles::Profile {
+    let profile = support::profile::only(profiles::Profile {
         endpoint: Some((addr, Metadata::default())),
         ..profiles::Profile::default()
-    }));
+    });
 
     assert_eq!(
-        Test::default().run(profile).await.unwrap().status(),
+        Test::default()
+            .with_profile(profile)
+            .run()
+            .await
+            .unwrap()
+            .status(),
         http::StatusCode::NO_CONTENT
     );
 }
@@ -41,7 +47,8 @@ async fn bad_domain() {
         ..Default::default()
     };
     let status = test
-        .run_default_profile()
+        .with_default_profile()
+        .run()
         .await
         .unwrap_err()
         .downcast_ref::<HttpError>()
@@ -57,7 +64,8 @@ async fn no_identity() {
         ..Default::default()
     };
     let status = test
-        .run_default_profile()
+        .with_default_profile()
+        .run()
         .await
         .unwrap_err()
         .downcast_ref::<HttpError>()
@@ -75,7 +83,8 @@ async fn forward_loop() {
         ..Default::default()
     };
     let status = test
-        .run_default_profile()
+        .with_default_profile()
+        .run()
         .await
         .unwrap_err()
         .downcast_ref::<HttpError>()
@@ -89,6 +98,7 @@ struct Test {
     target: NameAddr,
     client_id: Option<tls::ClientId>,
     orig_fwd: Option<&'static str>,
+    profile: Option<profiles::Receiver>,
 }
 
 impl Default for Test {
@@ -98,20 +108,19 @@ impl Default for Test {
             target: NameAddr::from_str("dst.test.example.com:4321").unwrap(),
             client_id: Some(tls::ClientId::from_str("client.id.test").unwrap()),
             orig_fwd: None,
+            profile: None,
         }
     }
 }
 
 impl Test {
-    async fn run(
-        self,
-        profile: Option<profiles::Receiver>,
-    ) -> Result<http::Response<http::BoxBody>, Error> {
+    async fn run(self) -> Result<http::Response<http::BoxBody>, Error> {
         let Self {
             suffix: _,
             target,
             client_id,
             orig_fwd,
+            profile,
         } = self;
 
         let (outbound, mut handle) =
@@ -161,18 +170,19 @@ impl Test {
         Ok(rsp)
     }
 
-    async fn run_default_profile(self) -> Result<http::Response<http::BoxBody>, Error> {
+    fn with_profile(mut self, profile: profiles::Receiver) -> Self {
         let allow = NameMatch::new(Some(dns::Suffix::from_str(self.suffix).unwrap()));
-        let profile = if allow.matches(self.target.name()) {
-            Some(support::profile::only(profiles::Profile {
-                addr: Some(self.target.clone().into()),
-                ..profiles::Profile::default()
-            }))
-        } else {
-            None
-        };
+        if allow.matches(self.target.name()) {
+            self.profile = Some(profile);
+        }
+        self
+    }
 
-        let resp = self.run(profile).await;
-        resp
+    fn with_default_profile(self) -> Self {
+        let target = self.target.clone();
+        self.with_profile(support::profile::only(profiles::Profile {
+            addr: Some(target.into()),
+            ..profiles::Profile::default()
+        }))
     }
 }
