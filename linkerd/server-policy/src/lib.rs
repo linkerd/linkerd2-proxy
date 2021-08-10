6ee268cc1dcca1f87202997ae919e958845f0cc9
Oliver Gould <ver@buoyant.io>
2021-08-10 07:23:08 +0000

    Remove unused trait for now

--- linkerd/server-policy/src/lib.rs
+++ linkerd/server-policy/src/lib.rs
@@ -1,17 +1,9 @@
-use futures::{future, prelude::*, stream};
 use ipnet::IpNet;
 use std::{
     collections::{HashMap, HashSet},
     net::IpAddr,
     time,
 };
-
-trait DiscoverServerPolicy {
-    type Stream: Stream<Item = ServerPolicy> + Send + 'static;
-    type Future: Future<Output = Self::Stream> + Send + 'static;
-
-    fn discover(&self, port: u16) -> Self::Future;
-}
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct ServerPolicy {
@@ -56,23 +48,6 @@ pub enum Authentication {
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Suffix {
     ends_with: String,
-}
-
-// === impl ServerPolicy ===
-
-/// A stub implementation of DiscoverServerPolicy that returns a single static policy and never
-/// updates.
-impl DiscoverServerPolicy for ServerPolicy {
-    type Stream = stream::BoxStream<'static, Self>;
-    type Future = future::Ready<Self::Stream>;
-
-    fn discover(&self, _port: u16) -> Self::Future {
-        let policy = self.clone();
-        future::ready(Box::pin(async_stream::stream! {
-            yield policy;
-            future::pending::<()>().await;
-        }))
-    }
 }
 
 // === impl Network ===
