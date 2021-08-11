ae62dc06e5e00516ddb5bfde86e662c997ee5ee5
Oliver Gould <ver@buoyant.io>
2021-08-11 15:57:14 +0000

    app: Defer stack construction until the proxy starts
    We currently eagerly build the inbound and outbound stacks. In an
    upcoming change, however, we'll want to ensure that we've obtained
    inbound port policies before initializing the inbound stack.
    
    This change modifies app initialization to move stack initialization
    into the proxy's start task. In order to do this, we eagerly bind the
    inbound/outbound listeners and provide the stream of incoming sockets to
    the stacks at construction-time.
    
    This doesn't actually change any behavior since stack construction was
    moved into a task that was run lazily. The change is purely internal,
    restructuring how/when this task is built.

--- linkerd/app/outbound/src/lib.rs
+++ linkerd/app/outbound/src/lib.rs
@@ -16,9 +16,10 @@ pub mod tcp;
 #[cfg(test)]
 pub(crate) mod test_util;
 
+use futures::Stream;
 use linkerd_app_core::{
-    config::{ProxyConfig, ServerConfig},
-    metrics, profiles,
+    config::ProxyConfig,
+    io, metrics, profiles,
     proxy::{
         api_resolve::{ConcreteAddr, Metadata},
         core::Resolve,
@@ -26,10 +27,10 @@ use linkerd_app_core::{
     serve,
     svc::{self, stack::Param},
     tls,
-    transport::{self, addrs::*, listen::Bind},
+    transport::{self, addrs::*},
     AddrMatch, Conditional, Error, ProxyRuntime,
 };
-use std::{collections::HashMap, future::Future, time::Duration};
+use std::{collections::HashMap, fmt::Debug, time::Duration};
 use tracing::info;
 
 const EWMA_DEFAULT_RTT: Duration = Duration::from_millis(30);
@@ -119,15 +120,12 @@ impl<S> Outbound<S> {
 }
 
 impl Outbound<()> {
-    pub fn serve<B, P, R>(
-        self,
-        bind: B,
-        profiles: P,
-        resolve: R,
-    ) -> (Local<ServerAddr>, impl Future<Output = ()>)
+    pub async fn serve<L, A, I, P, R>(self, listen: L, profiles: P, resolve: R)
     where
-        B: Bind<ServerConfig>,
-        B::Addrs: Param<Remote<ClientAddr>> + Param<OrigDstAddr>,
+        L: Stream<Item = io::Result<(A, I)>> + Send + Sync + 'static,
+        A: Param<Remote<ClientAddr>> + Param<OrigDstAddr> + Clone + Send + Sync + 'static,
+        I: io::AsyncRead + io::AsyncWrite + io::Peek + io::PeerAddr,
+        I: Debug + Unpin + Send + Sync + 'static,
         R: Clone + Send + Sync + Unpin + 'static,
         R: Resolve<ConcreteAddr, Endpoint = Metadata, Error = Error>,
         R::Resolution: Send,
@@ -136,33 +134,25 @@ impl Outbound<()> {
         P::Future: Send,
         P::Error: Send,
     {
-        let (listen_addr, listen) = bind
-            .bind(&self.config.proxy.server)
-            .expect("Failed to bind outbound listener");
-
-        let serve = async move {
-            if self.config.ingress_mode {
-                info!("Outbound routing in ingress-mode");
-                let stack = self
-                    .to_tcp_connect()
-                    .push_tcp_endpoint()
-                    .push_http_endpoint()
-                    .into_ingress(profiles, resolve);
-                let shutdown = self.runtime.drain.signaled();
-                serve::serve(listen, stack, shutdown).await;
-            } else {
-                let logical = self.to_tcp_connect().push_logical(resolve);
-                let endpoint = self.to_tcp_connect().push_endpoint();
-                let server = endpoint
-                    .push_switch_logical(logical.into_inner())
-                    .push_discover(profiles)
-                    .into_inner();
-                let shutdown = self.runtime.drain.signaled();
-                serve::serve(listen, server, shutdown).await;
-            }
-        };
-
-        (listen_addr, serve)
+        if self.config.ingress_mode {
+            info!("Outbound routing in ingress-mode");
+            let stack = self
+                .to_tcp_connect()
+                .push_tcp_endpoint()
+                .push_http_endpoint()
+                .into_ingress(profiles, resolve);
+            let shutdown = self.runtime.drain.signaled();
+            serve::serve(listen, stack, shutdown).await;
+        } else {
+            let logical = self.to_tcp_connect().push_logical(resolve);
+            let endpoint = self.to_tcp_connect().push_endpoint();
+            let server = endpoint
+                .push_switch_logical(logical.into_inner())
+                .push_discover(profiles)
+                .into_inner();
+            let shutdown = self.runtime.drain.signaled();
+            serve::serve(listen, server, shutdown).await;
+        }
     }
 }
 
