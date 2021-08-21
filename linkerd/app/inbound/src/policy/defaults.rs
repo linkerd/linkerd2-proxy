7efee00cdb24e5cdc081299cf566641647b173f1
Oliver Gould <ver@buoyant.io>
2021-08-21 21:52:59 +0000

    cleanup defaults code

--- linkerd/app/inbound/src/policy/defaults.rs
+++ linkerd/app/inbound/src/policy/defaults.rs
@@ -1,89 +1,86 @@
 use linkerd_app_core::{IpNet, Ipv4Net, Ipv6Net};
 use linkerd_server_policy::{Authentication, Authorization, Protocol, ServerPolicy, Suffix};
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 pub fn all_authenticated(timeout: Duration) -> ServerPolicy {
-    authenticated("default:all-authenticated", all_nets(), timeout)
+    mk(
+        "default:all-authenticated",
+        all_nets(),
+        authenticated(),
+        timeout,
+    )
 }
 
 pub fn all_unauthenticated(timeout: Duration) -> ServerPolicy {
-    unauthenticated("default:all-unauthenticated", all_nets(), timeout)
+    mk(
+        "default:all-unauthenticated",
+        all_nets(),
+        Authentication::Unauthenticated,
+        timeout,
+    )
 }
 
 pub fn cluster_authenticated(
     nets: impl IntoIterator<Item = IpNet>,
     timeout: Duration,
 ) -> ServerPolicy {
-    authenticated("default:cluster-authenticated", nets, timeout)
+    mk(
+        "default:cluster-authenticated",
+        nets,
+        authenticated(),
+        timeout,
+    )
 }
 
 pub fn cluster_unauthenticated(
     nets: impl IntoIterator<Item = IpNet>,
     timeout: Duration,
 ) -> ServerPolicy {
-    unauthenticated("default:cluster-unauthenticated", nets, timeout)
+    mk(
+        "default:cluster-unauthenticated",
+        nets,
+        Authentication::Unauthenticated,
+        timeout,
+    )
 }
 
 pub fn all_mtls_unauthenticated(timeout: Duration) -> ServerPolicy {
-    ServerPolicy {
-        protocol: Protocol::Detect { timeout },
-        authorizations: vec![Authorization {
-            networks: all_nets().map(Into::into).collect(),
-            authentication: Authentication::TlsUnauthenticated,
-            labels: Some((
-                "name".to_string(),
-                "default:all-tls-unauthenticated".to_string(),
-            ))
-            .into_iter()
-            .collect(),
-        }],
-        labels: Some(("name".to_string(), "default:all-tls-unauthenticated".into()))
-            .into_iter()
-            .collect(),
-    }
+    mk(
+        "default:all-tls-unauthenticated",
+        all_nets(),
+        Authentication::TlsUnauthenticated,
+        timeout,
+    )
 }
 
 fn all_nets() -> impl Iterator<Item = IpNet> {
     vec![Ipv4Net::default().into(), Ipv6Net::default().into()].into_iter()
 }
 
-fn authenticated(
-    name: impl Into<String>,
-    nets: impl IntoIterator<Item = IpNet>,
-    timeout: Duration,
-) -> ServerPolicy {
-    let name = name.into();
-    ServerPolicy {
-        protocol: Protocol::Detect { timeout },
-        authorizations: vec![Authorization {
-            networks: nets.into_iter().map(Into::into).collect(),
-            authentication: Authentication::TlsAuthenticated {
-                identities: Default::default(),
-                suffixes: vec![Suffix::from(vec![])],
-            },
-            labels: Some(("name".to_string(), name.clone()))
-                .into_iter()
-                .collect(),
-        }],
-        labels: Some(("name".to_string(), name)).into_iter().collect(),
+fn authenticated() -> Authentication {
+    Authentication::TlsAuthenticated {
+        identities: Default::default(),
+        suffixes: vec![Suffix::from(vec![])],
     }
 }
 
-fn unauthenticated(
-    name: impl Into<String>,
+fn mk(
+    name: &str,
     nets: impl IntoIterator<Item = IpNet>,
+    authentication: Authentication,
     timeout: Duration,
 ) -> ServerPolicy {
-    let name = name.into();
+    let labels = Some(("name".to_string(), name.to_string()))
+        .into_iter()
+        .collect::<HashMap<_, _>>();
+
     ServerPolicy {
         protocol: Protocol::Detect { timeout },
         authorizations: vec![Authorization {
             networks: nets.into_iter().map(Into::into).collect(),
-            authentication: Authentication::Unauthenticated,
-            labels: Some(("name".to_string(), name.clone()))
-                .into_iter()
-                .collect(),
+            authentication,
+            labels: labels.clone(),
         }],
-        labels: Some(("name".to_string(), name)).into_iter().collect(),
+        labels,
     }
 }
