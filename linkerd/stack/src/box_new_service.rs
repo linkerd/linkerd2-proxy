f2789c5af5e5cb7e93593285b4da502f372bd553
Oliver Gould <ver@buoyant.io>
2021-09-12 03:56:47 +0000

    Use Arc to avoid DynClone

--- linkerd/stack/src/box_new_service.rs
+++ linkerd/stack/src/box_new_service.rs
@@ -1,23 +1,8 @@
 use crate::{layer, NewService};
-use dyn_clone::DynClone;
-use std::fmt;
+use std::{fmt, sync::Arc};
 
 pub struct BoxNewService<T, S> {
-    inner: Box<dyn CloneNewService<T, S> + Send + Sync>,
-}
-
-trait CloneNewService<T, S>: DynClone {
-    fn inner_new_service(&self, t: T) -> S;
-}
-
-impl<T, N> CloneNewService<T, N::Service> for N
-where
-    N: NewService<T> + Clone + Send + Sync + 'static,
-    N::Service: Send + 'static,
-{
-    fn inner_new_service(&self, t: T) -> N::Service {
-        self.new_service(t)
-    }
+    inner: Arc<dyn NewService<T, Service = S> + Send + Sync>,
 }
 
 impl<T, S> BoxNewService<T, S> {
@@ -31,11 +16,11 @@ impl<T, S> BoxNewService<T, S> {
 
     pub fn new<N>(inner: N) -> Self
     where
-        N: NewService<T, Service = S> + Clone + Send + Sync + 'static,
+        N: NewService<T, Service = S> + Send + Sync + 'static,
         S: Send + 'static,
     {
         Self {
-            inner: Box::new(inner),
+            inner: Arc::new(inner),
         }
     }
 }
@@ -43,7 +28,7 @@ impl<T, S> BoxNewService<T, S> {
 impl<T, S> Clone for BoxNewService<T, S> {
     fn clone(&self) -> Self {
         Self {
-            inner: dyn_clone::clone_box(&*self.inner),
+            inner: self.inner.clone(),
         }
     }
 }
@@ -52,7 +37,7 @@ impl<T, S> NewService<T> for BoxNewService<T, S> {
     type Service = S;
 
     fn new_service(&self, t: T) -> S {
-        self.inner.inner_new_service(t)
+        self.inner.new_service(t)
     }
 }
 
