8bd76e9313284664449f20681e34e99e36c9b143
Oliver Gould <ver@buoyant.io>
2021-10-23 05:28:44 +0000

    wip

--- linkerd/proxy/identity/src/lib.rs
+++ linkerd/proxy/identity/src/lib.rs
@@ -5,9 +5,16 @@ pub mod certify;
 pub mod metrics;
 mod token;
 
-pub use self::{
-    certify::{AwaitCrt, Csr, LocalCrtKey},
-    token::TokenSource,
-};
+pub use self::{certify::Certify, metrics::Metrics, token::TokenSource};
 pub use linkerd_identity::*;
-pub use linkerd_tls_rustls::*;
+
+use linkerd_error::Result;
+use std::time::SystemTime;
+
+pub trait Credentials {
+    fn name(&self) -> &Name;
+
+    fn get_csr(&mut self) -> Vec<u8>;
+
+    fn set_crt(&mut self, leaf: Vec<u8>, chain: Vec<Vec<u8>>, expiry: SystemTime) -> Result<()>;
+}
