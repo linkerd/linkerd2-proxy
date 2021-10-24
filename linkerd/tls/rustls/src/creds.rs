e927c86a077f2ea82c2a32737112d795ce7b07b1
Oliver Gould <ver@buoyant.io>
2021-10-24 19:08:39 +0000

    wip

--- linkerd/tls/rustls/src/creds.rs
+++ linkerd/tls/rustls/src/creds.rs
@@ -3,16 +3,19 @@ use linkerd_proxy_identity as id;
 use ring::{error::KeyRejected, rand, signature::EcdsaKeyPair};
 use std::{sync::Arc, time::SystemTime};
 use thiserror::Error;
+use tokio::sync::watch;
 use tokio_rustls::rustls;
 use tracing::{debug, warn};
 
-#[derive(Clone, Debug)]
 pub struct Creds {
     roots: rustls::RootCertStore,
     key: Arc<EcdsaKeyPair>,
     csr: Arc<[u8]>,
     identity: id::Name,
-    certificate: Option<Crt>,
+    client_rx: watch::Receiver<Option<Crt<rustls::ClientConfig>>>,
+    client_tx: watch::Sender<Option<Crt<rustls::ClientConfig>>>,
+    server_rx: watch::Receiver<Option<Crt<rustls::ServerConfig>>>,
+    server_tx: watch::Sender<Option<Crt<rustls::ServerConfig>>>,
 }
 
 #[derive(Debug, Error)]
@@ -24,9 +27,9 @@ pub struct InvalidKey(KeyRejected);
 pub struct InvalidTrustRoots(());
 
 #[derive(Clone, Debug)]
-struct Crt {
-    chain: Vec<rustls::Certificate>,
-    expiry: SystemTime,
+pub struct Crt<T> {
+    pub config: Arc<T>,
+    pub expiry: SystemTime,
 }
 
 #[derive(Clone)]
@@ -42,6 +45,8 @@ const SIGNATURE_ALG_RUSTLS_SCHEME: rustls::SignatureScheme =
 const SIGNATURE_ALG_RUSTLS_ALGORITHM: rustls::internal::msgs::enums::SignatureAlgorithm =
     rustls::internal::msgs::enums::SignatureAlgorithm::ECDSA;
 const TLS_VERSIONS: &[rustls::ProtocolVersion] = &[rustls::ProtocolVersion::TLSv1_3];
+static RUSTLS_SUPPORTED_CIPHERSUITES: &[&rustls::SupportedCipherSuite] =
+    &[&rustls::ciphersuite::TLS13_CHACHA20_POLY1305_SHA256];
 
 impl Creds {
     pub fn load(identity: id::Name, roots_pem: &str, key_pkcs8: &[u8], csr: &[u8]) -> Result<Self> {
@@ -59,13 +64,27 @@ impl Creds {
         let key =
             EcdsaKeyPair::from_pkcs8(SIGNATURE_ALG_RING_SIGNING, key_pkcs8).map_err(InvalidKey)?;
 
+        let (client_tx, client_rx) = watch::channel(None);
+        let (server_tx, server_rx) = watch::channel(None);
+
         Ok(Self {
             roots,
             key: Arc::new(key),
             csr: csr.into(),
             identity,
-            certificate: None,
+            client_rx,
+            client_tx,
+            server_rx,
+            server_tx,
         })
+    }
+
+    pub fn client(&self) -> watch::Receiver<Option<Crt<rustls::ClientConfig>>> {
+        self.client_rx.clone()
+    }
+
+    pub fn server(&self) -> watch::Receiver<Option<Crt<rustls::ServerConfig>>> {
+        self.server_rx.clone()
     }
 }
 
@@ -88,71 +107,83 @@ impl id::Credentials for Creds {
         chain.push(rustls::Certificate(leaf));
         chain.extend(intermediates.into_iter().map(rustls::Certificate));
 
-        self.certificate = Some(Crt { chain, expiry });
+        let mut client = rustls::ClientConfig::new();
+
+        // XXX: Rustls's built-in verifiers don't let us tweak things as fully
+        // as we'd like (e.g. controlling the set of trusted signature
+        // algorithms), but they provide good enough defaults for now.
+        // TODO: lock down the verification further.
+        // TODO: Change Rustls's API to avoid needing to clone `root_cert_store`.
+        client.root_store = self.roots.clone();
+        client.ciphersuites = RUSTLS_SUPPORTED_CIPHERSUITES.to_vec();
+
+        // Disable session resumption for the time-being until resumption is
+        // more tested.
+        client.enable_tickets = false;
+
+        // Ensure the certificate is valid for the services we terminate for
+        // TLS. This assumes that server cert validation does the same or
+        // more validation than client cert validation.
+        //
+        // XXX: Rustls currently only provides access to a
+        // `ServerCertVerifier` through
+        // `ClientConfig::get_verifier()`.
+        //
+        // XXX: Once `ServerCertVerified` is exposed in Rustls's
+        // safe API, use it to pass proof to CertCertResolver::new....
+        //
+        // TODO: Restrict accepted signature algorithms.
+        let crt_id = webpki::DNSNameRef::try_from_ascii(self.identity.as_bytes())
+            .expect("identity must be a valid DNS name");
+        static NO_OCSP: &[u8] = &[];
+        client
+            .get_verifier()
+            .verify_server_cert(&self.roots, &*chain, crt_id, NO_OCSP)?;
+        debug!("Certified");
+
+        let resolver = Arc::new(CertResolver(rustls::sign::CertifiedKey::new(
+            chain,
+            Arc::new(Box::new(Key(self.key.clone()))),
+        )));
+
+        // Enable client authentication.
+        client.client_auth_cert_resolver = resolver.clone();
+
+        self.client_tx
+            .send(Some(Crt {
+                config: client.into(),
+                expiry,
+            }))
+            .ok()
+            .expect("receivers are held");
+
+        // Ask TLS clients for a certificate and accept any certificate issued
+        // by our trusted CA(s).
+        //
+        // XXX: Rustls's built-in verifiers don't let us tweak things as fully
+        // as we'd like (e.g. controlling the set of trusted signature
+        // algorithms), but they provide good enough defaults for now.
+        // TODO: lock down the verification further.
+        //
+        // TODO: Change Rustls's API to avoid needing to clone `root_cert_store`.
+        let mut server = rustls::ServerConfig::new(
+            rustls::AllowAnyAnonymousOrAuthenticatedClient::new(self.roots.clone()),
+        );
+        server.versions = TLS_VERSIONS.to_vec();
+        server.cert_resolver = resolver;
+        server.ciphersuites = RUSTLS_SUPPORTED_CIPHERSUITES.to_vec();
+
+        self.server_tx
+            .send(Some(Crt {
+                config: server.into(),
+                expiry,
+            }))
+            .ok()
+            .expect("receivers are held");
 
         Ok(())
     }
 }
-
-// === impl TrustAnchors ===
-
-/*
-let mut client = ClientConfig::new();
-
-// XXX: Rustls's built-in verifiers don't let us tweak things as fully
-// as we'd like (e.g. controlling the set of trusted signature
-// algorithms), but they provide good enough defaults for now.
-// TODO: lock down the verification further.
-// TODO: Change Rustls's API to Avoid needing to clone `root_cert_store`.
-client.root_store = roots;
-
-// Disable session resumption for the time-being until resumption is
-// more tested.
-client.enable_tickets = false;
-
-// Ensure the certificate is valid for the services we terminate for
-// TLS. This assumes that server cert validation does the same or
-// more validation than client cert validation.
-//
-// XXX: Rustls currently only provides access to a
-// `ServerCertVerifier` through
-// `ClientConfig::get_verifier()`.
-//
-// XXX: Once `ServerCertVerified` is exposed in Rustls's
-// safe API, use it to pass proof to CertCertResolver::new....
-//
-// TODO: Restrict accepted signature algorithms.
-static NO_OCSP: &[u8] = &[];
-let crt_id = webpki::DNSNameRef::try_from_ascii(crt.id.as_bytes())
-    .map_err(|e| InvalidCrt(TLSError::General(e.to_string())))?;
-client
-    .get_verifier()
-    .verify_server_cert(&client.root_store, &crt.chain, crt_id, NO_OCSP)
-    .map_err(InvalidCrt)?;
-debug!("certified {}", crt.id);
-
-let resolver = Arc::new(CertResolver(sign::CertifiedKey::new(crt.chain, Arc::new(Box::new(key)))));;
-
-// Enable client authentication.
-client.client_auth_cert_resolver = resolver.clone();
-*/
-
-/*
-// Ask TLS clients for a certificate and accept any certificate issued
-// by our trusted CA(s).
-//
-// XXX: Rustls's built-in verifiers don't let us tweak things as fully
-// as we'd like (e.g. controlling the set of trusted signature
-// algorithms), but they provide good enough defaults for now.
-// TODO: lock down the verification further.
-//
-// TODO: Change Rustls's API to Avoid needing to clone `root_cert_store`.
-let mut server = ServerConfig::new(AllowAnyAnonymousOrAuthenticatedClient::new(
-    self.0.root_store.clone(),
-));
-server.versions = TLS_VERSIONS.to_vec();
-server.cert_resolver = resolver;
-*/
 
 // === impl CertResolver ===
 
