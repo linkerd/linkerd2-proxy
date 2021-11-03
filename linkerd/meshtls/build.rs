4d07551d86bd1cc7798bc2d1283523c1552029d4
Oliver Gould <ver@buoyant.io>
2021-11-03 22:16:00 +0000

    Add a build.rs file for meshtls to ensure that a feature is present

---
+++ linkerd/meshtls/build.rs
@@ -0,0 +1,16 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Ensure that at least one TLS implementation feature is enabled.
+    static TLS_FEATURES: &[&'static str] = &["rustls"];
+    if !TLS_FEATURES
+        .iter()
+        .any(|f| std::env::var_os(&*format!("CARGO_FEATURE_{}", f.to_ascii_uppercase())).is_some())
+    {
+        return Err(format!(
+            "at least one of the following TLS implementations must be enabled: '{}'",
+            TLS_FEATURES.join("', '"),
+        )
+        .into());
+    }
+
+    Ok(())
+}
