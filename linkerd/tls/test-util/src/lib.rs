9ba4e4cc1f8a58db33aba12713495bb728d2711d
Oliver Gould <ver@buoyant.io>
2021-11-06 19:17:50 +0000

    Restore rustls credential tests
    In a prior change, the rustls credential-loading tests were accidentally
    disabled. This change restores these tests and updates them to use the
    newer API.

--- linkerd/tls/test-util/src/lib.rs
+++ linkerd/tls/test-util/src/lib.rs
@@ -19,6 +19,13 @@ pub static FOO_NS1: Entity = Entity {
     key: include_bytes!("testdata/foo-ns1-ca1/key.p8"),
 };
 
+pub static FOO_NS1_CA2: Entity = Entity {
+    name: "foo.ns1.serviceaccount.identity.linkerd.cluster.local",
+    trust_anchors: include_bytes!("testdata/ca2.pem"),
+    crt: include_bytes!("testdata/foo-ns1-ca2/crt.der"),
+    key: include_bytes!("testdata/foo-ns1-ca2/key.p8"),
+};
+
 pub static BAR_NS1: Entity = Entity {
     name: "bar.ns1.serviceaccount.identity.linkerd.cluster.local",
     trust_anchors: include_bytes!("testdata/ca1.pem"),
