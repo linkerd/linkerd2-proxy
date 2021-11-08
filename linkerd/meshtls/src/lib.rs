92d4eb9622d11d29c1ce521ce641dbff496a43fe
Oliver Gould <ver@buoyant.io>
2021-11-08 18:20:34 +0000

    wrap comments more tightly

--- linkerd/meshtls/src/lib.rs
+++ linkerd/meshtls/src/lib.rs
@@ -1,13 +1,15 @@
 #![deny(warnings, rust_2018_idioms)]
 #![forbid(unsafe_code)]
 
-//! This crate provides a static interface for the proxy's x509 certificate provisioning and
-//! creation of client/server services. It supports the `boring` and `rustls` TLS backends.
+//! This crate provides a static interface for the proxy's x509 certificate
+//! provisioning and creation of client/server services. It supports the
+//! `boring` and `rustls` TLS backends.
 //!
-//! This crate may be compiled without either implementation, in which case it will fail at runtime.
-//! This enables an implementation to be chosen by the proxy's frontend, so that other crates can
-//! depend on this crate without having to pin a TLS implementation. Furthermore, this crate
-//! supports both backends simultaneously so it can be compiled with `--all-features`.
+//! This crate may be compiled without either implementation, in which case it
+//! will fail at runtime.  This enables an implementation to be chosen by the
+//! proxy's frontend, so that other crates can depend on this crate without
+//! having to pin a TLS implementation. Furthermore, this crate supports both
+//! backends simultaneously so it can be compiled with `--all-features`.
 
 mod client;
 pub mod creds;
