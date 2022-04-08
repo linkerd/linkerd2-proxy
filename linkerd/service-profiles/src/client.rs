44362a252ffb6d179e324b66eb545c81d8b192ea
Oliver Gould <ver@buoyant.io>
2022-04-08 20:47:46 +0000

    Update to linkerd2-proxy-api v0.5 and tonic v0.7
    With tonic v0.7, we now have the ability to provide a fixed PROTOC
    binary (rather than building/fetching a protoc implementation at
    build-time).
    
    This change updates the `linkerd-transport-header` and
    `opencensus-proto` crates to use statically-generated sources,
    eliminating the need for a `protoc` binary at build-time. Each crate
    includes a `bootstrap` test that fails if the generated sources differ
    from what is checked into git. These tests can also be used to
    regenerate sources when the protobuf (or tonic generation) changes.
    
    A local `install-protoc` action is added that fetches a protoc binary
    and configures the `PROTOC_NO_VENDOR` and `PROTOC` environment
    variables. This action is used by the check-all, check-each, and test
    workflows.
    
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- linkerd/service-profiles/src/client.rs
+++ linkerd/service-profiles/src/client.rs
@@ -31,7 +31,7 @@ impl<R, S> Client<R, S>
 where
     S: GrpcService<BoxBody> + Clone + Send + 'static,
     S::ResponseBody: Send + Sync,
-    <S::ResponseBody as Body>::Data: Send,
+    S::ResponseBody: Default + Body<Data = tonic::codegen::Bytes> + Send + 'static,
     <S::ResponseBody as Body>::Error:
         Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
     S::Future: Send,
@@ -49,8 +49,7 @@ impl<T, R, S> Service<T> for Client<R, S>
 where
     T: Param<LookupAddr>,
     S: GrpcService<BoxBody> + Clone + Send + 'static,
-    S::ResponseBody: Send + Sync,
-    <S::ResponseBody as Body>::Data: Send,
+    S::ResponseBody: Default + Body<Data = tonic::codegen::Bytes> + Send + 'static,
     <S::ResponseBody as Body>::Error:
         Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
     S::Future: Send,
@@ -96,8 +95,7 @@ type InnerFuture =
 impl<S> Inner<S>
 where
     S: GrpcService<BoxBody> + Clone + Send + 'static,
-    S::ResponseBody: Send + Sync,
-    <S::ResponseBody as Body>::Data: Send,
+    S::ResponseBody: Default + Body<Data = tonic::codegen::Bytes> + Send + 'static,
     <S::ResponseBody as Body>::Error:
         Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
     S::Future: Send,
@@ -113,8 +111,7 @@ where
 impl<S> Service<LookupAddr> for Inner<S>
 where
     S: GrpcService<BoxBody> + Clone + Send + 'static,
-    S::ResponseBody: Send + Sync,
-    <S::ResponseBody as Body>::Data: Send,
+    S::ResponseBody: Default + Body<Data = tonic::codegen::Bytes> + Send + 'static,
     <S::ResponseBody as Body>::Error:
         Into<Box<dyn std::error::Error + Send + Sync + 'static>> + Send,
     S::Future: Send,
