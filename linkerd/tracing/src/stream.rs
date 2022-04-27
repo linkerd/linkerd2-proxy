654d658aa04a2137ca46633556bcf512efef9c1b
Eliza Weisman <eliza@buoyant.io>
2022-04-27 15:35:37 -0700

    wip
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/tracing/src/stream.rs
+++ linkerd/tracing/src/stream.rs
@@ -1,26 +1,56 @@
-use std::{io, sync::atomic::AtomicUsize};
-use thingbuf::mpsc::{self, SendRef};
+use std::{
+    io,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+use thingbuf::{
+    mpsc::{self, SendRef},
+    recycle::WithCapacity,
+};
 use tracing_subscriber::fmt::writer::MakeWriter;
 
+
 pub struct Writer {
-    tx: mpsc::Sender<String>,
+    tx: mpsc::Sender<String, WithCapacity>,
     shared: Arc<Shared>,
 }
 
 pub struct Reader {
-    rx: mpsc::Receiver<String>,
+    rx: mpsc::Receiver<String, WithCapacity>,
     shared: Arc<Shared>,
 }
 
-pub struct Line<'a>(SendRef<'a, String>);
+pub struct Line<'a>(Option<SendRef<'a, String>>);
+
+pub struct Writers
+
+struct Shared {
+    dropped_logs: AtomicUsize,
+}
 
 // === impl Line ===
 
 impl<'a> io::Write for Line<'a> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.0.write(buf)
+        if let Some(ref mut ine) = self.0 {
+            return line.write(buf);
+        }
+        // no channel capacity; drop the line, but pretend it succeeded
+        Ok(buf.len())
     }
 }
-struct Shared {
-    dropped_logs: AtomicUsize,
+
+// === impl Writer ===
+
+impl<'a> MakeWriter<'a> for Writer {
+    type Writer = Line<'a>;
+
+    fn make_writer(&'a self) -> Line<'a> {
+        match self.tx.try_send_ref() {
+            Ok(line) => Line(Some(line)),
+            Err(_) => {
+                self.shared.dropped_logs.fetch_add(1, Ordering::Relaxed);
+                Line(None)
+            }
+        }
+    }
 }
