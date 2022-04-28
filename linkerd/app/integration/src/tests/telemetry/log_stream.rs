9f7ce46b85dadde8ed51570676ce97e77eed51ee
Eliza Weisman <eliza@buoyant.io>
2022-04-28 12:37:10 -0700

    add a test for multiple concurrent streams
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- linkerd/app/integration/src/tests/telemetry/log_stream.rs
+++ linkerd/app/integration/src/tests/telemetry/log_stream.rs
@@ -1,6 +1,6 @@
 use super::*;
 use futures::future::{self, FutureExt};
-use tokio::sync::oneshot;
+use tokio::{sync::oneshot, task::JoinHandle};
 
 #[tokio::test]
 async fn is_valid_json() {
@@ -13,24 +13,13 @@ async fn is_valid_json() {
         ..
     } = Fixture::outbound().await;
 
-    let body = metrics
-        .request_body(
-            metrics
-                .request_builder("/logs")
-                .method(http::Method::GET)
-                .body(hyper::Body::from("info,linkerd=debug"))
-                .unwrap(),
-        )
-        .await
-        .into_body();
-    let (done_tx, done_rx) = oneshot::channel();
-    let logs = tokio::spawn(stream_logs(body, done_rx));
+    let (logs, done) = stream_logs(metrics, "info,linkerd=debug").await;
 
     info!("client.get(/)");
     assert_eq!(client.get("/").await, "hello");
 
     // finish streaming logs so we don't loop forever
-    let _ = done_tx.send(());
+    let _ = done.send(());
 
     let json = logs.await.unwrap();
     for obj in json {
@@ -38,33 +27,104 @@ async fn is_valid_json() {
     }
 }
 
-async fn stream_logs(
-    mut body: hyper::Body,
-    done_rx: oneshot::Receiver<()>,
-) -> Vec<serde_json::Value> {
-    let mut result = Vec::new();
-    let logs = &mut result;
-    let fut = async move {
-        while let Some(res) = body.data().await {
-            let chunk = match res {
-                Ok(chunk) => chunk,
-                Err(e) => {
-                    println!("body failed: {}", e);
-                    break;
-                }
-            };
-            let deserialized = serde_json::from_slice(&chunk[..]);
-            tracing::info!(?deserialized);
-            match deserialized {
-                Ok(json) => logs.push(json),
-                Err(error) => panic!(
-                    "parsing logs as JSON failed\n  error: {error}\n  chunk: {:?}",
-                    String::from_utf8_lossy(&chunk[..])
-                ),
-            }
-        }
-    };
-    future::select(Box::pin(fut), done_rx.map(|_| ())).await;
+#[tokio::test]
+async fn multi_filter() {
+    let Fixture {
+        client,
+        metrics,
+        proxy,
+        _profile,
+        dst_tx: _dst_tx,
+        ..
+    } = Fixture::outbound().await;
 
-    result
+    // start streaming the logs
+    let (debug_logs, debug_done) = stream_logs(metrics, "debug").await;
+    let (hyper_logs, hyper_done) =
+        stream_logs(client::http1(proxy.admin, "localhost"), "hyper=trace").await;
+
+    info!("client.get(/)");
+    assert_eq!(client.get("/").await, "hello");
+
+    // finish streaming logs so we don't loop forever
+    let _ = debug_done.send(());
+    let _ = hyper_done.send(());
+
+    let json = debug_logs.await.unwrap();
+    for obj in json {
+        let level = obj.get("level");
+        assert!(
+            matches!(
+                level.and_then(|value| value.as_str()),
+                Some("DEBUG") | Some("INFO") | Some("WARN") | Some("ERROR")
+            ),
+            "level must be DEBUG, INFO, WARN, or ERROR\n level: {:?}\n  json: {:#?}",
+            level,
+            obj
+        );
+    }
+
+    let json = hyper_logs.await.unwrap();
+    for obj in json {
+        let target = obj.get("target").and_then(|value| value.as_str());
+        match target {
+            Some(s) if s.starts_with("hyper") => {}
+            _ => panic!(
+                "target must be from a module in `hyper`!\n target: {:?}\n   json: {:#?}",
+                obj.get("target"),
+                obj
+            ),
+        }
+    }
+}
+
+async fn stream_logs(
+    client: client::Client,
+    filter: impl ToString,
+) -> (JoinHandle<Vec<serde_json::Value>>, oneshot::Sender<()>) {
+    let filter = filter.to_string();
+
+    // start the request
+    let mut body = client
+        .request_body(
+            client
+                .request_builder("/logs")
+                .method(http::Method::GET)
+                .body(hyper::Body::from(filter))
+                .unwrap(),
+        )
+        .await
+        .into_body();
+
+    // spawn a task to collect and parse all the logs
+    let (done_tx, done_rx) = oneshot::channel();
+    let result = tokio::spawn(async move {
+        let mut result = Vec::new();
+        let logs = &mut result;
+        let fut = async move {
+            while let Some(res) = body.data().await {
+                let chunk = match res {
+                    Ok(chunk) => chunk,
+                    Err(e) => {
+                        println!("body failed: {}", e);
+                        break;
+                    }
+                };
+                let deserialized = serde_json::from_slice(&chunk[..]);
+                tracing::info!(?deserialized);
+                match deserialized {
+                    Ok(json) => logs.push(json),
+                    Err(error) => panic!(
+                        "parsing logs as JSON failed\n  error: {error}\n  chunk: {:?}",
+                        String::from_utf8_lossy(&chunk[..])
+                    ),
+                }
+            }
+        };
+        future::select(Box::pin(fut), done_rx.map(|_| ())).await;
+
+        result
+    });
+
+    (result, done_tx)
 }
