d75db6b60f65f6dfc521ff4a50f067c1f2d960ca
Oliver Gould <ver@buoyant.io>
2022-04-29 16:19:54 +0000

    Updated generated protobuf binding
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- opencensus-proto/src/gen/opencensus.proto.agent.trace.v1.rs
+++ opencensus-proto/src/gen/opencensus.proto.agent.trace.v1.rs
@@ -56,7 +56,7 @@ pub mod trace_service_client {
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
         T::Error: Into<StdError>,
-        T::ResponseBody: Default + Body<Data = Bytes> + Send + 'static,
+        T::ResponseBody: Body<Data = Bytes> + Send + 'static,
         <T::ResponseBody as Body>::Error: Into<StdError> + Send,
     {
         pub fn new(inner: T) -> Self {
@@ -69,6 +69,7 @@ pub mod trace_service_client {
         ) -> TraceServiceClient<InterceptedService<T, F>>
         where
             F: tonic::service::Interceptor,
+            T::ResponseBody: Default,
             T: tonic::codegen::Service<
                 http::Request<tonic::body::BoxBody>,
                 Response = http::Response<
