35306ab56b266c798bfd410d3faeefad6a29c3cb
Eliza Weisman <eliza@buoyant.io>
2022-05-31 10:45:59 -0700

    http-box: add `EraseResponse` middleware
    The `linkerd-http-box` crate currently contains `BoxRequest` and
    `BoxResponse` middleware for boxing request/response bodies, and an
    `EraseRequest` middleware for erasing the type of request bodies, for
    use in a stack which must be able to serve *multiple* request body
    types. However, there is no corresponding `EraseResponse` middleware.
    
    This branch adds an `EraseResponse` middleware in `linkerd-http-box`.
    This is necessary for PR #1706, but I factored this out into its own
    branch as it's a fairly self-contained change.

--- linkerd/http-box/src/lib.rs
+++ linkerd/http-box/src/lib.rs
@@ -3,12 +3,14 @@
 
 mod body;
 mod erase_request;
+mod erase_response;
 mod request;
 mod response;
 
 pub use self::{
     body::{BoxBody, Data},
     erase_request::EraseRequest,
+    erase_response::EraseResponse,
     request::BoxRequest,
     response::BoxResponse,
 };
