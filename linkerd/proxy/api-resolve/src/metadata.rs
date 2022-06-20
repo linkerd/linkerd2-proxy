ff47f8d4fb651ace6b811c2787f877721ed24b90
Oliver Gould <ver@buoyant.io>
2022-06-20 15:25:16 -0700

    Dedupe discovery updates (#1759)
    The proxy can receive redundant discovery updates. When this occurs, it
    causes the balancer to churn, replacing an endpoint stack (and
    therefore needlessly dropping a connection).
    
    This change updates the discovery module to keep clones of the
    discovered endpoint metadata, so that updated values can be compared to
    eliminate duplicate updates.
    
    Relates to linkerd/linkerd2#8677

--- linkerd/proxy/api-resolve/src/metadata.rs
+++ linkerd/proxy/api-resolve/src/metadata.rs
@@ -2,7 +2,7 @@ use http::uri::Authority;
 use linkerd_tls::client::ServerId;
 use std::collections::BTreeMap;
 
-/// Endpoint labels are lexographically ordered by key.
+/// Endpoint labels are lexigraphically ordered by key.
 pub type Labels = std::sync::Arc<BTreeMap<String, String>>;
 
 /// Metadata describing an endpoint.
