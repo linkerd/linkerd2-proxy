1fc213529fa78dafb21500195d1a38ad00ceaa6d
Oliver Gould <ver@buoyant.io>
2022-06-21 19:56:02 -0700

    ci: Split fuzzer logic into a script (#1773)
    The fuzzer CI embeds a somewhat complicated script. This change splits
    it into a distinct file so that it can be shellchecked, etc.
    
    Signed-off-by: Oliver Gould <ver@buoyant.io>

---
+++ .github/fuzzers-list.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+## Lists the fuzzers that should be run given a set of changed files.
+
+# Find the nearest fuzzer crate, or nothing.
+find_fuzz_dir() {
+    d=${1%/*}
+    if [ "$d" = . ]; then
+        return
+    elif [ -d "$d" ] && [[ "$d" = */fuzz ]]; then
+        echo "$d"
+    elif [ -f "$d/fuzz/Cargo.toml" ]; then
+        echo "$d/fuzz"
+    else
+        find_fuzz_dir "$d"
+    fi
+}
+
+for file in "$@" ; do
+    if [ "$file" = .github/workflows/fuzzers.yml ] || [ "$file" = .github/fuzzers-list.sh ]; then
+        find * -type d -name fuzz
+        break
+    fi
+    find_fuzz_dir "$file"
+done | sort -u
