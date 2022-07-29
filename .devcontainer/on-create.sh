ee3d931b0cf940d4f56a96138aec758e99e72515
Oliver Gould <ver@buoyant.io>
2022-07-29 15:37:03 +0000

    Use an onCreateCommand to setup cargo-fuzz, linkerd
    Signed-off-by: Oliver Gould <ver@buoyant.io>

---
+++ .devcontainer/on-create.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rustup toolchain install --profile=minimal nightly
+cargo +nightly install cargo-fuzz
+
+scurl https://run.linkerd.io/install-edge | sh
+mkdir -p "$HOME/bin"
+(cd "$HOME/bin" && ln -s "$HOME/.linkerd2/bin/linkerd")
