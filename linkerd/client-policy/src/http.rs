6a2a352ee05fac13631a47a9911ab9c5eb081575
Eliza Weisman <eliza@buoyant.io>
2022-11-10 10:25:45 -0800

    add quick internal repr  of client policy types

---
+++ linkerd/client-policy/src/http.rs
@@ -0,0 +1,89 @@
+use crate::RoutePolicy;
+use linkerd_http_route::http;
+pub use linkerd_http_route::http::{filter, r#match, RouteMatch};
+
+pub type Route = http::Route<RoutePolicy>;
+pub type Rule = http::Rule<RoutePolicy>;
+
+#[inline]
+pub fn find<'r, B>(
+    routes: &'r [Route],
+    req: &::http::Request<B>,
+) -> Option<(http::RouteMatch, &'r RoutePolicy)> {
+    http::find(routes, req)
+}
+
+pub fn default() -> Route {
+    Route {
+        hosts: vec![],
+        rules: vec![Rule {
+            matches: vec![],
+            policy: RoutePolicy {
+                backends: vec![],
+                meta: crate::Meta::new_default("default"),
+            },
+        }],
+    }
+}
+
+#[cfg(feature = "proto")]
+pub mod proto {
+    use super::*;
+    use crate::{meta::proto::InvalidMeta, proto::InvalidBackend, Backend, Meta};
+    use linkerd2_proxy_api::outbound as api;
+    use linkerd_http_route::http::r#match::{
+        host::proto::InvalidHostMatch, proto::InvalidRouteMatch,
+    };
+    use std::sync::Arc;
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum InvalidHttpRoute {
+        #[error("invalid host match: {0}")]
+        HostMatch(#[from] InvalidHostMatch),
+
+        #[error("invalid route match: {0}")]
+        RouteMatch(#[from] InvalidRouteMatch),
+        #[error("invalid labels: {0}")]
+        Meta(#[from] InvalidMeta),
+        #[error("invalid backend: {0}")]
+        Backend(#[from] InvalidBackend),
+    }
+
+    pub fn try_route(proto: api::HttpRoute) -> Result<Route, InvalidHttpRoute> {
+        let api::HttpRoute {
+            hosts,
+            rules,
+            metadata,
+        } = proto;
+
+        let hosts = hosts
+            .into_iter()
+            .map(r#match::MatchHost::try_from)
+            .collect::<Result<Vec<_>, InvalidHostMatch>>()?;
+
+        let meta = Arc::new(Meta::try_from(metadata.ok_or(InvalidMeta::Missing)?)?);
+        let rules = rules
+            .into_iter()
+            .map(|r| try_rule(meta.clone(), r))
+            .collect::<Result<Vec<_>, InvalidHttpRoute>>()?;
+
+        Ok(Route { hosts, rules })
+    }
+
+    fn try_rule(meta: Arc<Meta>, proto: api::http_route::Rule) -> Result<Rule, InvalidHttpRoute> {
+        let api::http_route::Rule { matches, backends } = proto;
+        let matches = matches
+            .into_iter()
+            .map(r#match::MatchRequest::try_from)
+            .collect::<Result<Vec<_>, InvalidRouteMatch>>()?;
+        let backends = backends
+            .into_iter()
+            .map(Backend::try_from)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Rule {
+            matches,
+            policy: RoutePolicy { backends, meta },
+        })
+    }
+}
---
+++ linkerd/client-policy/src/lib.rs
@@ -0,0 +1,71 @@
+#![deny(rust_2018_idioms, clippy::disallowed_methods, clippy::disallowed_types)]
+#![forbid(unsafe_code)]
+
+use std::sync::Arc;
+pub mod http;
+use linkerd_addr::Addr;
+pub use linkerd_policy_core::{meta, Meta};
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ClientPolicy {
+    pub protocol: Protocol,
+    pub meta: Arc<Meta>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Protocol {
+    Http1(Arc<[http::Route]>),
+    Http2(Arc<[http::Route]>),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct RoutePolicy {
+    pub backends: Vec<Backend>,
+    pub meta: Arc<Meta>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Backend {
+    pub weight: u32,
+    pub addr: Addr,
+}
+
+#[cfg(feature = "proto")]
+pub mod proto {
+    use super::*;
+    use linkerd2_proxy_api::{destination, net, outbound as api};
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum InvalidBackend {
+        #[error("empty backend")]
+        EmptyBackend,
+        #[error("invalid destination address: {0}")]
+        HostMatch(#[from] linkerd_addr::Error),
+        #[error("WeightedAddr missing TCP address")]
+        NoTcpAddr,
+        #[error("invalid TCP address: {0}")]
+        InvalidTcpAddr(#[from] net::InvalidIpAddress),
+    }
+
+    impl TryFrom<api::Backend> for Backend {
+        type Error = InvalidBackend;
+        fn try_from(backend: api::Backend) -> Result<Self, Self::Error> {
+            let backend = backend.backend.ok_or(InvalidBackend::EmptyBackend)?;
+            let (addr, weight) = match backend {
+                api::backend::Backend::Dst(destination::WeightedDst { authority, weight }) => {
+                    let addr = authority.parse::<Addr>()?;
+                    (addr, weight)
+                }
+                api::backend::Backend::Endpoint(destination::WeightedAddr {
+                    addr, weight, ..
+                }) => {
+                    // TODO(eliza): what do we do with the rest of the stuff in
+                    // `WeightedAddr`?
+                    let addr = addr.ok_or(InvalidBackend::NoTcpAddr)?.try_into()?;
+                    (Addr::Socket(addr), weight)
+                }
+            };
+            Ok(Self { weight, addr })
+        }
+    }
+}
