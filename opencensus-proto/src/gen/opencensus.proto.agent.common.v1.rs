d6735a18e9c00ff809c9eadda3b931447e3c9a1f
Eliza Weisman <eliza@buoyant.io>
2022-11-29 10:42:23 -0800

    also regen opencensus protos

--- opencensus-proto/src/gen/opencensus.proto.agent.common.v1.rs
+++ opencensus-proto/src/gen/opencensus.proto.agent.common.v1.rs
@@ -5,48 +5,61 @@
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Node {
     /// Identifier that uniquely identifies a process within a VM/container.
-    #[prost(message, optional, tag="1")]
+    #[prost(message, optional, tag = "1")]
     pub identifier: ::core::option::Option<ProcessIdentifier>,
     /// Information on the OpenCensus Library that initiates the stream.
-    #[prost(message, optional, tag="2")]
+    #[prost(message, optional, tag = "2")]
     pub library_info: ::core::option::Option<LibraryInfo>,
     /// Additional information on service.
-    #[prost(message, optional, tag="3")]
+    #[prost(message, optional, tag = "3")]
     pub service_info: ::core::option::Option<ServiceInfo>,
     /// Additional attributes.
-    #[prost(map="string, string", tag="4")]
-    pub attributes: ::std::collections::HashMap<::prost::alloc::string::String, ::prost::alloc::string::String>,
+    #[prost(map = "string, string", tag = "4")]
+    pub attributes: ::std::collections::HashMap<
+        ::prost::alloc::string::String,
+        ::prost::alloc::string::String,
+    >,
 }
 /// Identifier that uniquely identifies a process within a VM/container.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ProcessIdentifier {
     /// The host name. Usually refers to the machine/container name.
     /// For example: os.Hostname() in Go, socket.gethostname() in Python.
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub host_name: ::prost::alloc::string::String,
     /// Process id.
-    #[prost(uint32, tag="2")]
+    #[prost(uint32, tag = "2")]
     pub pid: u32,
     /// Start time of this ProcessIdentifier. Represented in epoch time.
-    #[prost(message, optional, tag="3")]
+    #[prost(message, optional, tag = "3")]
     pub start_timestamp: ::core::option::Option<::prost_types::Timestamp>,
 }
 /// Information on OpenCensus Library.
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct LibraryInfo {
     /// Language of OpenCensus Library.
-    #[prost(enumeration="library_info::Language", tag="1")]
+    #[prost(enumeration = "library_info::Language", tag = "1")]
     pub language: i32,
     /// Version of Agent exporter of Library.
-    #[prost(string, tag="2")]
+    #[prost(string, tag = "2")]
     pub exporter_version: ::prost::alloc::string::String,
     /// Version of OpenCensus Library.
-    #[prost(string, tag="3")]
+    #[prost(string, tag = "3")]
     pub core_library_version: ::prost::alloc::string::String,
 }
 /// Nested message and enum types in `LibraryInfo`.
 pub mod library_info {
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+    #[derive(
+        Clone,
+        Copy,
+        Debug,
+        PartialEq,
+        Eq,
+        Hash,
+        PartialOrd,
+        Ord,
+        ::prost::Enumeration
+    )]
     #[repr(i32)]
     pub enum Language {
         Unspecified = 0,
@@ -87,6 +100,6 @@ pub mod library_info {
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct ServiceInfo {
     /// Name of the service.
-    #[prost(string, tag="1")]
+    #[prost(string, tag = "1")]
     pub name: ::prost::alloc::string::String,
 }
