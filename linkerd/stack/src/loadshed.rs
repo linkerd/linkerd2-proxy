cf8e6d85d58ab3748c3fe88b27716dfd09088f79
Eliza Weisman <eliza@buoyant.io>
2022-12-28 10:05:46 -0800

    add tests for LoadShed

--- linkerd/stack/src/loadshed.rs
+++ linkerd/stack/src/loadshed.rs
@@ -20,23 +20,32 @@ pub struct LoadShedError(());
 // === impl LoadShed ===
 
 impl<S> LoadShed<S> {
-    /// Fails requests when the inner service is not ready.
+    /// Returns a `Layer` that fails requests when the inner service is not
+    /// ready.
     ///
     /// Innner services MUST be responsible for driving themselves to ready
     /// (e.g., via [`SpawnReady`], where appropriate).
     ///
     /// [`SpawnReady`]: tower::spawn_ready::SpawnReady
     pub fn layer() -> impl layer::Layer<S, Service = Self> + Copy + Clone {
-        layer::mk(|inner| Self { inner, open: true })
+        layer::mk(Self::new)
+    }
+
+    /// Fails requests when the inner service is not
+    /// ready.
+    ///
+    /// Innner services MUST be responsible for driving themselves to ready
+    /// (e.g., via [`SpawnReady`], where appropriate).
+    ///
+    /// [`SpawnReady`]: tower::spawn_ready::SpawnReady
+    pub fn new(inner: S) -> Self {
+        Self { inner, open: true }
     }
 }
 
 impl<S: Clone> Clone for LoadShed<S> {
     fn clone(&self) -> Self {
-        Self {
-            inner: self.inner.clone(),
-            open: true,
-        }
+        Self::new(self.inner.clone())
     }
 }
 
@@ -79,7 +88,64 @@ where
         if self.open {
             future::Either::Left(self.inner.call(req).map_err(Into::into))
         } else {
+            tracing::debug!("Service shedding load");
             future::Either::Right(future::err(LoadShedError(()).into()))
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio_test::{assert_ready_err, assert_ready_ok};
+    use tower_test::mock::{self, Spawn};
+
+    #[tokio::test]
+    async fn sheds_load() {
+        let _trace = linkerd_tracing::test::trace_init();
+        let (service, mut handle) = mock::pair::<(), ()>();
+        let mut service = Spawn::new(LoadShed::new(service));
+
+        // The inner service accepts one request.
+        handle.allow(1);
+        assert_ready_ok!(service.poll_ready());
+
+        let call = service.call(());
+        let (_, send_rsp) = handle
+            .next_request()
+            .await
+            .expect("should call inner service");
+        send_rsp.send_response(());
+        call.await.expect("should succeed");
+
+        // Then the inner service becomes unavailable.
+        assert_ready_ok!(
+            service.poll_ready(),
+            "load shed service should always be ready"
+        );
+        service.call(()).await.expect_err("should shed load");
+
+        // The inner service becomes ready again, so it should no longer shed load.
+        handle.allow(1);
+        assert_ready_ok!(service.poll_ready());
+
+        let call = service.call(());
+        let (_, send_rsp) = handle
+            .next_request()
+            .await
+            .expect("should call inner service");
+        send_rsp.send_response(());
+        call.await.expect("should succeed");
+    }
+
+    #[tokio::test]
+    async fn propagates_inner_error() {
+        let _trace = linkerd_tracing::test::trace_init();
+        let (service, mut handle) = mock::pair::<(), ()>();
+        let mut service = Spawn::new(LoadShed::new(service));
+
+        // The inner service errors.
+        handle.send_error("service machine broke");
+        assert_ready_err!(service.poll_ready());
     }
 }
