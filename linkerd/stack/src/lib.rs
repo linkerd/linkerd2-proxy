1d989b89a8c5a7ae2c85b0aec85c7a045101455f
Eliza Weisman <eliza@buoyant.io>
2023-01-20 10:43:09 -0800

    module visibility tweaks

--- linkerd/stack/src/lib.rs
+++ linkerd/stack/src/lib.rs
@@ -23,7 +23,7 @@ pub mod monitor;
 pub mod new_service;
 mod on_service;
 pub mod proxy;
-pub mod queue;
+mod queue;
 mod result;
 mod switch_ready;
 mod timeout;
@@ -50,6 +50,7 @@ pub use self::{
     new_service::{NewCloneService, NewService},
     on_service::{OnService, OnServiceLayer},
     proxy::Proxy,
+    queue::{NewQueue, QueueParams},
     result::ResultService,
     switch_ready::{NewSwitchReady, SwitchReady},
     timeout::{Timeout, TimeoutError},
