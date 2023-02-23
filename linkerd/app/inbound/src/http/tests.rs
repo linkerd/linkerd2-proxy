89d3d8ea89dd03dbe1471d9c5713b6ce26d09832
Eliza Weisman <eliza@buoyant.io>
2023-02-23 11:16:36 -0800

    test fixy

--- linkerd/app/inbound/src/http/tests.rs
+++ linkerd/app/inbound/src/http/tests.rs
@@ -578,11 +578,15 @@ impl Target {
     fn addr() -> SocketAddr {
         ([127, 0, 0, 1], 80).into()
     }
+
+    fn dst_addr() -> SocketAddr {
+        ([192, 0, 2, 2], 80).into()
+    }
 }
 
 impl svc::Param<OrigDstAddr> for Target {
     fn param(&self) -> OrigDstAddr {
-        OrigDstAddr(([192, 0, 2, 2], 80).into())
+        OrigDstAddr(Self::dst_addr())
     }
 }
 
@@ -622,7 +626,7 @@ impl svc::Param<policy::AllowPolicy> for Target {
             }),
         }]);
         let (policy, _) = policy::AllowPolicy::for_test(
-            self.param(),
+            ServerAddr(Self::dst_addr()),
             policy::ServerPolicy {
                 protocol: policy::Protocol::Http1(Arc::new([
                     linkerd_proxy_server_policy::http::default(authorizations),
