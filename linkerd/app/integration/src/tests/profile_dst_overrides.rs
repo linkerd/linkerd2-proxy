a1fa6a37d87a9a22746d24e49a04059d4f8f3023
Eliza Weisman <eliza@buoyant.io>
2023-03-10 11:49:06 -0800

    fix dst overrides

--- linkerd/app/integration/src/tests/profile_dst_overrides.rs
+++ linkerd/app/integration/src/tests/profile_dst_overrides.rs
@@ -91,24 +91,14 @@ async fn add_a_dst_override() {
 
     let n = 100;
 
-    // 1. Send `n` requests to apex service
-    profile_tx.send(pb::DestinationProfile {
-        fully_qualified_name: "apex.svc.cluster.local".into(),
-        ..Default::default()
-    });
-    for _ in 0..n {
-        assert_eq!(client.get("/").await, apex);
-    }
-    assert_eq!(apex_svc.response_counter.load(Ordering::SeqCst), n);
-
-    // 2. Add dst override
+    // 1. Add dst override
     profile_tx.send(profile(
         "override",
         vec![controller::dst_override(leaf_svc.authority(), 10000)],
     ));
     wait_for_profile_stage(&client, &metrics, "override").await;
 
-    // 3. Send `n` requests to apex service with override
+    // 2. Send `n` requests to apex service with override
     apex_svc.response_counter.store(0, Ordering::SeqCst);
     for _ in 0..n {
         assert_eq!(client.get("/").await, leaf);
@@ -137,10 +127,13 @@ async fn add_multiple_dst_overrides() {
         .send_addr(leaf_b_svc.svc.addr);
 
     let profile_tx = ctrl.profile_tx(apex_svc.addr().to_string());
-    profile_tx.send(pb::DestinationProfile {
-        fully_qualified_name: "apex.svc.cluster.local".into(),
-        ..Default::default()
-    });
+    profile_tx.send(profile(
+        "overrides",
+        vec![
+            controller::dst_override(leaf_a_svc.authority(), 5000),
+            controller::dst_override(leaf_b_svc.authority(), 5000),
+        ],
+    ));
 
     let proxy = proxy::new()
         .controller(ctrl.run().await)
@@ -153,23 +146,9 @@ async fn add_multiple_dst_overrides() {
 
     let n = 100;
 
-    // 1. Send `n` requests to apex service
-    for _ in 0..n {
-        assert_eq!(client.get("/").await, apex);
-    }
-    assert_eq!(apex_svc.response_counter.load(Ordering::SeqCst), n);
-
-    // 2. Add dst overrides
-    profile_tx.send(profile(
-        "overrides",
-        vec![
-            controller::dst_override(leaf_a_svc.authority(), 5000),
-            controller::dst_override(leaf_b_svc.authority(), 5000),
-        ],
-    ));
     wait_for_profile_stage(&client, &metrics, "overrides").await;
 
-    // 3. Send `n` requests to apex service with overrides
+    // Send `n` requests to apex service with overrides
     apex_svc.response_counter.store(0, Ordering::SeqCst);
     for _ in 0..n {
         let rsp = client.get("/").await;
