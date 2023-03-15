d94185611899d01e8f6c333348f217504334fa5c
Eliza Weisman <eliza@buoyant.io>
2023-03-15 12:20:58 -0700

    fixup

--- linkerd/app/gateway/src/discover.rs
+++ linkerd/app/gateway/src/discover.rs
@@ -1,11 +1,11 @@
 use crate::Gateway;
 use futures::FutureExt;
-use linkerd_app_core::{errors, profiles, svc, Error, NameAddr};
+use linkerd_app_core::{errors, profiles, svc, Error};
 use linkerd_app_inbound::{GatewayAddr, GatewayDomainInvalid};
 use linkerd_app_outbound::{self as outbound};
-use linkerd_proxy_client_policy::{self as policy};
+use linkerd_proxy_client_policy::{self as policy, ClientPolicy};
 use once_cell::sync::Lazy;
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 use tokio::sync::watch;
 use tracing::Instrument;
 
@@ -33,9 +33,9 @@ impl Gateway {
         use futures::future;
 
         let allowlist = self.config.allow_discovery.clone();
-        let detect_timeout = self.config.proxy.detect_protocol_timeout;
+        let detect_timeout = self.outbound.config().proxy.detect_protocol_timeout;
         let queue = {
-            let queue = self.config.outbound.tcp_connection_queue;
+            let queue = self.outbound.config().tcp_connection_queue;
             policy::Queue {
                 capacity: queue.capacity,
                 failfast_timeout: queue.failfast_timeout,
@@ -66,43 +66,41 @@ impl Gateway {
                 // If there was a policy resolution, return it with the profile so
                 // the stack can determine how to switch on them.
                 match policy {
-                    Ok(policy) => Ok((Some(profile), policy)),
+                    Ok(policy) => return Ok((Some(profile), policy)),
                     // The policy controller currently rejects discovery for pod
                     // names (rather than service names) so we'll need to
                     // synthesize a policy.
-                    Err(error) if is_not_found(&error) => {
-                        tracing::debug!("Policy not found");
-                        let policy = outbound::discover::spawn_synthesized_profile_policy(
-                            profile.clone().into(),
-                            move |profile| {
-                                static META: Lazy<Arc<policy::Meta>> = Lazy::new(|| {
-                                    Arc::new(policy::Meta::Default {
-                                        name: "gateway".into(),
-                                    })
-                                });
-
-                                match profile.endpoint.clone() {
-                                    Some((addr, meta)) => ClientPolicy::synthesize_forward(
-                                        &META,
-                                        detect_timeout,
-                                        queue,
-                                        addr,
-                                        meta,
-                                    ),
-                                    None => {
-                                        // TODO(eliza): should we synthesize a policy from the nameaddr?
-                                        tracing::debug!(
-                                            "Gateway ServiceProfile does not contain an endpoint"
-                                        );
-                                        ClientPolicy::invalid(detect_timeout)
-                                    }
-                                }
-                            },
-                        );
-                        Ok((Some(profile), policy))
-                    }
-                    Err(error) => Err(error),
+                    Err(error) if is_not_found(&error) => tracing::debug!("Policy not found"),
+                    Err(error) => return Err(error),
                 }
+
+                let policy = outbound::discover::spawn_synthesized_profile_policy(
+                    profile.clone().into(),
+                    move |profile| {
+                        static META: Lazy<Arc<policy::Meta>> = Lazy::new(|| {
+                            Arc::new(policy::Meta::Default {
+                                name: "gateway".into(),
+                            })
+                        });
+
+                        match profile.endpoint.clone() {
+                            Some((addr, meta)) => ClientPolicy::synthesize_forward(
+                                &META,
+                                detect_timeout,
+                                queue,
+                                addr,
+                                meta,
+                            ),
+                            None => {
+                                tracing::debug!(
+                                    "Gateway ServiceProfile does not contain an endpoint"
+                                );
+                                ClientPolicy::invalid(detect_timeout)
+                            }
+                        }
+                    },
+                );
+                Ok((Some(profile), policy))
             });
 
             future::Either::Right(discovery)
