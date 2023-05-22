704ef31a286280187c1f4679170e897243f99900
Alex Leong <alex@buoyant.io>
2023-05-22 13:40:37 -0700

    Classify grpc requests properly in the http classifier (#2410)
    The gRPC protocol always sets the HTTP response status code to 200 and instead communicates failures in a grpc-status header sent in a TRAILERS frame. Linkerd uses the HTTP response status code to determine if a response is successful, and therefore will consider all gRPC responses successful regardless of their gRPC status code. This means that functionality such as retries and circuit breaking do not function correctly with gRPC traffic.
    
    We update the Http classifier to look for the presence of a `Content-Type: application/grpc` header and use Grpc response classification when it is set.
    
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- linkerd/app/core/src/classify.rs
+++ linkerd/app/core/src/classify.rs
@@ -71,17 +71,16 @@ impl classify::Classify for Request {
         match self {
             Self::Profile(classes) => Response::Profile(classes.clone()),
 
-            Self::ClientPolicy(ClientPolicy::Http(policy)) => Response::Http(policy.clone()),
+            Self::ClientPolicy(ClientPolicy::Http(policy)) => {
+                if is_grpc(req.headers()) {
+                    return Response::Grpc(Default::default());
+                }
+                Response::Http(policy.clone())
+            }
             Self::ClientPolicy(ClientPolicy::Grpc(policy)) => Response::Grpc(policy.clone()),
 
             Self::Default => {
-                let is_grpc = req
-                    .headers()
-                    .get(http::header::CONTENT_TYPE)
-                    .and_then(|v| v.to_str().ok())
-                    .map(|ct| ct.starts_with("application/grpc"))
-                    .unwrap_or(false);
-                if is_grpc {
+                if is_grpc(req.headers()) {
                     return Response::Grpc(Default::default());
                 }
                 Response::default()
@@ -222,6 +221,13 @@ fn h2_error(err: &Error) -> String {
     }
 }
 
+fn is_grpc(hdrs: &http::HeaderMap) -> bool {
+    hdrs.get(http::header::CONTENT_TYPE)
+        .and_then(|v| v.to_str().ok())
+        .map(|ct| ct.starts_with("application/grpc"))
+        .unwrap_or(false)
+}
+
 // === impl Class ===
 
 impl Class {
@@ -242,8 +248,11 @@ impl Class {
 #[cfg(test)]
 mod tests {
     use super::Class;
-    use http::{HeaderMap, Response, StatusCode};
-    use linkerd_proxy_http::classify::{ClassifyEos, ClassifyResponse};
+    use http::{HeaderMap, Request, Response, StatusCode};
+    use linkerd_proxy_http::{
+        classify::{ClassifyEos, ClassifyResponse},
+        Classify,
+    };
 
     #[test]
     fn http_response_status_ok() {
@@ -341,6 +350,40 @@ mod tests {
         let class = super::Response::Profile(Default::default())
             .start(&rsp)
             .eos(Some(&trailers));
+        assert_eq!(class, Class::Grpc(Err(tonic::Code::DeadlineExceeded)));
+    }
+
+    #[test]
+    fn grpc_over_http_trailer_ok() {
+        let req = Request::builder()
+            .header(http::header::CONTENT_TYPE, "application/grpc+proto")
+            .body(())
+            .unwrap();
+        let rsp = Response::builder().status(StatusCode::OK).body(()).unwrap();
+        let mut trailers = HeaderMap::new();
+        trailers.insert("grpc-status", 0.into());
+        let class = super::Request::ClientPolicy(super::ClientPolicy::Http(Default::default()))
+            .classify(&req)
+            .start(&rsp)
+            .eos(Some(&trailers));
+
+        assert_eq!(class, Class::Grpc(Ok(tonic::Code::Ok)));
+    }
+
+    #[test]
+    fn grpc_over_http_trailer_error() {
+        let req = Request::builder()
+            .header(http::header::CONTENT_TYPE, "application/grpc+proto")
+            .body(())
+            .unwrap();
+        let rsp = Response::builder().status(StatusCode::OK).body(()).unwrap();
+        let mut trailers = HeaderMap::new();
+        trailers.insert("grpc-status", 4.into());
+        let class = super::Request::ClientPolicy(super::ClientPolicy::Http(Default::default()))
+            .classify(&req)
+            .start(&rsp)
+            .eos(Some(&trailers));
+
         assert_eq!(class, Class::Grpc(Err(tonic::Code::DeadlineExceeded)));
     }
 }
