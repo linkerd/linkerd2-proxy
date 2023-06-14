8ae551e19e171d12c29daad20bf80287a03eb3e4
Eliza Weisman <eliza@buoyant.io>
2023-06-14 07:54:35 -0700

    okay that's mostly good

--- linkerd/app/outbound/src/ingress.rs
+++ linkerd/app/outbound/src/ingress.rs
@@ -5,6 +5,7 @@ use linkerd_app_core::{
     proxy::{
         api_resolve::{ConcreteAddr, Metadata},
         core::Resolve,
+        http::balance,
     },
     svc::{self, ServiceExt},
     transport::addrs::*,
@@ -81,95 +82,7 @@ impl Outbound<()> {
         // Endpoint resolver.
         R: Resolve<ConcreteAddr, Endpoint = Metadata, Error = Error>,
     {
-        let discover = {
-            let detect_timeout = self.config.proxy.detect_protocol_timeout;
-            let queue = {
-                let queue = self.config.tcp_connection_queue;
-                policy::Queue {
-                    capacity: queue.capacity,
-                    failfast_timeout: queue.failfast_timeout,
-                }
-            };
-            svc::mk(move |DiscoverAddr(addr)| {
-                tracing::debug!(%addr, "Discover");
-
-                let profile = profiles
-                    .clone()
-                    .get_profile(profiles::LookupAddr(addr))
-                    .instrument(tracing::debug_span!("profiles"));
-                let policy = policies
-                    .get_policy(addr)
-                    .instrument(tracing::debug_span!("policy"));
-
-                Box::pin(async move {
-                    let (profile, policy) = tokio::join!(profile, policy);
-                    tracing::debug!("Discovered");
-
-                    let profile = profile.unwrap_or_else(|error| {
-                        tracing::warn!(%error, "Error resolving ServiceProfile");
-                        None
-                    });
-
-                    // If there was a policy resolution, return it with the profile so
-                    // the stack can determine how to switch on them.
-                    let policy_error = match policy {
-                        Ok(policy) => return Ok((profile, policy)),
-                        // XXX(ver) The policy controller may (for the time being) reject
-                        // our lookups, since it doesn't yet serve endpoint metadata for
-                        // forwarding.
-                        Err(error) if errors::has_grpc_status(&error, tonic::Code::NotFound) => {
-                            tracing::debug!("Policy not found");
-                            error
-                        }
-                        // Earlier versions of the Linkerd control plane (e.g.
-                        // 2.12.x) will return `Unimplemented` for requests to the
-                        // OutboundPolicy API. Log a warning and synthesize a policy
-                        // for backwards compatibility.
-                        Err(error)
-                            if errors::has_grpc_status(&error, tonic::Code::Unimplemented) =>
-                        {
-                            tracing::warn!("Policy controller returned `Unimplemented`, the control plane may be out of date.");
-                            error
-                        }
-                        Err(error) => return Err(error),
-                    };
-
-                    // If there was a profile resolution, try to use it to synthesize a
-                    // enpdoint policy.
-                    if let Some(profile) = profile {
-                        let policy = crate::discover::spawn_synthesized_profile_policy(
-                            profile.clone().into(),
-                            move |profile: &profiles::Profile| {
-                                static META: Lazy<Arc<policy::Meta>> = Lazy::new(|| {
-                                    Arc::new(policy::Meta::Default {
-                                        name: "endpoint".into(),
-                                    })
-                                });
-                                let (addr, meta) = match profile.endpoint.clone() {
-                                    Some(ep) => ep,
-                                    // XXX(eliza): we probably need to
-                                    // handle this...
-                                    None => return Err(policy_error),
-                                };
-                                // TODO(ver) We should be able to figure out resource coordinates for
-                                // the endpoint?
-                                crate::synthesize_forward_policy(
-                                    &META,
-                                    detect_timeout,
-                                    queue,
-                                    addr,
-                                    meta,
-                                )
-                            },
-                        );
-                        return Ok((Some(profile), policy));
-                    }
-
-                    // Otherwise, return an error.
-                    return Err(policy_error);
-                })
-            })
-        };
+        let discover = self.ingress_resolver(profiles, policies);
 
         // The fallback stack is the same thing as the normal proxy stack, but
         // it doesn't include TCP metrics, since they are already instrumented
@@ -200,6 +113,123 @@ impl Outbound<()> {
         http.push_ingress(opaque)
             .push_tcp_instrument(|t: &T| tracing::info_span!("ingress", addr = %t.param()))
             .into_inner()
+    }
+
+    fn ingress_resolver(
+        &self,
+        profiles: impl profiles::GetProfile<Error = Error>,
+        policies: impl policy::GetPolicy,
+    ) -> impl svc::Service<
+        DiscoverAddr,
+        Error = Error,
+        Response = (Option<profiles::Receiver>, policy::Receiver),
+        Future = impl Send,
+    > + Clone
+           + Send
+           + Sync
+           + 'static {
+        let detect_timeout = self.config.proxy.detect_protocol_timeout;
+        let queue = {
+            let queue = self.config.tcp_connection_queue;
+            policy::Queue {
+                capacity: queue.capacity,
+                failfast_timeout: queue.failfast_timeout,
+            }
+        };
+        let load = {
+            let balance::EwmaConfig { decay, default_rtt } =
+                crate::http::logical::profile::DEFAULT_EWMA;
+            policy::Load::PeakEwma(policy::PeakEwma { decay, default_rtt })
+        };
+        svc::mk(move |DiscoverAddr(addr)| {
+            tracing::debug!(%addr, "Discover");
+
+            let profile = profiles
+                .clone()
+                .get_profile(profiles::LookupAddr(addr.clone()))
+                .instrument(tracing::debug_span!("profiles"));
+            let policy = policies
+                .get_policy(addr)
+                .instrument(tracing::debug_span!("policy"));
+
+            Box::pin(async move {
+                let (profile, policy) = tokio::join!(profile, policy);
+                tracing::debug!("Discovered");
+
+                let profile = profile.unwrap_or_else(|error| {
+                    tracing::warn!(%error, "Error resolving ServiceProfile");
+                    None
+                });
+
+                // If there was a policy resolution, return it with the profile so
+                // the stack can determine how to switch on them.
+                let policy_error = match policy {
+                    Ok(policy) => return Ok((profile, policy)),
+                    // XXX(ver) The policy controller may (for the time being) reject
+                    // our lookups, since it doesn't yet serve endpoint metadata for
+                    // forwarding.
+                    Err(error) if errors::has_grpc_status(&error, tonic::Code::NotFound) => {
+                        tracing::debug!("Policy not found");
+                        error
+                    }
+                    // Earlier versions of the Linkerd control plane (e.g.
+                    // 2.12.x) will return `Unimplemented` for requests to the
+                    // OutboundPolicy API. Log a warning and synthesize a policy
+                    // for backwards compatibility.
+                    Err(error) if errors::has_grpc_status(&error, tonic::Code::Unimplemented) => {
+                        tracing::warn!("Policy controller returned `Unimplemented`, the control plane may be out of date.");
+                        error
+                    }
+                    Err(error) => return Err(error),
+                };
+
+                // If there was a profile resolution, try to use it to synthesize a
+                // policy.
+                if let Some(profile) = profile {
+                    let policy = crate::discover::spawn_synthesized_profile_policy(
+                        profile.clone().into(),
+                        move |profile: &profiles::Profile| {
+                            static ENDPOINT_META: Lazy<Arc<policy::Meta>> = Lazy::new(|| {
+                                Arc::new(policy::Meta::Default {
+                                    name: "endpoint".into(),
+                                })
+                            });
+                            static PROFILE_META: Lazy<Arc<policy::Meta>> = Lazy::new(|| {
+                                Arc::new(policy::Meta::Default {
+                                    name: "profile".into(),
+                                })
+                            });
+
+                            if let Some((addr, meta)) = profile.endpoint.clone() {
+                                return crate::discover::synthesize_forward_policy(
+                                    &ENDPOINT_META,
+                                    detect_timeout,
+                                    queue,
+                                    addr,
+                                    meta,
+                                );
+                            }
+
+                            if let Some(ref logical) = profile.addr {
+                                return crate::discover::synthesize_balance_policy(
+                                    &PROFILE_META,
+                                    detect_timeout,
+                                    queue,
+                                    load,
+                                    logical,
+                                );
+                            }
+
+                            todo!("eliza: orig dst? needs to fail");
+                        },
+                    );
+                    return Ok((Some(profile), policy));
+                }
+
+                // Otherwise, return an error.
+                return Err(policy_error);
+            })
+        })
     }
 }
 
