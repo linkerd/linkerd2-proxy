a1d3c79af3d59d59d720d794604d1e128977e21c
Scott Fleener <scott@buoyant.io>
2024-09-23 19:49:21 +0000

    Add OpenTelemetry exporter implementation
    This duplicates most of the logic from the existing OpenCensus exporter to keep the functionality as similar as possible.
    
    [#10111](linkerd/linkerd2#10111)
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

---
+++ linkerd/opentelemetry/src/metrics.rs
@@ -0,0 +1,58 @@
+use linkerd_metrics::{metrics, Counter, FmtMetrics};
+use std::fmt;
+use std::sync::Arc;
+
+metrics! {
+    opentelemetry_span_export_streams: Counter { "Total count of opened span export streams" },
+    opentelemetry_span_export_requests: Counter { "Total count of span export request messages" },
+    opentelemetry_span_exports: Counter { "Total count of spans exported" }
+}
+
+#[derive(Debug)]
+struct Metrics {
+    streams: Counter,
+    requests: Counter,
+    spans: Counter,
+}
+
+#[derive(Clone, Debug)]
+pub struct Registry(Arc<Metrics>);
+
+#[derive(Clone, Debug)]
+pub struct Report(Arc<Metrics>);
+
+pub fn new() -> (Registry, Report) {
+    let metrics = Metrics {
+        streams: Counter::default(),
+        requests: Counter::default(),
+        spans: Counter::default(),
+    };
+    let shared = Arc::new(metrics);
+    (Registry(shared.clone()), Report(shared))
+}
+
+impl Registry {
+    pub fn start_stream(&mut self) {
+        self.0.streams.incr()
+    }
+
+    pub fn send(&mut self, spans: u64) {
+        self.0.requests.incr();
+        self.0.spans.add(spans);
+    }
+}
+
+impl FmtMetrics for Report {
+    fn fmt_metrics(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        opentelemetry_span_export_streams.fmt_help(f)?;
+        opentelemetry_span_export_streams.fmt_metric(f, &self.0.streams)?;
+
+        opentelemetry_span_export_requests.fmt_help(f)?;
+        opentelemetry_span_export_requests.fmt_metric(f, &self.0.requests)?;
+
+        opentelemetry_span_exports.fmt_help(f)?;
+        opentelemetry_span_exports.fmt_metric(f, &self.0.spans)?;
+
+        Ok(())
+    }
+}
