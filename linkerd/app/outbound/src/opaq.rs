linkerd/linkerd2-proxy

commit 7eadac4d9b2a3f009fabe6eedc9ccc47751f6dd7

feat(outbound): Use the policy API for opaq routing (#3306)
The OutboundPolicy API is able to express configuration for opaq routing, but
the proxy only currently uses the ServiceProfile to configure routing. To
support configuring routing via the Gateway API's TCPRoute, we need to begin
using the OutboundPolicy API to configure opaq routing without breaking
existing configurations that use profiles.

With this change, the outbound proxy is updated so that route configurations are
required for the proxy to handle outbound opaq traffic. Route configurations may
be provided by:

* The Profile API, when it returns an Endpoint, so that the proxy synthesizes a
  route configuration that forwards traffic to the endpoint.
* The Profile API, when it returns a traffic split configuration, so that the
  proxy synthesizes a route configuration that forwards traffic over backend
  services.
* The OutboundPolicy API when it returns opaq routes.

When no routes are returned by the API, the proxy fails to route the
connections.

Follow up changes will enhance this functionality with metrics and improved
metadata.

linkerd/app/outbound/src/opaq.rs
@@ -1,4 +1,4 @@
-use crate::{tcp, Outbound};
+use crate::{policy, service_meta, tcp, Outbound, ParentRef, UNKNOWN_META};
 use linkerd_app_core::{
     io,
     metrics::prom,
@@ -9,17 +9,19 @@ use linkerd_app_core::{
     },
     svc,
     transport::addrs::*,
-    Error,
+    Addr, Error,
 };
-use std::{fmt::Debug, hash::Hash};
+use once_cell::sync::Lazy;
+use std::{fmt::Debug, hash::Hash, sync::Arc};
+use tokio::sync::watch;
 
 mod concrete;
 mod logical;
 
-pub use self::logical::Logical;
+pub use self::logical::{Concrete, Logical, Routes};
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
-struct Opaq(Logical);
+struct Opaq<T>(T);
 
 #[derive(Clone, Debug, Default)]
 pub struct OpaqMetrics {
@@ -36,8 +38,8 @@ impl<C> Outbound<C> {
     pub fn push_opaq_cached<T, I, R>(self, resolve: R) -> Outbound<svc::ArcNewCloneTcp<T, I>>
     where
         // Opaque target
-        T: svc::Param<Logical>,
-        T: Clone + Send + Sync + 'static,
+        T: Clone + Debug + PartialEq + Eq + Hash + Send + Sync + 'static,
+        T: svc::Param<watch::Receiver<Routes>>,
         // Server-side connection
         I: io::AsyncRead + io::AsyncWrite + io::PeerAddr,
         I: Debug + Send + Sync + Unpin + 'static,
@@ -57,7 +59,7 @@ impl<C> Outbound<C> {
                 stk.push_new_idle_cached(config.discovery_idle_timeout)
                     // Use a dedicated target type to configure parameters for
                     // the opaque stack. It also helps narrow the cache key.
-                    .push_map_target(|t: T| Opaq(t.param()))
+                    .push_map_target(Opaq)
                     .arc_new_clone_tcp()
             })
     }
@@ -65,18 +67,12 @@ impl<C> Outbound<C> {
 
 // === impl Opaq ===
 
-impl svc::Param<Logical> for Opaq {
-    fn param(&self) -> Logical {
-        self.0.clone()
-    }
-}
-
-impl svc::Param<Option<profiles::Receiver>> for Opaq {
-    fn param(&self) -> Option<profiles::Receiver> {
-        match self.0.param() {
-            Logical::Route(_, rx) => Some(rx),
-            _ => None,
-        }
+impl<T> svc::Param<watch::Receiver<logical::Routes>> for Opaq<T>
+where
+    T: svc::Param<watch::Receiver<logical::Routes>>,
+{
+    fn param(&self) -> watch::Receiver<logical::Routes> {
+        self.0.param()
     }
 }
 
@@ -88,4 +84,169 @@ impl OpaqMetrics {
             concrete::BalancerMetrics::register(registry.sub_registry_with_prefix("balancer"));
         Self { balance }
     }
+}
+
+fn should_override_opaq_policy(
+    rx: &watch::Receiver<profiles::Profile>,
+) -> Option<profiles::LogicalAddr> {
+    let p = rx.borrow();
+    if p.has_targets() {
+        p.addr.clone()
+    } else {
+        None
+    }
+}
+/// Given both profiles and policy information, this function constructs `opaq::Routes``.
+/// The decision on whether profiles or policy should be used is made by inspecting the
+/// returned profiles and checking whether there are any targets defined. This is done
+/// in order to support traffic splits. Everything else should be delivered through client
+/// policy.
+pub fn routes_from_discovery(
+    addr: Addr,
+    profile: Option<profiles::Receiver>,
+    mut policy: policy::Receiver,
+) -> watch::Receiver<Routes> {
+    if let Some(mut profile) = profile.map(watch::Receiver::from) {
+        if let Some(paddr) = should_override_opaq_policy(&profile) {
+            tracing::debug!("Using ServiceProfile");
+            let init = routes_from_profile(paddr.clone(), &profile.borrow_and_update());
+
+            return spawn_routes(profile, init, move |profile: &profiles::Profile| {
+                Some(routes_from_profile(paddr.clone(), profile))
+            });
+        }
+    }
+
+    tracing::debug!("Using ClientPolicy routes");
+    let init = routes_from_policy(addr.clone(), &policy.borrow_and_update())
+        .expect("initial policy must be opaque");
+
+    spawn_routes(policy, init, move |policy: &policy::ClientPolicy| {
+        routes_from_policy(addr.clone(), policy)
+    })
+}
+
+fn routes_from_policy(addr: Addr, policy: &policy::ClientPolicy) -> Option<Routes> {
+    let routes = match policy.protocol {
+        policy::Protocol::Opaque(policy::opaq::Opaque { ref routes }) => routes.clone(),
+        // we support a detect stack to cover the case when we do detection and fallback to opaq
+        policy::Protocol::Detect { ref opaque, .. } => opaque.routes.clone(),
+        _ => {
+            tracing::info!("Ignoring a discovery update that changed a route from opaq");
+            return None;
+        }
+    };
+
+    Some(Routes {
+        logical: Logical {
+            addr,
+            meta: ParentRef(policy.parent.clone()),
+        },
+        routes,
+        backends: policy.backends.clone(),
+    })
+}
+
+fn routes_from_profile(
+    profiles::LogicalAddr(profiles_addr): profiles::LogicalAddr,
+    profile: &profiles::Profile,
+) -> Routes {
+    // TODO: make configurable
+    let queue = {
+        policy::Queue {
+            capacity: 100,
+            failfast_timeout: std::time::Duration::from_secs(3),
+        }
+    };
+
+    const EWMA: policy::Load = policy::Load::PeakEwma(policy::PeakEwma {
+        default_rtt: std::time::Duration::from_millis(30),
+        decay: std::time::Duration::from_secs(10),
+    });
+
+    // TODO(ver) use resource metadata from the profile response.
+    let parent_meta = service_meta(&profiles_addr).unwrap_or_else(|| UNKNOWN_META.clone());
+
+    let backends: Vec<(policy::RouteBackend<policy::opaq::Filter>, u32)> = profile
+        .targets
+        .iter()
+        .map(|target| {
+            // TODO(ver) use resource metadata from the profile response.
+            let backend_meta = service_meta(&target.addr).unwrap_or_else(|| UNKNOWN_META.clone());
+            let backend = policy::RouteBackend {
+                backend: policy::Backend {
+                    meta: backend_meta,
+                    queue,
+                    dispatcher: policy::BackendDispatcher::BalanceP2c(
+                        EWMA,
+                        policy::EndpointDiscovery::DestinationGet {
+                            path: target.addr.to_string(),
+                        },
+                    ),
+                },
+                filters: std::sync::Arc::new([]),
+            };
+
+            (backend, target.weight)
+        })
+        .collect();
+
+    let distribution = policy::RouteDistribution::RandomAvailable(backends.clone().into());
+
+    static ROUTE_META: Lazy<Arc<policy::Meta>> =
+        Lazy::new(|| policy::Meta::new_default("serviceprofile"));
+    let route = policy::opaq::Route {
+        policy: policy::opaq::Policy {
+            // TODO(ver) use resource metadata from the profile response.
+            meta: ROUTE_META.clone(),
+            params: (),
+            filters: std::sync::Arc::new([]),
+            distribution,
+        },
+    };
+
+    Routes {
+        logical: Logical {
+            addr: profiles_addr.into(),
+            meta: ParentRef(parent_meta),
+        },
+        backends: backends.into_iter().map(|(b, _)| b.backend).collect(),
+        routes: Some(route),
+    }
+}
+
+fn spawn_routes<T>(
+    mut route_rx: watch::Receiver<T>,
+    init: Routes,
+    mut mk: impl FnMut(&T) -> Option<Routes> + Send + Sync + 'static,
+) -> watch::Receiver<Routes>
+where
+    T: Send + Sync + 'static,
+{
+    let (tx, rx) = watch::channel(init);
+
+    tokio::spawn(async move {
+        loop {
+            let res = tokio::select! {
+                biased;
+                _ = tx.closed() => return,
+                res = route_rx.changed() => res,
+            };
+
+            if res.is_err() {
+                // Drop the `tx` sender when the profile sender is
+                // dropped.
+                return;
+            }
+
+            if let Some(routes) = (mk)(&*route_rx.borrow_and_update()) {
+                if tx.send(routes).is_err() {
+                    // Drop the `tx` sender when all of its receivers are dropped.
+                    return;
+                }
+            }
+        }
+    });
+
+    rx
 }

linkerd/app/gateway/src/opaq.rs
@@ -1,10 +1,15 @@
 use super::{server::Opaq, Gateway};
 use inbound::{GatewayAddr, GatewayDomainInvalid};
-use linkerd_app_core::{io, profiles, svc, tls, transport::addrs::*, Error};
+use linkerd_app_core::{io, svc, tls, transport::addrs::*, Error};
 use linkerd_app_inbound as inbound;
 use linkerd_app_outbound as outbound;
+use tokio::sync::watch;
 
-pub type Target = outbound::opaq::Logical;
+#[derive(Clone, Debug)]
+pub struct Target {
+    addr: GatewayAddr,
+    routes: watch::Receiver<outbound::opaq::Routes>,
+}
 
 impl Gateway {
     /// Wrap the provided outbound opaque stack with inbound authorization and
@@ -33,22 +38,55 @@ impl Gateway {
             .push_filter(
                 |(_, opaq): (_, Opaq<T>)| -> Result<_, GatewayDomainInvalid> {
                     // Fail connections were not resolved.
-                    let profile = svc::Param::<Option<profiles::Receiver>>::param(&*opaq)
-                        .ok_or(GatewayDomainInvalid)?;
-                    if let Some(profiles::LogicalAddr(addr)) = profile.logical_addr() {
-                        Ok(outbound::opaq::Logical::Route(addr, profile))
-                    } else if let Some((addr, metadata)) = profile.endpoint() {
-                        Ok(outbound::opaq::Logical::Forward(
-                            Remote(ServerAddr(addr)),
-                            metadata,
-                        ))
-                    } else {
-                        Err(GatewayDomainInvalid)
-                    }
+                    Target::try_from(opaq)
                 },
             )
             // Authorize connections to the gateway.
             .push(self.inbound.authorize_tcp())
             .arc_new_tcp()
+    }
+}
+
+impl<T> TryFrom<Opaq<T>> for Target
+where
+    T: svc::Param<GatewayAddr>,
+{
+    type Error = GatewayDomainInvalid;
+
+    fn try_from(opaq: Opaq<T>) -> Result<Self, Self::Error> {
+        use svc::Param;
+
+        let addr: GatewayAddr = (**opaq).param();
+        let Some(profile) = (*opaq).param() else {
+            // The gateway address must be resolvable via the profile API.
+            return Err(GatewayDomainInvalid);
+        };
+        let routes = outbound::opaq::routes_from_discovery(
+            addr.0.clone().into(),
+            Some(profile),
+            (*opaq).param(),
+        );
+
+        Ok(Target { addr, routes })
+    }
+}
+
+impl svc::Param<watch::Receiver<outbound::opaq::Routes>> for Target {
+    fn param(&self) -> watch::Receiver<outbound::opaq::Routes> {
+        self.routes.clone()
+    }
+}
+
+impl PartialEq for Target {
+    fn eq(&self, other: &Self) -> bool {
+        self.addr == other.addr
+    }
+}
+
+impl Eq for Target {}
+
+impl std::hash::Hash for Target {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.addr.hash(state);
     }
 }

linkerd/proxy/client-policy/src/opaq.rs
@@ -1,11 +1,13 @@
-use crate::RoutePolicy;
+use linkerd_opaq_route as opaq;
 
-#[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
+pub type Policy = crate::RoutePolicy<Filter, ()>;
+pub type Route = opaq::Route<Policy>;
+pub type Rule = opaq::Rule<Policy>;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Opaque {
-    pub policy: Option<Policy>,
+    pub routes: Option<Route>,
 }
-
-pub type Policy = RoutePolicy<Filter, ()>;
 
 #[derive(Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct NonIoErrors;
@@ -59,63 +61,47 @@ pub(crate) mod proto {
         Missing(&'static str),
     }
 
+    pub(crate) fn fill_route_backends(rts: Option<&Route>, set: &mut BackendSet) {
+        if let Some(Route { policy, .. }) = rts {
+            policy.distribution.fill_backends(set);
+        }
+    }
+
     impl TryFrom<outbound::proxy_protocol::Opaque> for Opaque {
         type Error = InvalidOpaqueRoute;
         fn try_from(proto: outbound::proxy_protocol::Opaque) -> Result<Self, Self::Error> {
-            if proto.routes.len() != 1 {
+            if proto.routes.len() > 1 {
                 return Err(InvalidOpaqueRoute::OnlyOneRoute(proto.routes.len()));
             }
+            let routes = proto.routes.into_iter().next().map(try_route).transpose()?;
 
-            proto
-                .routes
-                .into_iter()
-                .next()
-                .ok_or(InvalidOpaqueRoute::OnlyOneRoute(0))?
-                .try_into()
+            Ok(Self { routes })
         }
     }
 
-    impl TryFrom<outbound::OpaqueRoute> for Opaque {
-        type Error = InvalidOpaqueRoute;
+    fn try_route(
+        outbound::OpaqueRoute {
+            metadata,
+            rules,
+            error: _, // TODO
+        }: outbound::OpaqueRoute,
+    ) -> Result<Route, InvalidOpaqueRoute> {
+        let meta = Arc::new(
+            metadata
+                .ok_or(InvalidMeta("missing metadata"))?
+                .try_into()?,
+        );
 
-        fn try_from(
-            outbound::OpaqueRoute {
-                metadata,
-                rules,
-                error: _, // TODO
-            }: outbound::OpaqueRoute,
-        ) -> Result<Self, Self::Error> {
-            let meta = Arc::new(
-                metadata
-                    .ok_or(InvalidMeta("missing metadata"))?
-                    .try_into()?,
-            );
-
-            // Currently, opaque rules have no match expressions, so if there's
-            // more than one rule, we have no way of determining which one to
-            // use. Therefore, require that there's exactly one rule.
-            if rules.len() != 1 {
-                return Err(InvalidOpaqueRoute::OnlyOneRule(rules.len()));
-            }
-
-            let policy = rules
-                .into_iter()
-                .map(|rule| try_rule(&meta, rule))
-                .next()
-                .ok_or(InvalidOpaqueRoute::OnlyOneRule(0))??;
-
-            Ok(Self {
-                policy: Some(policy),
-            })
+        // Currently, opaque rules have no match expressions, so if there's
+        // more than one rule, we have no way of determining which one to
+        // use. Therefore, require that there's exactly one rule.
+        if rules.len() != 1 {
+            return Err(InvalidOpaqueRoute::OnlyOneRule(rules.len()));
         }
-    }
 
-    impl Opaque {
-        pub(crate) fn fill_backends(&self, set: &mut BackendSet) {
-            for p in &self.policy {
-                p.distribution.fill_backends(set);
-            }
-        }
+        let rule = rules.first().cloned().expect("already checked");
+        let policy = try_rule(&meta, rule)?;
+        Ok(Route { policy })
     }
 
     fn try_rule(

Cargo.toml
@@ -40,6 +40,7 @@ members = [
     "linkerd/meshtls/rustls",
     "linkerd/meshtls/verifier",
     "linkerd/metrics",
+    "linkerd/opaq-route",
     "linkerd/opencensus",
     "linkerd/opentelemetry",
     "linkerd/pool",

linkerd/app/outbound/Cargo.toml
@@ -40,6 +40,7 @@ linkerd-http-retry = { path = "../../http/retry" }
 linkerd-http-route = { path = "../../http/route" }
 linkerd-identity = { path = "../../identity" }
 linkerd-meshtls-rustls = { path = "../../meshtls/rustls", optional = true }
+linkerd-opaq-route = { path = "../../opaq-route" }
 linkerd-proxy-client-policy = { path = "../../proxy/client-policy", features = [
     "proto",
 ] }

linkerd/app/outbound/src/discover.rs
@@ -234,16 +234,18 @@ fn policy_for_backend(
     static NO_OPAQ_FILTERS: Lazy<Arc<[policy::opaq::Filter]>> = Lazy::new(|| Arc::new([]));
 
     let opaque = policy::opaq::Opaque {
-        policy: Some(policy::opaq::Policy {
-            meta: meta.clone(),
-            filters: NO_OPAQ_FILTERS.clone(),
-            params: Default::default(),
-            distribution: policy::RouteDistribution::FirstAvailable(Arc::new([
-                policy::RouteBackend {
-                    filters: NO_OPAQ_FILTERS.clone(),
-                    backend: backend.clone(),
-                },
-            ])),
+        routes: Some(policy::opaq::Route {
+            policy: policy::opaq::Policy {
+                meta: meta.clone(),
+                filters: NO_OPAQ_FILTERS.clone(),
+                params: Default::default(),
+                distribution: policy::RouteDistribution::FirstAvailable(Arc::new([
+                    policy::RouteBackend {
+                        filters: NO_OPAQ_FILTERS.clone(),
+                        backend: backend.clone(),
+                    },
+                ])),
+            },
         }),
     };
 
@@ -324,6 +326,15 @@ impl<T> From<((Option<profiles::Receiver>, policy::Receiver), T)> for Discovery<
 impl<T> svc::Param<Option<profiles::Receiver>> for Discovery<T> {
     fn param(&self) -> Option<profiles::Receiver> {
         self.profile.clone()
+    }
+}
+
+impl<T> svc::Param<OrigDstAddr> for Discovery<T>
+where
+    T: svc::Param<OrigDstAddr>,
+{
+    fn param(&self) -> OrigDstAddr {
+        self.parent.param()
     }
 }
 

linkerd/app/outbound/src/http/logical/profile.rs
@@ -2,11 +2,11 @@ use super::{
     super::{concrete, retry},
     CanonicalDstHeader, Concrete, NoRoute,
 };
-use crate::{policy, BackendRef, ParentRef, UNKNOWN_META};
+use crate::{service_meta, BackendRef, ParentRef, UNKNOWN_META};
 use linkerd_app_core::{
     classify, metrics,
     proxy::http::{self, balance},
-    svc, Error, NameAddr,
+    svc, Error,
 };
 use linkerd_distribute as distribute;
 use std::{fmt::Debug, hash::Hash, sync::Arc, time};
@@ -106,26 +106,6 @@ where
             routes,
             targets,
         } = routes;
-
-        fn service_meta(addr: &NameAddr) -> Option<Arc<policy::Meta>> {
-            let mut parts = addr.name().split('.');
-
-            let name = parts.next()?;
-            let namespace = parts.next()?;
-
-            if !parts.next()?.eq_ignore_ascii_case("svc") {
-                return None;
-            }
-
-            Some(Arc::new(policy::Meta::Resource {
-                group: "core".to_string(),
-                kind: "Service".to_string(),
-                namespace: namespace.to_string(),
-                name: name.to_string(),
-                section: None,
-                port: Some(addr.port().try_into().ok()?),
-            }))
-        }
 
         let parent_meta = service_meta(&addr).unwrap_or_else(|| UNKNOWN_META.clone());
 

linkerd/app/outbound/src/ingress.rs
@@ -22,8 +22,11 @@ struct Http<T> {
     version: http::Version,
 }
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-struct Opaq<T>(Discovery<T>);
+#[derive(Clone, Debug)]
+struct Opaq {
+    orig_dst: OrigDstAddr,
+    routes: watch::Receiver<opaq::Routes>,
+}
 
 #[derive(Clone, Debug)]
 struct SelectTarget<T>(Http<T>);
@@ -91,7 +94,7 @@ impl Outbound<()> {
             let discover = discover.clone();
             self.to_tcp_connect()
                 .push_opaq_cached(resolve.clone())
-                .map_stack(|_, _, stk| stk.push_map_target(Opaq))
+                .map_stack(|_, _, stk| stk.push_map_target(Opaq::from))
                 .push_discover(svc::mk(move |OrigDstAddr(addr)| {
                     discover.clone().oneshot(DiscoverAddr(addr.into()))
                 }))
@@ -600,42 +603,41 @@ impl std::hash::Hash for Logical {
     }
 }
 
-// === impl Opaq ===
-
-impl<T> std::ops::Deref for Opaq<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<T> svc::Param<Remote<ServerAddr>> for Opaq<T>
+impl<T> From<Discovery<T>> for Opaq
 where
     T: svc::Param<OrigDstAddr>,
 {
-    fn param(&self) -> Remote<ServerAddr> {
-        let OrigDstAddr(addr) = (*self.0).param();
-        Remote(ServerAddr(addr))
+    fn from(discovery: Discovery<T>) -> Self {
+        use svc::Param;
+
+        let orig_dst: OrigDstAddr = discovery.param();
+        let routes = opaq::routes_from_discovery(
+            Addr::Socket(orig_dst.into()),
+            discovery.param(),
+            discovery.param(),
+        );
+
+        Self { routes, orig_dst }
     }
 }
 
-impl<T> svc::Param<opaq::Logical> for Opaq<T>
-where
-    T: svc::Param<OrigDstAddr>,
-{
-    fn param(&self) -> opaq::Logical {
-        if let Some(profile) = svc::Param::<Option<profiles::Receiver>>::param(&self.0) {
-            if let Some(profiles::LogicalAddr(addr)) = profile.logical_addr() {
-                return opaq::Logical::Route(addr, profile);
-            }
+impl PartialEq for Opaq {
+    fn eq(&self, other: &Self) -> bool {
+        self.orig_dst == other.orig_dst
+    }
+}
 
-            if let Some((addr, metadata)) = profile.endpoint() {
-                return opaq::Logical::Forward(Remote(ServerAddr(addr)), metadata);
-            }
-        }
+impl Eq for Opaq {}
 
-        opaq::Logical::Forward(self.param(), Default::default())
+impl std::hash::Hash for Opaq {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.orig_dst.hash(state);
+    }
+}
+
+impl svc::Param<watch::Receiver<opaq::Routes>> for Opaq {
+    fn param(&self) -> watch::Receiver<opaq::Routes> {
+        self.routes.clone()
     }
 }
 

commit 810eddf79a1712603df725faa2ecd27cf2452a91

fix(outbound): fall back to profile-provided authority
To fix a regression in 7eadac4d9, where authority labels are not properly
set on legacy TCP metrics, this commmit uses the profile-provided authority
when it is set.

linkerd/app/outbound/src/opaq.rs
@@ -9,7 +9,7 @@ use linkerd_app_core::{
     },
     svc,
     transport::addrs::*,
-    Addr, Error,
+    Addr, Error, NameAddr,
 };
 use once_cell::sync::Lazy;
 use std::{fmt::Debug, hash::Hash, sync::Arc};
@@ -86,16 +86,6 @@ impl OpaqMetrics {
     }
 }
 
-fn should_override_opaq_policy(
-    rx: &watch::Receiver<profiles::Profile>,
-) -> Option<profiles::LogicalAddr> {
-    let p = rx.borrow();
-    if p.has_targets() {
-        p.addr.clone()
-    } else {
-        None
-    }
-}
 /// Given both profiles and policy information, this function constructs `opaq::Routes``.
 /// The decision on whether profiles or policy should be used is made by inspecting the
 /// returned profiles and checking whether there are any targets defined. This is done
@@ -106,18 +96,34 @@ pub fn routes_from_discovery(
     profile: Option<profiles::Receiver>,
     mut policy: policy::Receiver,
 ) -> watch::Receiver<Routes> {
-    if let Some(mut profile) = profile.map(watch::Receiver::from) {
-        if let Some(paddr) = should_override_opaq_policy(&profile) {
-            tracing::debug!("Using ServiceProfile");
-            let init = routes_from_profile(paddr.clone(), &profile.borrow_and_update());
-
-            return spawn_routes(profile, init, move |profile: &profiles::Profile| {
-                Some(routes_from_profile(paddr.clone(), profile))
-            });
+    let profile_addr = if let Some(mut rx) = profile.map(watch::Receiver::from) {
+        let profile = rx.borrow_and_update();
+        if let Some(profiles::LogicalAddr(addr)) = profile.addr.clone() {
+            if profile.has_targets() {
+                tracing::debug!(%addr, "Using ServiceProfile");
+                let init = routes_from_profile(addr.clone(), &profile);
+                drop(profile);
+                return spawn_routes(rx, init, move |profile: &profiles::Profile| {
+                    Some(routes_from_profile(addr.clone(), profile))
+                });
+            }
         }
-    }
 
-    tracing::debug!("Using ClientPolicy routes");
+        profile
+            .addr
+            .clone()
+            .map(|profiles::LogicalAddr(pa)| Addr::Name(pa))
+    } else {
+        None
+    };
+
+    // Prefer a named address if the given address is a socket address.
+    let addr = match addr {
+        Addr::Name(na) => Addr::Name(na),
+        addr => profile_addr.unwrap_or(addr),
+    };
+
+    tracing::debug!(%addr, "Using ClientPolicy routes");
     let init = routes_from_policy(addr.clone(), &policy.borrow_and_update())
         .expect("initial policy must be opaque");
 
@@ -147,10 +153,7 @@ fn routes_from_policy(addr: Addr, policy: &policy::ClientPolicy) -> Option<Route
     })
 }
 
-fn routes_from_profile(
-    profiles::LogicalAddr(profiles_addr): profiles::LogicalAddr,
-    profile: &profiles::Profile,
-) -> Routes {
+fn routes_from_profile(addr: NameAddr, profile: &profiles::Profile) -> Routes {
     // TODO: make configurable
     let queue = {
         policy::Queue {
@@ -165,7 +168,7 @@ fn routes_from_profile(
     });
 
     // TODO(ver) use resource metadata from the profile response.
-    let parent_meta = service_meta(&profiles_addr).unwrap_or_else(|| UNKNOWN_META.clone());
+    let parent_meta = service_meta(&addr).unwrap_or_else(|| UNKNOWN_META.clone());
 
     let backends: Vec<(policy::RouteBackend<policy::opaq::Filter>, u32)> = profile
         .targets
@@ -207,7 +210,7 @@ fn routes_from_profile(
 
     Routes {
         logical: Logical {
-            addr: profiles_addr.into(),
+            addr: addr.into(),
             meta: ParentRef(parent_meta),
         },
         backends: backends.into_iter().map(|(b, _)| b.backend).collect(),