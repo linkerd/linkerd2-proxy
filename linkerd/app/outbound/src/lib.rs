cf67200f596c9a68a753dc7f8769ff697432dba0
katelyn martin <kate@buoyant.io>
2024-12-01 00:00:00 +0000

    chore(ci): empty commit to check ci action
    Signed-off-by: katelyn martin <kate@buoyant.io>

--- linkerd/app/outbound/src/lib.rs
+++ linkerd/app/outbound/src/lib.rs
@@ -2,6 +2,8 @@
 //!
 //! The outbound proxy is responsible for routing traffic from the local application to other hosts.
 
+// XXX(kate): change a rust file to prevent ci from skipping jobs.
+
 #![deny(rust_2018_idioms, clippy::disallowed_methods, clippy::disallowed_types)]
 #![allow(opaque_hidden_inferred_bound)]
 #![forbid(unsafe_code)]
