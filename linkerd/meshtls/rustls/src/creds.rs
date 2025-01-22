ebfe8806d3d24ba4a39c3a880661aa5d5f996ce4
Scott Fleener <scott@buoyant.io>
2025-01-22 15:57:07 +0000

    Expand the set of supported signature algorithms
    During the rustls upgrade, it accidentally limited the set of supported signature algorithms to ECDSA256 signatures. This would cause the identity control plane proxy to reject all certify requests with BadSignature if an RSA certificate was used instead of ECDSA.
    
    This updates the set of supported algorithms to most of the full set of what rustls+ring supports, minus a few legacy algorithms.
    
    Tested by deploying to a local cluster and verifying the control plane comes up correctly and app-level networking works as expected.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

--- linkerd/meshtls/rustls/src/creds.rs
+++ linkerd/meshtls/rustls/src/creds.rs
@@ -121,12 +121,49 @@ mod params {
         rustls::SignatureScheme::ECDSA_NISTP256_SHA256;
     pub const SIGNATURE_ALG_RUSTLS_ALGORITHM: rustls::SignatureAlgorithm =
         rustls::SignatureAlgorithm::ECDSA;
+    // A subset of the algorithms supported by rustls+ring, imported from
+    // https://github.com/rustls/rustls/blob/v/0.23.21/rustls/src/crypto/ring/mod.rs#L107
     pub static SUPPORTED_SIG_ALGS: &WebPkiSupportedAlgorithms = &WebPkiSupportedAlgorithms {
-        all: &[webpki::ring::ECDSA_P256_SHA256],
-        mapping: &[(
-            SIGNATURE_ALG_RUSTLS_SCHEME,
-            &[webpki::ring::ECDSA_P256_SHA256],
-        )],
+        all: &[
+            webpki::ring::ECDSA_P256_SHA256,
+            webpki::ring::ECDSA_P256_SHA384,
+            webpki::ring::ECDSA_P384_SHA256,
+            webpki::ring::ECDSA_P384_SHA384,
+            webpki::ring::ED25519,
+            webpki::ring::RSA_PKCS1_2048_8192_SHA256,
+            webpki::ring::RSA_PKCS1_2048_8192_SHA384,
+            webpki::ring::RSA_PKCS1_2048_8192_SHA512,
+            webpki::ring::RSA_PKCS1_3072_8192_SHA384,
+        ],
+        mapping: &[
+            (
+                rustls::SignatureScheme::ECDSA_NISTP384_SHA384,
+                &[
+                    webpki::ring::ECDSA_P384_SHA384,
+                    webpki::ring::ECDSA_P256_SHA384,
+                ],
+            ),
+            (
+                rustls::SignatureScheme::ECDSA_NISTP256_SHA256,
+                &[
+                    webpki::ring::ECDSA_P256_SHA256,
+                    webpki::ring::ECDSA_P384_SHA256,
+                ],
+            ),
+            (rustls::SignatureScheme::ED25519, &[webpki::ring::ED25519]),
+            (
+                rustls::SignatureScheme::RSA_PKCS1_SHA512,
+                &[webpki::ring::RSA_PKCS1_2048_8192_SHA512],
+            ),
+            (
+                rustls::SignatureScheme::RSA_PKCS1_SHA384,
+                &[webpki::ring::RSA_PKCS1_2048_8192_SHA384],
+            ),
+            (
+                rustls::SignatureScheme::RSA_PKCS1_SHA256,
+                &[webpki::ring::RSA_PKCS1_2048_8192_SHA256],
+            ),
+        ],
     };
     pub static TLS_VERSIONS: &[&rustls::SupportedProtocolVersion] = &[&rustls::version::TLS13];
     pub static TLS_SUPPORTED_CIPHERSUITES: &[rustls::SupportedCipherSuite] =
