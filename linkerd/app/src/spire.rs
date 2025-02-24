802963ffbe9dc52421378631b6a4d1840845c35f
katelyn martin <kate@buoyant.io>
2025-02-24 00:00:00 +0000

    chore(app): upgrade to hyper 1.x
    Signed-off-by: katelyn martin <kate@buoyant.io>

--- linkerd/app/src/spire.rs
+++ linkerd/app/src/spire.rs
@@ -1,3 +1,4 @@
+use futures::TryFutureExt;
 use linkerd_app_core::{exp_backoff::ExponentialBackoff, Error};
 use std::sync::Arc;
 use tokio::sync::watch;
@@ -68,7 +69,7 @@ impl tower::Service<()> for Client {
             // as the request to the `MakeConnection`.
             let chan = Endpoint::try_from(TONIC_DEFAULT_URI)?
                 .connect_with_connector(tower::util::service_fn(move |_: Uri| {
-                    UnixStream::connect(stripped_path.clone())
+                    UnixStream::connect(stripped_path.clone()).map_ok(hyper_util::rt::TokioIo::new)
                 }))
                 .await?;
 
