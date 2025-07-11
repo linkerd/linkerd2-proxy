966a5e2c2a86011f9f84796c8477c3a3083d2a6b
katelyn martin <kate@buoyant.io>
2025-07-11 00:00:00 +0000

    fix(app/env): a lower default maximum per-host connection limit
    see also:
    * #4004
    * linkerd/linkerd2#14204
    
    in #4004 we fixed an issue related to our HTTP/1.1 client's connection
    pool.
    
    this further hedges against future issues related to our HTTP client
    exhausting resources available to its container. today, the limit by
    default is `usize::MAX`, which is dramatically higher than the practical
    limit.
    
    this commit changes the limit for outbound idle connections per-host to
    10,000.
    
    Signed-off-by: katelyn martin <kate@buoyant.io>

--- linkerd/app/src/env.rs
+++ linkerd/app/src/env.rs
@@ -348,7 +348,11 @@ const DEFAULT_OUTBOUND_HTTP1_CONNECTION_POOL_IDLE_TIMEOUT: Duration = Duration::
 // many concurrent requests. It's generally preferable to use the MAX_IDLE_AGE
 // limitations to quickly drop idle connections.
 const DEFAULT_INBOUND_MAX_IDLE_CONNS_PER_ENDPOINT: usize = usize::MAX;
-const DEFAULT_OUTBOUND_MAX_IDLE_CONNS_PER_ENDPOINT: usize = usize::MAX;
+// By default, we limit the number of outbound connections that may be opened
+// per-host. We pick a high number (10k) that shouldn't interfere with most
+// workloads, but will prevent issues with our outbound HTTP client from
+// exhausting the file descriptors available to the process.
+const DEFAULT_OUTBOUND_MAX_IDLE_CONNS_PER_ENDPOINT: usize = 10_000;
 
 // These settings limit the number of requests that have not received responses,
 // including those buffered in the proxy and dispatched to the destination
