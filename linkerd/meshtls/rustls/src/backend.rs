cbb51dc09e4eae4674629d7291ec15d1c338e22c
Scott Fleener <scott@buoyant.io>
2025-07-29 12:12:12 -0400

    feat(tls): Remove ring as crypto backend
    The broader ecosystem has mostly moved to aws-lc-rs as the primary rustls backend, and we should follow suit. This will also simplify the maintenance of the proxy's TLS implementation in the long term.
    
    There will need to be some refactoring to clean up the rustls provider interfaces, but that will come in follow-ups.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

--- linkerd/meshtls/rustls/src/backend.rs
+++ linkerd/meshtls/rustls/src/backend.rs
@@ -1,16 +1,3 @@
-#[cfg(all(feature = "aws-lc", feature = "ring"))]
-compile_error!(
-    "Multiple rustls backends enabled. Enabled one of the \"ring\" or \"aws-lc\" features"
-);
-#[cfg(not(any(feature = "aws-lc", feature = "ring")))]
-compile_error!("No rustls backend enabled. Enabled one of the \"ring\" or \"aws-lc\" features");
-
-#[cfg(feature = "aws-lc")]
-mod aws_lc;
-#[cfg(feature = "ring")]
-mod ring;
-
-#[cfg(feature = "aws-lc")]
 pub use aws_lc::{default_provider, SUPPORTED_SIG_ALGS, TLS_SUPPORTED_CIPHERSUITES};
-#[cfg(feature = "ring")]
-pub use ring::{default_provider, SUPPORTED_SIG_ALGS, TLS_SUPPORTED_CIPHERSUITES};
+
+mod aws_lc;
