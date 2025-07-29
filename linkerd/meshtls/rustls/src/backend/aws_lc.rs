1ca7631eac6f263ba95683d032284fa1fc5274c8
Scott Fleener <scott@buoyant.io>
2025-07-29 12:12:12 -0400

    feat(tls): Remove ring as crypto backend
    The broader ecosystem has mostly moved to aws-lc-rs as the primary rustls backend, and we should follow suit. This will also simplify the maintenance of the proxy's TLS implementation in the long term.
    
    There will need to be some refactoring to clean up the rustls provider interfaces, but that will come in follow-ups.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

--- linkerd/meshtls/rustls/src/backend/aws_lc.rs
+++ linkerd/meshtls/rustls/src/backend/aws_lc.rs
@@ -4,14 +4,14 @@ use tokio_rustls::rustls::{
     crypto::{aws_lc_rs, WebPkiSupportedAlgorithms},
 };
 
-#[cfg(not(feature = "aws-lc-fips"))]
+#[cfg(not(feature = "fips"))]
 pub static TLS_SUPPORTED_CIPHERSUITES: &[rustls::SupportedCipherSuite] = &[
     aws_lc_rs::cipher_suite::TLS13_AES_128_GCM_SHA256,
     aws_lc_rs::cipher_suite::TLS13_AES_256_GCM_SHA384,
     aws_lc_rs::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
 ];
 // Prefer aes-256-gcm if fips is enabled
-#[cfg(feature = "aws-lc-fips")]
+#[cfg(feature = "fips")]
 pub static TLS_SUPPORTED_CIPHERSUITES: &[rustls::SupportedCipherSuite] = &[
     aws_lc_rs::cipher_suite::TLS13_AES_256_GCM_SHA384,
     aws_lc_rs::cipher_suite::TLS13_AES_128_GCM_SHA256,
