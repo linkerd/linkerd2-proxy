a64170bd61148ebe50e5ab74da5f4c55730289d1
Scott Fleener <scott@buoyant.io>
2025-08-18 15:27:09 -0400

    chore(tls): Refactor `rustls` shims out of `linkerd-meshtls` (#4080)
    * chore(tls): Refactor shims out of meshtls
    
    Meshtls previously assumed that mutliple TLS implementations could be used. Now that we've consolidated on rustls as the TLS implementation, we can remove these shims.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>
    
    * chore(tls): Refactor mode out of meshtls
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>
    
    ---------
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

--- linkerd/meshtls/src/lib.rs
+++ linkerd/meshtls/src/lib.rs
@@ -20,61 +20,6 @@ pub use self::{
     client::{ClientIo, Connect, ConnectFuture, NewClient},
     server::{Server, ServerIo, TerminateFuture},
 };
-use linkerd_dns_name as dns;
-use linkerd_error::{Error, Result};
-use linkerd_identity as id;
-use std::str::FromStr;
 
 pub use linkerd_meshtls_rustls as rustls;
-
-#[derive(Copy, Clone, Debug)]
-pub enum Mode {
-    Rustls,
-}
-
-// === impl Mode ===
-
-impl Default for Mode {
-    fn default() -> Self {
-        Self::Rustls
-    }
-}
-
-impl Mode {
-    pub fn watch(
-        self,
-        local_id: id::Id,
-        server_name: dns::Name,
-        roots_pem: &str,
-    ) -> Result<(creds::Store, creds::Receiver)> {
-        match self {
-            Self::Rustls => {
-                let (store, receiver) = rustls::creds::watch(local_id, server_name, roots_pem)?;
-                Ok((
-                    creds::Store::Rustls(store),
-                    creds::Receiver::Rustls(receiver),
-                ))
-            }
-        }
-    }
-}
-
-impl FromStr for Mode {
-    type Err = Error;
-
-    fn from_str(s: &str) -> Result<Self> {
-        if s.eq_ignore_ascii_case("rustls") {
-            return Ok(Self::Rustls);
-        }
-
-        Err(format!("unknown TLS backend: {s}").into())
-    }
-}
-
-impl std::fmt::Display for Mode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Rustls => "rustls".fmt(f),
-        }
-    }
-}
+pub use rustls::creds::watch;
