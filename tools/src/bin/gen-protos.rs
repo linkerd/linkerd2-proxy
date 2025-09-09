16434eb78d6d47e2bc628d335d97bb8567299fe3
Scott Fleener <scott@buoyant.io>
2025-09-09 16:59:42 -0400

    fix(tracing): Remove opentelemetry vendoring (#4025)
    * fix(tracing): Remove opentelemetry vendoring
    
    We used to vendor the opentelemetry protobufs because we relied on a version of tonic that `opentelemetry-sdk` didn't support. Now that we've upgraded tonic, we can remove the vendoring and use the official APIs.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>
    
    * chore(deps): Update opentelemetry deps to 0.30
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>
    
    * chore(otel): Remove opentelemetry proto generation
    
    OpenTelemetry protos are no longer vendored, so we no longer need to generate them.
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>
    
    ---------
    
    Signed-off-by: Scott Fleener <scott@buoyant.io>

--- tools/src/bin/gen-protos.rs
+++ tools/src/bin/gen-protos.rs
@@ -1,7 +1,6 @@
 use std::path::{Path, PathBuf};
 
 fn main() {
-    generate_opentelemetry_protos();
     generate_opencensus_protos();
 }
 
@@ -22,24 +21,6 @@ fn generate_opencensus_protos() {
     };
 
     generate_protos(&out_dir, iface_files, &opencensus_dir);
-}
-
-fn generate_opentelemetry_protos() {
-    let opentelemetry_dir = get_proto_dir("opentelemetry-proto");
-
-    let out_dir = opentelemetry_dir.join("src").join("gen");
-
-    let iface_files = {
-        let proto_dir = opentelemetry_dir.join("opentelemetry").join("proto");
-        &[
-            proto_dir.join("collector/trace/v1/trace_service.proto"),
-            proto_dir.join("common/v1/common.proto"),
-            proto_dir.join("resource/v1/resource.proto"),
-            proto_dir.join("trace/v1/trace.proto"),
-        ]
-    };
-
-    generate_protos(&out_dir, iface_files, &opentelemetry_dir);
 }
 
 fn get_proto_dir(name: &str) -> PathBuf {
